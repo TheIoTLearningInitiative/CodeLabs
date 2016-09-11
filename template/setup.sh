2928ccbd0a0fb4fd05401596af4f8f2fbdd20168
Abraham Arce <xe1gyq@gmail.com>
2016-09-11 15:37:11 +0000

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ template/setup.sh
@@ -0,0 +1,18 @@
+echo
+echo "Welcome to Project Playground"
+echo
+
+sleep 4
+
+git clone 
+cd CodeLabs/project/
+
+pip install pip --upgrade
+sh requirements.opkg
+pip install -r requirements.pip
+
+echo
+echo "Now go to CodeLabs/project directory to get started!"
+echo
+echo "Happy Project'ing!"
+echo
---
+++ uxmal/setup.sh
@@ -0,0 +1,18 @@
+echo
+echo "Welcome to Uxmal Playground"
+echo
+
+sleep 4
+
+git clone 
+cd CodeLabs/uxmal/
+
+pip install pip --upgrade
+sh requirements.opkg
+pip install -r requirements.pip
+
+echo
+echo "Now go to CodeLabs/uxmal directory to get started!"
+echo
+echo "Happy Uxmal'ing!"
+echo
