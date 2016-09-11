6ce5cfa958cc20d107569128b784d2755f63e02f
Abraham Arce <xe1gyq@gmail.com>
2016-09-11 17:44:06 +0000

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ Xcambo/setup.sh
@@ -0,0 +1,19 @@
+echo
+echo "Welcome to Xcambo Playground"
+echo
+
+sleep 4
+
+git clone https://github.com/TheIoTLearningInitiative/CodeLabs.git
+cd CodeLabs/Xcambo/
+
+pip install pip --upgrade
+sh requirements.opkg
+pip install -r requirements.pip
+sh requirements.manual
+
+echo
+echo "Now go to CodeLabs/Xcambo directory to get started!"
+echo
+echo "Happy Xcambo'ing!"
+echo
