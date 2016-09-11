c9ae30ea1bfb23e449b4a173f474758c2ed0e769
Abraham Arce <xe1gyq@gmail.com>
2016-09-11 17:03:41 +0000

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- Chicanna/setup.sh
+++ Chicanna/setup.sh
@@ -8,8 +8,10 @@ git clone https://github.com/TheIoTLearningInitiative/CodeLabs.git
 cd CodeLabs/Chicanna/
 
 wget https://raw.githubusercontent.com/xe1gyq/core/master/configuration/haarcascade_frontalface_alt.xml
-wget https://upload.wikimedia.org/wikipedia/commons/5/5d/Barack_Obama_family_portrait_2011.jpg
-mv Barack_Obama_family_portrait_2011.jpg imageinput.jpg
+# wget https://upload.wikimedia.org/wikipedia/commons/5/5d/Barack_Obama_family_portrait_2011.jpg
+# mv Barack_Obama_family_portrait_2011.jpg imageinput.jpeg
+wget https://dab1nmslvvntp.cloudfront.net/wp-content/uploads/2015/09/1442313353nasa-small.jpg
+mv 1442313353nasa-small.jpg imageinput.jpeg
 
 pip install pip --upgrade
 sh requirements.opkg
