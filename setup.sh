d42a7f2ef3d1ec13672c4230abb11db9f16bf587
Abraham Arce <xe1gyq@gmail.com>
2016-09-11 18:07:16 +0000

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,11 @@
+echo
+echo "Welcome to The IoT Learning Initiative Code Labs Playground"
+echo
+
+sleep 4
+
+git clone https://github.com/TheIoTLearningInitiative/CodeLabs.git
+
+echo
+echo "Happy Hacking'ing!"
+echo
