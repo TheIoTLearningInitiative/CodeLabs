5194edde8d0531087deb28535e08ef720b7fb17b
Abraham Arce <xe1gyq@gmail.com>
2016-09-11 18:41:21 +0000

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -6,6 +6,11 @@ sleep 4
 
 git clone https://github.com/TheIoTLearningInitiative/CodeLabs.git
 
+sh Chicanna/setup.sh
+sh Tulum/device/setup.sh
+sh Uxmal/setup.sh
+sh Xcambo/setup.sh
+
 echo
 echo "Happy Hacking'ing!"
 echo
