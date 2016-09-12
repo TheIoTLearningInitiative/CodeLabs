205f57468a7fa42a69901bd210928524b1a83fb2
Abraham Arce <xe1gyq@gmail.com>
2016-09-12 12:52:09 -0500

    Update setup.sh

--- Coba/setup.sh
+++ Coba/setup.sh
@@ -20,7 +20,7 @@ rm data.tar.gz
 rm linux-headers-3.10.17-poky-edison_3.10.17-poky-edison-1_i386.deb
 
 cd
-test build || git clone https://github.com/TheIoTLearningInitiative/CodeLabs.git
+git clone https://github.com/TheIoTLearningInitiative/CodeLabs.git
 # cd CodeLabs/Coba/
 
 # pip install pip --upgrade
