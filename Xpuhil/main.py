92b8c9f37376374f291fd0266727b24fd1ce4df5
Abraham Arce <xe1gyq@gmail.com>
2016-09-13 23:27:59 +0000

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ Xpuhil/main.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+import atexit
+import dweepy
+import signal
+import sys
+import time
+
+import pyupm_grove as grove
+import pyupm_grovespeaker as upmGrovespeaker
+import pyupm_i2clcd as lcd
+
+button = grove.GroveButton(8)
+display = lcd.Jhd1313m1(0, 0x3E, 0x62)
+light = grove.GroveLight(0)
+relay = grove.GroveRelay(2)
+
+def SIGINTHandler(signum, frame):
+	raise SystemExit
+
+def exitHandler():
+	print "Exiting"
+	sys.exit(0)
+
+atexit.register(exitHandler)
+signal.signal(signal.SIGINT, SIGINTHandler)
+
+if __name__ == '__main__':
+
+    message = "Hi! I'm Main!"
+
+    while True:
+
+        luxes = light.value()
+        luxes = int(luxes)
+        display.setColor(luxes, luxes, luxes)
+        display.clear()
+
+        datafreeboard = {}
+        datafreeboard['alive'] = "1"
+        datafreeboard['luxes'] =  luxes
+        datafreeboard['message'] = message
+        dweepy.dweet_for('INTECS2016', datafreeboard)
+
+        if button.value() is 1:
+            display.setColor(255, 0, 0)
+            display.setCursor(0,0)
+            display.write(str(message))
+            relay.on()
+            time.sleep(1)
+            relay.off()
+
+        time.sleep(1)
---
+++ Xpuhil/setup.sh
@@ -0,0 +1,18 @@
+echo
+echo "Welcome to Xpuhil Playground"
+echo
+
+sleep 4
+
+git clone https://github.com/TheIoTLearningInitiative/CodeLabs.git
+cd CodeLabs/Xpuhil/
+
+pip install pip --upgrade
+sh requirements.opkg
+pip install -r requirements.pip
+
+echo
+echo "Now go to CodeLabs/Xpuhil directory to get started!"
+echo
+echo "Happy Xpuhil'ing!"
+echo
