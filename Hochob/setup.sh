5fa23bb2dddbd9d43ac553c61bb72fdb355cf846
Abraham Arce <xe1gyq@gmail.com>
2016-09-13 19:58:15 +0000

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ Hochob/setup.sh
@@ -0,0 +1,16 @@
+echo
+echo "Welcome to Hochob Playground"
+echo
+
+sleep 4
+
+git clone https://github.com/TheIoTLearningInitiative/CodeLabs.git
+cd CodeLabs/Hochob/
+
+sh requirements.opkg
+
+echo
+echo "Now go to CodeLabs/Hochob directory to get started!"
+echo
+echo "Happy Hochob'ing!"
+echo
