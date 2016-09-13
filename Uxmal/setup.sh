ee8f05260ea209df534db73124ade6f38e700ef9
Abraham Arce <xe1gyq@gmail.com>
2016-09-12 20:50:52 -0500

    Update setup.sh

--- Uxmal/setup.sh
+++ Uxmal/setup.sh
@@ -7,6 +7,13 @@ sleep 4
 git clone https://github.com/TheIoTLearningInitiative/CodeLabs.git
 cd CodeLabs/Uxmal/
 
+echo > /etc/opkg/base-feeds.conf
+cat << EOT >> /etc/opkg/base-feeds.conf
+src/gz all http://repo.opkg.net/edison/repo/all
+src/gz edison http://repo.opkg.net/edison/repo/edison
+src/gz core2-32 http://repo.opkg.net/edison/repo/core2-32
+EOT
+
 pip install pip --upgrade
 sh requirements.opkg
 pip install -r requirements.pip
