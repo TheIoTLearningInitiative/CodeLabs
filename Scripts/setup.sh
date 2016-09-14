a188bf23fafb5529355428d7bfa6ee36980d8e2a
Abraham Arce <xe1gyq@gmail.com>
2016-09-13 20:33:12 -0500

    Update setup.sh

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -6,11 +6,15 @@ sleep 4
 
 git clone https://github.com/TheIoTLearningInitiative/CodeLabs.git
 
+sh CodeLabs/Chacmultun/setup.sh
 sh CodeLabs/Chicanna/setup.sh
 sh CodeLabs/Coba/setup.sh
+sh CodeLabs/Hochob/setup.sh
+sh CodeLabs/Kohunlich/setup.sh
 sh CodeLabs/Tulum/device/setup.sh
 sh CodeLabs/Uxmal/setup.sh
 sh CodeLabs/Xcambo/setup.sh
+sh CodeLabs/Xpuhil/setup.sh
 
 echo
 echo "Happy Hacking'ing!"
