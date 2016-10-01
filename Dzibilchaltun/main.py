db4b803dedf27a7399c8f37c13299b722b8a127a
Abraham Arce <xe1gyq@gmail.com>
2016-10-01 15:34:13 +0000

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ Dzibilchaltun/main.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+
+import atexit
+import ConfigParser
+import signal
+import sys
+import time
+
+import pyupm_grove as grove
+import pyupm_grovespeaker as upmGrovespeaker
+import pyupm_i2clcd as lcd
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+credentials = ConfigParser.ConfigParser()
+credentialsfile = "credentials.config"
+credentials.read(credentialsfile)
+
+button = grove.GroveButton(8)
+display = lcd.Jhd1313m1(0, 0x3E, 0x62)
+light = grove.GroveLight(0)
+relay = grove.GroveRelay(2)
+
+def functionLight(bot, update):
+    luxes = light.value()
+    bot.sendMessage(update.message.chat_id, text='Light ' + str(luxes))
+
+def functionMessage(bot, update):
+    bot.sendMessage(update.message.chat_id, text=message)
+
+def functionRelay(bot, update):
+    relay.on()
+    time.sleep(2)
+    relay.off()
+    bot.sendMessage(update.message.chat_id, text='Relay Used!')
+
+def functionEcho(bot, update):
+    bot.sendMessage(update.message.chat_id, text=update.message.text)
+
+def SIGINTHandler(signum, frame):
+	raise SystemExit
+
+def exitHandler():
+	print "Exiting"
+	sys.exit(0)
+
+atexit.register(exitHandler)
+signal.signal(signal.SIGINT, SIGINTHandler)
+
+if __name__ == '__main__':
+
+    credential = credentials.get("telegram", "token")
+    updater = Updater(credential)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("light", functionLight))
+    dp.add_handler(CommandHandler("message", functionMessage))
+    dp.add_handler(CommandHandler("relay", functionRelay))
+    dp.add_handler(MessageHandler([Filters.text], functionEcho))
+
+    updater.start_polling()
+
+    message = "Hi! I'm Dzibilchaltun!"
+
+    while True:
+
+        luxes = light.value()
+        luxes = int(luxes)    
+        display.setColor(luxes, luxes, luxes)
+        display.clear()
+
+        if button.value() is 1:
+            display.setColor(255, 0, 0)
+            display.setCursor(0,0)
+            display.write(str(message))
+            relay.on()
+            time.sleep(1)
+            relay.off()
+
+    updater.idle()
---
+++ Dzibilchaltun/setup.sh
@@ -0,0 +1,22 @@
+echo
+echo "Welcome to Dzibilchaltun Playground"
+echo
+
+sleep 4
+
+git clone https://github.com/TheIoTLearningInitiative/CodeLabs.git
+cd CodeLabs/Dzibilchaltun
+
+pip install pip --upgrade
+sh requirements.opkg
+pip install -r requirements.pip
+sh requirements.manual
+
+echo
+echo "Now go to CodeLabs/Dzibilchaltun directory to get started!"
+echo
+echo "Work on having your credentials ready!"
+echo "credentials.config"
+echo
+echo "Happy Dzibilchaltun'ing!"
+echo
