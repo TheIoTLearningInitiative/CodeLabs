4b77c81f16a06e4e2877843e7f010ff3718e1bc8
root <cynthia perez>
2016-10-07 22:19:31 +0000

    Update
    Signed-off-by: root <cynthia perez>

--- Coba/main.c
+++ Coba/main.c
@@ -2,15 +2,21 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 
+static char *minombre = "blah";
+
+module_param(minombre, charp, 0000);
+MODULE_PARM_DESC(minombre, "A character string");
+
 static int module_init_function(void)
 {
-    printk(KERN_INFO "Main? Hello!\n");
+    //printk(KERN_INFO "Main? Hello!\n");
+    printk(KERN_INFO "Main? Hello! %s\n", minombre);
     return 0;
 }
 
 static void module_exit_function(void)
 {
-    printk(KERN_INFO "Main? Bye!\n");
+    printk(KERN_INFO "Main? Bye! %s\n", minombre);
 }
 
 MODULE_LICENSE("GPL");
