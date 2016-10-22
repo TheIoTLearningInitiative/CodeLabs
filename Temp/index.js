df082c9f73395cf573a48e5c35094f565e7e3e5c
Abraham Arce <xe1gyq@gmail.com>
2016-10-22 23:52:25 +0000

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- Temp/index.js
+++ Temp/index.js
@@ -1,6 +1,21 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var five = require("johnny-five");
+var Edison = require("edison-io");
+var board = new five.Board({
+  io: new Edison()
+});
+
+var led;
+var lcd;
+
+board.on("ready", function() {
+  led = new five.Led(13);
+  lcd = new five.LCD({
+    controller: "JHD1313M1"
+  });
+});
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -9,6 +24,10 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+  });
+  socket.on('led:on', function(msg){
+      led.blink();
+      lcd.cursor(0, 0).print(msg);
   });
 });
 
