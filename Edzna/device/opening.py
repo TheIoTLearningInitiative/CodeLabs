4067d7214c798aa71f86704416f92f90834ea025
Abraham Arce <xe1gyq@gmail.com>
2016-10-28 16:33:51 +0000

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- Edzna/device/opening.py
+++ Edzna/device/opening.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python
 
 import paho.mqtt.client as paho
+import pywapi
+import signal
+import sys
 import time
-import pyupm_biss0001
+import pyupm_grove as grove
 from threading import Thread
 
-motion = pyupm_biss0001.BISS0001(6)
+button = grove.GroveButton(8)
 
 def functionDataSensor():
-    return motion.value()
+    return button.value()
 
 def functionDataSensorMqttPublish():
     mqttclient = paho.Client()
     mqttclient.connect("iot.eclipse.org", 1883, 60)
     while True:
         data = functionDataSensor()
-        topic = "edzna/principal/motion"
+        topic = "edzna/principal/opening"
         mqttclient.publish(topic, data)
         time.sleep(1)
 
@@ -24,7 +27,7 @@ if __name__ == '__main__':
     threadmqttpublish = Thread(target=functionDataSensorMqttPublish)
     threadmqttpublish.start()
 
-    print "Hello Edzna @ Motion"
+    print "Hello Edzna @ Opening"
 
     while True:
         time.sleep(5)
