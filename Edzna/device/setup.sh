1156b16dc5e79e3b5db7d4e767378091579878b3
Abraham Arce <xe1gyq@gmail.com>
2016-11-21 17:55:02 -0600

    Create setup.sh

---
+++ Edzna/device/setup.sh
@@ -0,0 +1,20 @@
+echo
+echo "Welcome to Edzna Playground"
+echo
+
+sleep 4
+
+#git clone https://github.com/TheIoTLearningInitiative/CodeLabs.git
+#cd CodeLabs/Edzna/
+#cd device
+
+pip install pip --upgrade
+sh requirements.opkg
+pip install -r requirements.pip
+sh requirements.manual
+
+echo
+echo "Now go to CodeLabs/Edzna directory to get started!"
+echo
+echo "Happy Edzna'ing!"
+echo
