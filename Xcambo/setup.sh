0aef0a1c5bca4a88338498683de5b414af48ac77
Abraham Arce <xe1gyq@gmail.com>
2016-11-21 18:46:23 -0600

    Update setup.sh

--- Xcambo/setup.sh
+++ Xcambo/setup.sh
@@ -4,8 +4,8 @@ echo
 
 sleep 4
 
-#git clone https://github.com/TheIoTLearningInitiative/CodeLabs.git
-#cd CodeLabs/Xcambo/
+# git clone https://github.com/TheIoTLearningInitiative/CodeLabs.git
+cd CodeLabs/Xcambo/
 
 pip install pip --upgrade
 sh requirements.opkg
