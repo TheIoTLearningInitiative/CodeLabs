f852a4ff54dcfb4da4d1132a7196a84b19ac2ccc
Abraham Arce <xe1gyq@gmail.com>
2016-11-21 18:41:02 -0600

    Update setup.sh

--- Chacmultun/setup.sh
+++ Chacmultun/setup.sh
@@ -4,7 +4,7 @@ echo
 
 sleep 4
 
-git clone https://github.com/TheIoTLearningInitiative/CodeLabs.git
+# git clone https://github.com/TheIoTLearningInitiative/CodeLabs.git
 cd CodeLabs/Chacmultun/
 
 sh requirements.opkg
