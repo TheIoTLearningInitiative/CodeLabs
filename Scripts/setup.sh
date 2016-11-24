f09ca419d6b2ddc188b98fb212a3e32a6721a0e5
Abraham Arce <xe1gyq@gmail.com>
2016-11-23 23:54:24 -0600

    Update setup.sh

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -10,17 +10,17 @@ opkg update
 
 sh CodeLabs/Chacmultun/setup.sh
 sh CodeLabs/Chicanna/setup.sh
-sh CodeLabs/ChichenItza/setup.sh
-sh CodeLabs/Coba/setup.sh
+# sh CodeLabs/ChichenItza/setup.sh
+# sh CodeLabs/Coba/setup.sh
 sh CodeLabs/Dzibilchaltun/setup.sh
 sh CodeLabs/Edzna/device/setup.sh
-sh CodeLabs/Hochob/setup.sh
+# sh CodeLabs/Hochob/setup.sh
 # sh CodeLabs/Hormiguero/
 # sh CodeLabs/Kanki/
 # sh CodeLabs/KinichKaKMoo
-sh CodeLabs/Kohunlich/setup.sh
+# sh CodeLabs/Kohunlich/setup.sh
 # sh CodeLabs/LaRutaPuuc
-sh CodeLabs/Mayapan/setup.sh
+# sh CodeLabs/Mayapan/setup.sh
 # sh CodeLabs/Tabasqueno
 sh CodeLabs/Tulum/device/setup.sh
 sh CodeLabs/Uxmal/setup.sh
