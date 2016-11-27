5530df5acabbf625c69538213f19994f577a03d4
Abraham Arce <xe1gyq@gmail.com>
2016-11-27 09:09:29 -0600

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- Caracol/xrecognize.py
+++ Caracol/xrecognize.py
@@ -11,7 +11,7 @@ from xsound import playAudio
 
 class xSpeechRecognition(object):
 
-    def __init__(self, engine='google', wavfile='/files/sound.wav'):
+    def __init__(self, engine='witai', wavfile='/files/sound.wav'):
         self.directorycurrent = os.path.dirname(os.path.realpath(__file__))
         self.wavfile = self.directorycurrent + wavfile
         print self.wavfile
@@ -29,11 +29,26 @@ class xSpeechRecognition(object):
             try:
                 from pprint import pprint
                 print("Google Speech Recognition")
-                # return self.r.recognize_google(self.audio, show_all=False)
+                return self.r.recognize_google(self.audio, show_all=False)
             except sr.UnknownValueError:
                 print("Google Speech Recognition could not understand audio")
             except sr.RequestError as e:
                 print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        if self.engine == 'witai':
+            WIT_AI_KEY = "7LUFYD6ZZKLYSI652C75J4UZWTIMJIPX"
+            try:
+                print("Wit.ai thinks you said " + self.r.recognize_wit(self.audio, key=WIT_AI_KEY))
+            except sr.UnknownValueError:
+                print("Wit.ai could not understand audio")
+            except sr.RequestError as e:
+                print("Could not request results from Wit.ai service; {0}".format(e))
+        if self.engine == 'sphinx':
+            try:
+                print("Sphinx thinks you said " + self.r.recognize_sphinx(self.audio))
+            except sr.UnknownValueError:
+                print("Sphinx could not understand audio")
+            except sr.RequestError as e:
+                print("Sphinx error; {0}".format(e))
         return None
 
 def recognizeSpeech():
