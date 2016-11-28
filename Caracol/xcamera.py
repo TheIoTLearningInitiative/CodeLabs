cfccfe3b75219a2f06b275c10ee87c184881a189
Abraham Arce <xe1gyq@gmail.com>
2016-11-28 12:42:49 -0600

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- Caracol/xcamera.py
+++ Caracol/xcamera.py
@@ -17,9 +17,8 @@ class xCamera(object):
 
     def setup(self):
         self.fswebcam = 'fswebcam'
-        #self.fswebcamarguments = ' -r 1280x720 -s brightness=65% -s Contrast=50% -s Gamma=100% --jpeg 100 --no-banner '
+        self.fswebcamarguments = ' -r 1280x720 -s brightness=65% -s Contrast=50% -s Gamma=100% --jpeg 100 --no-banner '
         #self.fswebcamarguments = ' -r 1280x720 --jpeg 100 --no-banner '
-        self.fswebcamarguments = ' -r 1280x720 --jpeg 100 --no-banner '
         self.fswebcamcommand = self.fswebcam + self.fswebcamarguments + self.picturefswebcam
 
     def capture(self):
