906e5e508b8eeb1f134a111fedc8ba0c8bf9a4a7
Abraham Arce <xe1gyq@gmail.com>
2017-02-20 03:28:56 +0000

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ Muyil/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import pywapi
+import signal
+import sys
+import time
+
+from threading import Thread
+
+def functionApiWeather():
+    data = pywapi.get_weather_from_weather_com('MXJO0043', 'metric')
+    message = data['location']['name']
+    message = message + ", Temperature " + data['current_conditions']['temperature'] + " C"
+    message = message + ", Atmospheric Pressure " + data['current_conditions']['barometer']['reading'][:-3] + " mbar"
+    return message
+
+def functionSignalHandler(signal, frame):
+    sys.exit(0)
+
+if __name__ == '__main__':
+
+    signal.signal(signal.SIGINT, functionSignalHandler)
+
+    while True:
+        print "Hello Muyil"
+        print "API Weather: %s " % functionApiWeather()
+        time.sleep(5)
+
+# End of File
---
+++ Muyil/setup.sh
@@ -0,0 +1,17 @@
+echo
+echo "Welcome to Muyil Playground"
+echo
+
+sleep 4
+
+# git clone https://github.com/TheIoTLearningInitiative/CodeLabs.git
+cd CodeLabs/Muyil/
+
+sh requirements.opkg
+sh requirements.manual
+
+echo
+echo "Now go to CodeLabs/Muyil directory to get started!"
+echo
+echo "Happy Muyil'ing!"
+echo
