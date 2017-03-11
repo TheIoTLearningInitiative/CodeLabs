a61a67ac5f289be736522be7c81793afcaf45b53
Abraham Arce <xe1gyq@gmail.com>
2017-03-11 20:32:04 +0000

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- ChichenItza/setup/setup.sh
+++ ChichenItza/setup/setup.sh
@@ -2,34 +2,81 @@ echo
 echo "Welcome to Chichen Itza Playground"
 echo
 
-sleep 4
+# Audio: Advanced Linux Sound Architecture
 
-#git clone https://github.com/TheIoTLearningInitiative/CodeLabs.git
-cd CodeLabs/ChichenItza/
+opkg install alsa-utils libportaudio2 libasound2 bison alsa-lib-dev alsa-utils-dev alsa-dev
 
-pip install pip --upgrade
-sh requirements.opkg
-sh requirements.manual
-pip install -r requirements.pip
-
-cat <<EOF > ~/.asoundrc
+cat << EOT >> /home/root/.asoundrc
 pcm.!default {
-    type plug
-       slave {
-           pcm "hw:2,0"
-       }
+        type asym
+        playback.pcm {
+                type plug
+                slave.pcm "hw:2,0"
+        }
+        capture.pcm {
+                type plug
+                slave.pcm "hw:2,0"
+        }
 }
-ctl.!default {
-    type plug
-        slave {
-           pcm "hw:2,0"
-       }
-}
-EOF
+EOT
 
-mkdir configuration
-echo > configuration/voicerss.mk
-echo > configuration/voicerss.ak
+# Audio: PortAudio
+
+wget http://www.portaudio.com/archives/pa_stable_v190600_20161030.tgz
+tar xvf pa_stable_v190600_20161030.tgz
+rm pa_stable_v190600_20161030.tgz
+cd portaudio/
+./configure
+make
+make install
+cd ..
+ldconfig
+cat << EOT >> ~/.profile          
+export LD_LIBRARY_PATH=/usr/local/lib
+EOT
+
+# Audio: PyAudio
+
+pip install pyaudio
+
+# Text To Speech: eSpeak
+
+opkg install espeak
+pip install pyttsx
+
+# Speech To Text: CMU Sphynx: SphinxBase
+
+pip install cython
+
+wget https://sourceforge.net/projects/cmusphinx/files/sphinxbase/5prealpha/sphinxbase-5prealpha.tar.gz/download
+tar xvf download
+rm download
+cd sphinxbase-5prealpha/
+./configure
+make
+make install
+cd ..
+
+ldconfig
+cat << EOT >> ~/.profile
+export LD_LIBRARY_PATH=/usr/local/lib
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+EOT
+
+# Speech To Text: CMU Sphynx: PocketSphinx
+
+wget https://sourceforge.net/projects/cmusphinx/files/pocketsphinx/5prealpha/pocketsphinx-5prealpha.tar.gz/download
+tar xvf download
+rm download
+cd pocketsphinx-5prealpha/
+./configure
+make
+make install
+cd ..
+
+# Speech To Text: SpeechRecognition
+
+pip install --upgrade SpeechRecognition
 
 echo
 echo "Now go to CodeLabs/ChichenItza directory to get started!"
