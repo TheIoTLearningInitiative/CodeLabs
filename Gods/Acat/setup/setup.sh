e4d5ddbc8d831aa1dc787510b833be1fc6cf5162
Abraham Arce <xe1gyq@gmail.com>
2017-03-12 13:48:49 -0500

    Update setup.sh

--- Gods/Acat/setup/setup.sh
+++ Gods/Acat/setup/setup.sh
@@ -2,6 +2,8 @@ echo
 echo "Welcome to Acat Playground"
 echo
 
+# General
+
 opkg update
 opkg install mraa
 opkg install upm
@@ -9,16 +11,28 @@ opkg install upm
 cd
 mkdir nodered
 cd nodered/
+
+# Node-RED Base
+
 npm install node-red
+
+# Npm Contributions
+
 npm install mraa
 npm install galileo-io
-npm install node-red-contrib-gpio
+
+# Node-RED Official
+
 npm install node-red-node-intel-gpio
+npm install node-red-node-watson
+npm install node-red-bluemix-nodes
+
+# Node-RED Contributions
+
+npm install node-red-contrib-gpio
 npm install node-red-contrib-grove-edison
 npm install node-red-contrib-upm
-npm install node-red-node-watson
 npm install node-red-contrib-play-audio
-npm install node-red-bluemix-nodes
 npm install node-red-contrib-voicerss-tts
 
 echo
