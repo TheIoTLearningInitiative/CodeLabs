8b6c218be1564ebfba26b6db435050737cbbeb9b
Abraham Arce <xe1gyq@gmail.com>
2017-03-12 13:01:39 -0500

    Update setup.sh

--- Coba/setup.sh
+++ Coba/setup.sh
@@ -2,8 +2,6 @@ echo
 echo "Welcome to Coba Playground"
 echo
 
-sleep 4
-
 cd
 wget https://github.com/SourceCodeCat/IoTDownloads/raw/master/linux-headers-3.10.17-poky-edison_3.10.17-poky-edison-1_i386.deb
 ar x linux-headers-3.10.17-poky-edison_3.10.17-poky-edison-1_i386.deb
@@ -18,13 +16,6 @@ ln -s /home/root/usr/src/linux-headers-3.10.98-poky-edison build
 cd
 rm data.tar.gz
 rm linux-headers-3.10.17-poky-edison_3.10.17-poky-edison-1_i386.deb
-
-git clone https://github.com/TheIoTLearningInitiative/CodeLabs.git
-# cd CodeLabs/Coba/
-
-# pip install pip --upgrade
-# sh requirements.opkg
-# pip install -r requirements.pip
 
 echo
 echo "Now go to CodeLabs/Coba directory to get started!"
