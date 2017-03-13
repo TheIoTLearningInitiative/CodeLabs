9922803b1262896db50f38fa0c8692ad2b7d1820
Abraham Arce <xe1gyq@gmail.com>
2017-03-12 21:21:59 -0500

    Update setup.sh

--- Coba/setup/setup.sh
+++ Coba/setup/setup.sh
@@ -6,6 +6,8 @@ cd
 wget https://github.com/SourceCodeCat/IoTDownloads/raw/master/linux-headers-3.10.17-poky-edison_3.10.17-poky-edison-1_i386.deb
 ar x linux-headers-3.10.17-poky-edison_3.10.17-poky-edison-1_i386.deb
 tar -xvf data.tar.gz
+rm data.tar.gz
+rm linux-headers-3.10.17-poky-edison_3.10.17-poky-edison-1_i386.deb
 cd ~/usr/src/
 ln -s linux-headers-3.10.17-poky-edison linux-headers-3.10.98-poky-edison
 cd
@@ -14,8 +16,6 @@ echo "#define UTS_RELEASE \"3.10.98-poky-edison+\"" > ~/usr/src/linux-headers-3.
 cd /lib/modules/3.10.98-poky-edison+
 ln -s /home/root/usr/src/linux-headers-3.10.98-poky-edison build
 cd
-rm data.tar.gz
-rm linux-headers-3.10.17-poky-edison_3.10.17-poky-edison-1_i386.deb
 
 echo
 echo "Now go to CodeLabs/Coba directory to get started!"
