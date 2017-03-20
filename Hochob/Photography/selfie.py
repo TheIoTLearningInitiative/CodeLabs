a6a17fc421af9974ad949b83ba66ecf1f0fa9358
Abraham Arce <xe1gyq@gmail.com>
2017-03-20 10:46:46 -0600

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- Hochob/Photography/selfie.py
+++ Hochob/Photography/selfie.py
@@ -2,7 +2,6 @@
 
 import os
 import time
-import time
 
 from xcamera import takePhoto
 from subprocess import call
@@ -11,19 +10,25 @@ if __name__ == "__main__":
 
     directorycurrent = os.path.dirname(os.path.realpath(__file__))
 
-    picturepath = directorycurrent  + "/Pictures/"
-    picturename = picturepath + "10.jpeg"
+    mediapath = directorycurrent  + "/Media/"
+
+    picturename = time.strftime("%Y%m%d-%H%M%S") + ".jpg"
+    picturepath = mediapath + picturename
+
     videoname = time.strftime("%Y%m%d-%H%M%S") + ".mp4"
-    videopath = picturepath + videoname
+    videopath = mediapath + videoname
 
     for i in range(1,120):
-        name = "%02d" % i + ".jpeg"
-        picture = picturepath + name
-        takePhoto(name)
+        name = "%02d" % i + ".jpg"
+        picture = mediapath + name
+        takePhoto(picture)
+        time.sleep(1)
 
-    pictures = picturepath + "%02d.jpeg"
+    takePhoto(picturename)
+
+    pictures = mediapath + "%02d.jpg"
     call(["ffmpeg", "-framerate", "30", "-i", pictures, "-vf", "fps=30", "-pix_fmt", "yuv420p", videopath])
     call(["mv", videopath, "/home/hochob/Selfies/"])
-    call(["cp", picturename, "/home/hochob/Pictures/"])
+    call(["mv", picturename, "/home/hochob/Pictures/"])
 
 # End of File
