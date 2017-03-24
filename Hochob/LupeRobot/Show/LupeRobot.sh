50bbf6aba23c16c39b2656aa33191a42311ec2d7
Abraham Arce <xe1gyq@gmail.com>
2017-03-24 07:18:23 -0600

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ Hochob/LupeRobot/Show/LupeRobot.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+pacmd "set-default-sink 1"
+
+sleep 1
+
+sh /home/hochob/CodeLabs/Hochob/LupeRobot/Show/LupeRobot01.sh &
+(sleep 8.5) && echo "Perdon! Lo se!, Es que me emociono" | espeak -v es-la -a 200
+(sleep 3.0) && echo "Por supuesto" | espeak -v es-la -a 200
+(sleep 38.5) && echo "En mi caso, mi cerebro esta esta ubicado en mi pecho, com pueden ver aqui!" | espeak -v es-la -a 200
+(sleep 25.3) && echo "Yo funciono con la plataforma Intel Edison" | espeak -v es-la -a 200
+sleep 75
+
+pkill vlc
+
+# End Of File
---
+++ Hochob/LupeRobot/Show/LupeRobot01.sh
@@ -0,0 +1,2 @@
+cvlc --no-video-title-show --fullscreen /home/hochob/LupeRobot/FinalIntroVideo.mp4 &
+sleep 0.6 && echo hola a todos | espeak -v es-la -a 200
