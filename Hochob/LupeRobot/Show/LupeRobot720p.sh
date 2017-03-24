8845635478f0de59d45978840d54d1f969d6df51
Abraham Arce <xe1gyq@gmail.com>
2017-03-23 20:52:00 -0600

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- Hochob/LupeRobot/Show/LupeRobot720p.sh
+++ Hochob/LupeRobot/Show/LupeRobot720p.sh
@@ -5,13 +5,13 @@ pacmd "set-default-sink 1"
 sleep 1
 
 sh /home/hochob/CodeLabs/Hochob/LupeRobot/Show/LupeRobot720p01.sh &
-(sleep 9.0) && echo "Perdon! Lo se!, Es que me emociono" | espeak -v es-la -a 200
+(sleep 9.5) && echo "Perdon! Lo se!, Es que me emociono" | espeak -v es-la -a 200
 cvlc --no-video-title-show --fullscreen /home/hochob/LupeRobot/720p/Parte2_v2.mp4 &
-(sleep 3.0) && echo "Por supuesto" | espeak -v es-la -a 200
+(sleep 3.5) && echo "Por supuesto" | espeak -v es-la -a 200
 cvlc --no-video-title-show --fullscreen /home/hochob/LupeRobot/720p/Parte3_v2.mp4 &
-(sleep 39.0) && echo "En mi caso, mi cerebro esta esta ubicado en mi pecho, com pueden ver aqui!" | espeak -v es-la -a 200
+(sleep 39.5) && echo "En mi caso, mi cerebro esta esta ubicado en mi pecho, com pueden ver aqui!" | espeak -v es-la -a 200
 cvlc --no-video-title-show --fullscreen /home/hochob/LupeRobot/720p/Parte4_v2.mp4 &
-(sleep 26.0) && echo "Yo funciono con la plataforma Intel Edison" | espeak -v es-la -a 200
+(sleep 26.5) && echo "Yo funciono con la plataforma Intel Edison" | espeak -v es-la -a 200
 cvlc --no-video-title-show --fullscreen /home/hochob/LupeRobot/720p/Parte5_v2.mp4
 
 pkill vlc
