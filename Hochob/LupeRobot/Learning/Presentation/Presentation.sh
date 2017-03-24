100f789511ab826efc68101d05d4af385050e1aa
Abraham Arce <xe1gyq@gmail.com>
2017-03-24 00:47:20 -0600

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ Hochob/LupeRobot/Learning/Presentation/Presentation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Hola! Mi nombre es Lupe" | espeak -v es-la -a 200
+echo "Mi creador se llama Mario Quiroga" | espeak -v es-la -a 200
+echo "Actualmente puedo moverme, bailar y reconocer lo que me dices" | espeak -v es-la -a 200
+echo "Mucho gusto! Que disfrutes tu recorrido" | espeak -v es-la -a 200
+
+
