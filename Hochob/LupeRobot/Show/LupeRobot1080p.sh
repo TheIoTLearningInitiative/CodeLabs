a3c3ef0fbfbe4b9a964bc91c7f2181392a53f922
Abraham Arce <xe1gyq@gmail.com>
2017-03-30 23:16:44 -0600

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- Hochob/LupeRobot/Show/LupeRobot1080p.sh
+++ Hochob/LupeRobot/Show/LupeRobot1080p.sh
@@ -1,19 +1,29 @@
 #!/bin/sh
 
-#pacmd "set-default-sink 1"
-
-sleep 1
+set -x
 
 cvlc --no-video-title-show --fullscreen /home/hochob/LupeRobot/1080p/Silencio.mp4 &
-(sleep 1) && echo "hola a todos" | espeak -v es-la -a 200
+#(sleep 1) && echo "hola a todos" | espeak -v es-la -a 200
+(mosquitto_pub -h iot.eclipse.org -p 1883 -t lupe/inicial -m 1) && sleep 1.25
+
 cvlc --no-video-title-show --fullscreen /home/hochob/LupeRobot/1080p/Parte1_v3.mp4 &
-(sleep 7.5) && echo "Perdon! Lo se!, Es que me emociono" | espeak -v es-la -a 200
+#(sleep 7.5) && echo "Perdon! Lo se!, Es que me emociono" | espeak -v es-la -a 200
+sleep 6.35 && mosquitto_pub -h iot.eclipse.org -p 1883 -t lupe/emocion -m 1 && sleep 4
+
 cvlc --no-video-title-show --fullscreen /home/hochob/LupeRobot/1080p/Parte2_v2.mp4 &
-(sleep 3.5) && echo "Por supuesto" | espeak -v es-la -a 200
+##(sleep 3.5) && echo "Por supuesto" | espeak -v es-la -a 200
+sleep 2.7 && mosquitto_pub -h iot.eclipse.org -p 1883 -t lupe/porsupuesto -m 1 && sleep 2.5
+
 cvlc --no-video-title-show --fullscreen /home/hochob/LupeRobot/1080p/Parte3_v2.mp4 &
-(sleep 39.5) && echo "En mi caso, mi cerebro esta esta ubicado en mi pecho, com pueden ver aqui!" | espeak -v es-la -a 200
+##(sleep 39.5) && echo "En mi caso, mi cerebro esta aqui!" | espeak -v es-la -a 200
+sleep 37.5 && mosquitto_pub -h iot.eclipse.org -p 1883 -t lupe/cerebro -m 1
+mosquitto_pub -h iot.eclipse.org -p 1883 -t lupe/rightfold -m 1
+mosquitto_pub -h iot.eclipse.org -p 1883 -t lupe/rightup -m 1 && sleep 3
+
 cvlc --no-video-title-show --fullscreen /home/hochob/LupeRobot/1080p/Parte4_v2.mp4 &
-(sleep 26.5) && echo "Yo funciono con la plataforma Intel Edison" | espeak -v es-la -a 200
+##(sleep 26.5) && echo "Yo funciono con la plataforma Intel Edison" | espeak -v es-la -a 200
+sleep 25.5 && mosquitto_pub -h iot.eclipse.org -p 1883 -t lupe/inteledison -m 1 && sleep 4
+
 cvlc --no-video-title-show --fullscreen /home/hochob/LupeRobot/1080p/Parte5_v2.mp4 &
 
 # End Of File
