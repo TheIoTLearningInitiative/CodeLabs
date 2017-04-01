1d73c6199f0d8ac31251ddab63c9c050154eb21b
Abraham Arce <xe1gyq@gmail.com>
2017-04-01 17:13:21 -0500

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- Hochob/Audio/SectionOne.sh
+++ Hochob/Audio/SectionOne.sh
@@ -2,16 +2,26 @@ set -x
 
 killall -9 stream2chromecast.py
 
-cd /home/hochob/CodeLabs/Hochob/Audio/stream2chromecast
-./stream2chromecast.py -devicename "Juum" /home/hochob/Audio/SectionOne01.mp3
-./stream2chromecast.py -devicename "Juum" -stop
-sleep 10
-./stream2chromecast.py -devicename "Juum" /home/hochob/Audio/SectionOne02.mp3
-./stream2chromecast.py -devicename "Juum" -stop
-sleep 5
-./stream2chromecast.py -devicename "Juum" /home/hochob/Audio/SectionOne03.mp3
-./stream2chromecast.py -devicename "Juum" -stop
-sleep 3
-./stream2chromecast.py -devicename "Juum" /home/hochob/Audio/SectionOne04.mp3
-./stream2chromecast.py -devicename "Juum" -stop
+IAM=`whoami`
 
+if [ "$IAM" == "root" ]; then
+        IPATH="/"$IAM"/"
+	cd $IPATH/CodeLabs/Hochob/Audio/stream2chromecast
+        DEVICE="Juum"
+else
+        IPATH="/home/"$IAM"/"
+	cd $IPATH/CodeLabs/Hochob/Audio/stream2chromecast
+        DEVICE="Juum"
+fi
+
+./stream2chromecast.py -devicename $DEVICE $IPATH/Audio/SectionOne01.mp3
+./stream2chromecast.py -devicename $DEVICE -stop
+sleep 10
+./stream2chromecast.py -devicename $DEVICE $IPATH/Audio/SectionOne02.mp3
+./stream2chromecast.py -devicename $DEVICE -stop
+sleep 5
+./stream2chromecast.py -devicename $DEVICE $IPATH/Audio/SectionOne03.mp3
+./stream2chromecast.py -devicename $DEVICE -stop
+sleep 3
+./stream2chromecast.py -devicename $DEVICE $IPATH/Audio/SectionOne04.mp3
+./stream2chromecast.py -devicename $DEVICE -stop
