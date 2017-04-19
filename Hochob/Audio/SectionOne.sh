01cd4d3694405e4db725682b8c12b836c70c1f44
Abraham Arce <xe1gyq@gmail.com>
2017-04-19 12:37:43 -0500

    ~Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- Hochob/Audio/SectionOne.sh
+++ Hochob/Audio/SectionOne.sh
@@ -18,14 +18,17 @@ fi
 
 ./stream2chromecast.py -devicename $DEVICE -setvol 1.0
 
-./stream2chromecast.py -devicename $DEVICE $IPATH/Audio/SectionOne01.mp3
+./stream2chromecast.py -devicename $DEVICE $IPATH/Audio/SectionOne.mp3
+./stream2chromecast.py -devicename $DEVICE -stop
+
+#./stream2chromecast.py -devicename $DEVICE $IPATH/Audio/SectionOne01.mp3
 #./stream2chromecast.py -devicename $DEVICE -stop
 #sleep 10
-./stream2chromecast.py -devicename $DEVICE $IPATH/Audio/SectionOne02.mp3
+#./stream2chromecast.py -devicename $DEVICE $IPATH/Audio/SectionOne02.mp3
 #./stream2chromecast.py -devicename $DEVICE -stop
 #sleep 5
-./stream2chromecast.py -devicename $DEVICE $IPATH/Audio/SectionOne03.mp3
+#./stream2chromecast.py -devicename $DEVICE $IPATH/Audio/SectionOne03.mp3
 #./stream2chromecast.py -devicename $DEVICE -stop
 #sleep 3
-./stream2chromecast.py -devicename $DEVICE $IPATH/Audio/SectionOne04.mp3
+#./stream2chromecast.py -devicename $DEVICE $IPATH/Audio/SectionOne04.mp3
 #./stream2chromecast.py -devicename $DEVICE -stop
