be9432815c164cfca93c518c4f77d9019535961d
Hochob Server <hochob.vc@gmail.com>
2017-05-18 21:44:29 +0000

    Update
    Signed-off-by: Hochob Server <hochob.vc@gmail.com>

---
+++ Hochob/LupeRobot/Scripts/serviceson.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+systemctl disable bluetooth-rfkill-event.service
+systemctl disable bluetooth.service
+systemctl disable clloader.service
+systemctl disable dbus-org.bluez.service
+systemctl disable mcu_fw_loader.service
+systemctl disable mdns.service
+systemctl disable mosquitto.service
+systemctl disable ofono.service
+#systemctl disable pulseaudio.service
+systemctl disable pwr-button-handler.service
+systemctl disable redis.service
+systemctl disable sketch-check.service
+systemctl disable sketch_reset.service
+systemctl disable wyliodrin-hypervisor.service
+systemctl disable wyliodrin-server.service
+systemctl disable xdk-daemon.service
+
+# End Of File
