05260af4d46b631cd5a9fe8d804b68ae77fbb16d
Abraham Arce <xe1gyq@gmail.com>
2017-05-18 16:23:17 -0500

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- Hochob/LupeRobot/Learning/Recognize/Recognize.sh
+++ Hochob/LupeRobot/Learning/Recognize/Recognize.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-# pacmd "set-default-source pacmd alsa_card.usb-Microsoft_Microsoft___LifeCam_Studio_TM_-02"
+pacmd "set-default-source 3"
 
 cd /home/hochob/CodeLabs/Hochob/LupeRobot/Learning/Recognize
 python3 SrGoogleEs.py
