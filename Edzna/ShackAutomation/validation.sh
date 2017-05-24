25ee2dd51fa18027e517555d480f4c6cdde454ba
Abraham Arce <xe1gyq@gmail.com>
2017-05-24 17:29:44 -0500

    Create validation.sh

---
+++ Edzna/ShackAutomation/validation.sh
@@ -0,0 +1,14 @@
+mosquitto_pub -h test.mosquitto.org -p 1883 -t edzna/sa/room/60 -m 15
+mosquitto_pub -h test.mosquitto.org -p 1883 -t edzna/sa/room/temperature -m 27
+mosquitto_pub -h test.mosquitto.org -p 1883 -t edzna/sa/room/humidity -m 30
+
+mosquitto_pub -h test.mosquitto.org -p 1883 -t edzna/sa/movement/motion -m 1
+mosquitto_pub -h test.mosquitto.org -p 1883 -t edzna/sa/movement/motion -m 0
+mosquitto_pub -h test.mosquitto.org -p 1883 -t edzna/sa/movement/window -m 1
+mosquitto_pub -h test.mosquitto.org -p 1883 -t edzna/sa/movement/window -m 0
+mosquitto_pub -h test.mosquitto.org -p 1883 -t edzna/sa/station/occupancy -m 1
+mosquitto_pub -h test.mosquitto.org -p 1883 -t edzna/sa/station/occupancy -m 0
+mosquitto_pub -h test.mosquitto.org -p 1883 -t edzna/sa/station/power -m 1
+mosquitto_pub -h test.mosquitto.org -p 1883 -t edzna/sa/station/power -m 0
+mosquitto_pub -h test.mosquitto.org -p 1883 -t edzna/sa/station/sound -m 1
+mosquitto_pub -h test.mosquitto.org -p 1883 -t edzna/sa/station/sound -m 0
