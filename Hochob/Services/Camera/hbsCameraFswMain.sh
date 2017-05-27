8891c188f8a4120c0d0eed0a13f7b51adcc67e3a
Abraham Arce <xe1gyq@gmail.com>
2017-05-27 15:54:57 -0500

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- Hochob/Services/Camera/hbsCameraFswMain.sh
+++ Hochob/Services/Camera/hbsCameraFswMain.sh
@@ -11,8 +11,8 @@ set -x
 export HBS_CAMERA_FSW_ROOT=`pwd`
 export HBS_CAMERA_FSW_BINARY=fswebcam
 export HBS_CAMERA_FSW_ARGUMENTS="-r 1280x720 -s brightness=65% -s Contrast=50% -s Gamma=100% --jpeg 100 --no-banner"
-export HBS_CAMERA_FSW_DIRECTORY=$HOCHOB_MEDIA_IMAGE_VISITORS/Fsw/
-export HBS_CAMERA_FSW_IMAGE=$HBS_CAMERA_FSW_DIRECTORY/hbsFswImage.jpg
+export HBS_CAMERA_FSW_REPOSITORY=$HOCHOB_MEDIA_IMAGE_VISITORS_FSWEBCAM
+export HBS_CAMERA_FSW_IMAGE=$HOCHOB_MEDIA_IMAGE_VISITORS_FSWEBCAM_IMAGE
 
 # =============================================================================
 # Functions
---
+++ Hochob/Services/Sound/SpeechSynthesizer/hbsSoundSsEspeakMain.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+set -x
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. ~/CodeLabs/Hochob/Main.sh
+
+export HBS_SOUND_SS_ESPEAK_ROOT=`pwd`
+export HBS_SOUND_SS_ESPEAK_BINARY=espeak
+export HBS_SOUND_SS_ESPEAK_ARGUMENTS="--stdout"
+export HBS_SOUND_SS_ESPEAK_LANGUAGE="-ven+m5"
+export HBS_SOUND_SS_ESPEAK_INPUT="No input has been provided"
+export HBS_SOUND_SS_ESPEAK_DIRECTORY=$HOCHOB_MEDIA_SOUND_ESPEAK
+export HBS_SOUND_SS_ESPEAK_OUTPUT=$HOCHOB_MEDIA_SOUND_ESPEAK_OUTPUT
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+LOCAL_PLAY="$1"
+LOCAL_LANGUAGE="$2"
+LOCAL_TEXT="$3"
+
+if [ "$LOCAL_LANGUAGE" = "english" ]; then
+    export HBS_SOUND_SS_ESPEAK_LANGUAGE="-ven+m5"
+elif [ "$LOCAL_LANGUAGE" = "spanish" ]; then
+    export HBS_SOUND_SS_ESPEAK_LANGUAGE="-ves-la+m5"
+fi
+
+export HBS_SOUND_SS_ESPEAK_INPUT="$LOCAL_TEXT"
+
+if [ "$LOCAL_PLAY" = "on" ]; then
+   $HBS_SOUND_SS_ESPEAK_BINARY $HBS_SOUND_SS_ESPEAK_LANGUAGE $HBS_SOUND_SS_ESPEAK_ARGUMENTS "$HBS_SOUND_SS_ESPEAK_INPUT" | tee $HOCHOB_MEDIA_SOUND_ESPEAK_OUTPUT | aplay -
+elif [ "$LOCAL_PLAY" = "off" ]; then
+   $HBS_SOUND_SS_ESPEAK_BINARY -w $HBS_SOUND_SS_ESPEAK_OUTPUT $HBS_SOUND_SS_ESPEAK_LANGUAGE "$HBS_SOUND_SS_ESPEAK_INPUT"
+fi
+
+# End of File
