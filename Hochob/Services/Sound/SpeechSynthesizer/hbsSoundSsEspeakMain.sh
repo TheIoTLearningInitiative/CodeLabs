c529e6ad0da492b540033eb887dd5ad1c8cf58f4
Abraham Arce <xe1gyq@gmail.com>
2017-05-29 20:32:59 -0500

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- Hochob/Services/Sound/SpeechSynthesizer/hbsSoundSsEspeakMain.sh
+++ Hochob/Services/Sound/SpeechSynthesizer/hbsSoundSsEspeakMain.sh
@@ -33,18 +33,25 @@ export PATH=$PATH:$HBS_SOUND_SS_ESPEAK_ROOT
 # Main
 # =============================================================================
 
-if [ "$LOCAL_LANGUAGE" = "english" ]; then
-    export HBS_SOUND_SS_ESPEAK_LANGUAGE="-ven+m5"
-elif [ "$LOCAL_LANGUAGE" = "spanish" ]; then
-    export HBS_SOUND_SS_ESPEAK_LANGUAGE="-ves-la+m5"
-fi
 
-export HBS_SOUND_SS_ESPEAK_INPUT="$LOCAL_TEXT"
+if [ $# -eq 3 ]
+then
+    if [ "$LOCAL_LANGUAGE" = "english" ]; then
+        export HBS_SOUND_SS_ESPEAK_LANGUAGE="-ven+m5"
+    elif [ "$LOCAL_LANGUAGE" = "spanish" ]; then
+        export HBS_SOUND_SS_ESPEAK_LANGUAGE="-ves-la+m5"
+    fi
 
-if [ "$LOCAL_PLAY" = "on" ]; then
-   $HBS_SOUND_SS_ESPEAK_BINARY $HBS_SOUND_SS_ESPEAK_LANGUAGE $HBS_SOUND_SS_ESPEAK_ARGUMENTS "$HBS_SOUND_SS_ESPEAK_INPUT" | tee $HOCHOB_MEDIA_SOUND_ESPEAK_OUTPUT | aplay -
-elif [ "$LOCAL_PLAY" = "off" ]; then
-   $HBS_SOUND_SS_ESPEAK_BINARY -w $HBS_SOUND_SS_ESPEAK_OUTPUT $HBS_SOUND_SS_ESPEAK_LANGUAGE "$HBS_SOUND_SS_ESPEAK_INPUT"
+    export HBS_SOUND_SS_ESPEAK_INPUT="$LOCAL_TEXT"
+
+    if [ "$LOCAL_PLAY" = "on" ]; then
+       $HBS_SOUND_SS_ESPEAK_BINARY $HBS_SOUND_SS_ESPEAK_LANGUAGE $HBS_SOUND_SS_ESPEAK_ARGUMENTS "$HBS_SOUND_SS_ESPEAK_INPUT" | tee $HOCHOB_MEDIA_SOUND_ESPEAK_OUTPUT | aplay -
+    elif [ "$LOCAL_PLAY" = "off" ]; then
+       $HBS_SOUND_SS_ESPEAK_BINARY -w $HBS_SOUND_SS_ESPEAK_OUTPUT $HBS_SOUND_SS_ESPEAK_LANGUAGE "$HBS_SOUND_SS_ESPEAK_INPUT"
+    fi
+else
+    echo "Invalid number of arguments, see Documentation"
+    exit 1
 fi
 
 # End of File
