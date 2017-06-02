8fc726268e31fbcc27c9b5e34cdc192fbd40b6be
Abraham Arce <xe1gyq@gmail.com>
2017-06-02 15:10:14 -0500

    Update Setup.sh

--- Hochob/Setup.sh
+++ Hochob/Setup.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Setup
 sudo apt-get install git
+
+# Automation
+sudo apt-get install python3-setuptools
+sudo apt-get install python3-pip
+sudo pip3 install --upgrade pip
+sudo pip3 install homeassistant
 
 # End of File
