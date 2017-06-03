fe087bb7c1ce92a71b55d83aa2117cf837466ee4
Abraham Arce <xe1gyq@gmail.com>
2017-06-03 09:13:26 -0500

    Update Aguascalientes.sh

--- Hochob/Applications/NorthernMayaLowlands/Aguascalientes.sh
+++ Hochob/Applications/NorthernMayaLowlands/Aguascalientes.sh
@@ -23,6 +23,7 @@ export AGUASCALIENTES_DIRECTORY=$(dirname "$AGUASCALIENTES_PATH")
 # =============================================================================
 
 Eog.sh "$CORPORATE_IMAGE/*" &
+sleep 1
 Mpg123.sh "$APPLICATIONS_SOUND/Aguascalientes.mp3"
 
 kill -- -$(ps -o pgid=$AGUASCALIENTES_PID | grep -o '[0-9]*')
