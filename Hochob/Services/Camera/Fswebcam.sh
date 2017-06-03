6ae7545a0fda74b4b0d20e02017ba0cf79721855
Abraham Arce <xe1gyq@gmail.com>
2017-06-03 01:45:56 -0500

    Update Fswebcam.sh

--- Hochob/Services/Camera/Fswebcam.sh
+++ Hochob/Services/Camera/Fswebcam.sh
@@ -8,13 +8,13 @@ set -x
 
 . ~/CodeLabs/Hochob/Main.sh
 
-export HBS_CAMERA_FSW_PID=$$
-export HBS_CAMERA_FSW_ROOT=$HOCHOB_SERVER_SERVICES_CAMERA
-export HBS_CAMERA_FSW_BINARY=fswebcam
-export HBS_CAMERA_FSW_ARGUMENTS="-r 1280x720 -s brightness=65% -s Contrast=50% -s Gamma=100% --jpeg 100 --no-banner"
-export HBS_CAMERA_FSW_IMAGE=$HOCHOB_SERVER_MEDIA_SOURCE_CAMERA_FSWEBCAM_IMAGE
+export FSWEBCAM_PID=$$
+export FSWEBCAM_ROOT=$HOCHOB_SERVER_SERVICES_CAMERA
+export FSWEBCAM_BINARY=fswebcam
+export FSWEBCAM_ARGUMENTS="-r 1280x720 -s brightness=65% -s Contrast=50% -s Gamma=100% --jpeg 100 --no-banner"
+export FSWEBCAM_IMAGE=$HOCHOB_SERVER_MEDIA_SOURCE_CAMERA_FSWEBCAM_IMAGE
 
-export PATH=$PATH:$HOCHOB_SERVER_SERVICES_CAMERA
+export PATH=$PATH:$FSWEBCAM_ROOT
 
 # =============================================================================
 # Functions
@@ -26,6 +26,6 @@ export PATH=$PATH:$HOCHOB_SERVER_SERVICES_CAMERA
 # Main
 # =============================================================================
 
-$HBS_CAMERA_FSW_BINARY $HBS_CAMERA_FSW_ARGUMENTS $HBS_CAMERA_FSW_IMAGE
+$FSWEBCAM_BINARY $FSWEBCAM_ARGUMENTS $FSWEBCAM_IMAGE
 
 # End of File
