32f938d47330f7b7a6b382637f241a0c9c1e02e1
Abraham Arce <xe1gyq@gmail.com>
2017-06-03 08:26:06 -0500

    Update Mpg123.sh

--- Hochob/Services/Sound/Play/Mpg123.sh
+++ Hochob/Services/Sound/Play/Mpg123.sh
@@ -6,15 +6,11 @@ set -x
 # Variables
 # =============================================================================
 
-#. ~/CodeLabs/Hochob/Main.sh
-
 export MPG123_PID=$$
 export MPG123_ROOT=$HOCHOB_SERVER_SERVICES_SOUND_PLAY
 export MPG123_BINARY=mpg123
 
 LOCAL_FILE="$1"
-
-#export PATH=$PATH:$MPG123_ROOT
 
 # =============================================================================
 # Functions
@@ -28,7 +24,6 @@ LOCAL_FILE="$1"
 
 if [ $# -eq 1 ]
 then
-    killall -9 $MPG123_BINARY
     $MPG123_BINARY $LOCAL_FILE
 else
     echo "Invalid number of arguments, see Documentation"
