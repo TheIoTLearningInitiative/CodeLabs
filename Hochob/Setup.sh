15720eb4449715c70e5027aa22532c6c076d2d38
Abraham Arce <xe1gyq@gmail.com>
2017-06-02 19:49:33 -0500

    Update Setup.sh

--- Hochob/Setup.sh
+++ Hochob/Setup.sh
@@ -1,12 +1,28 @@
 #!/bin/sh
 
-# Setup
+# Server Setup
 sudo apt-get install git
 
-# Automation
+# Server Services Automation Home Assistant
 sudo apt-get install python3-setuptools
 sudo apt-get install python3-pip
 sudo pip3 install --upgrade pip
 sudo pip3 install homeassistant
+
+# Server Services Calendar
+# None
+
+# Server Services Camera
+sudo apt install fswebcam
+
+# Server Services Institution
+# None
+
+# Server Services Image
+sudo apt install eog
+
+# Server Services Stream
+sudo apt-get install ffmpeg
+sudo apt-get install libav-tools
 
 # End of File
