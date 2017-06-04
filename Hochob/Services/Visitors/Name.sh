9daf0f37522e5f592efaaf94ce768623bd634b98
Abraham Arce <xe1gyq@gmail.com>
2017-06-04 12:58:17 -0500

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ Hochob/Services/Visitors/Name.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+set -x
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. ~/CodeLabs/Hochob/Main.sh
+
+export FSWEBCAM_PID=$$
+export FSWEBCAM_BINARY=fswebcam
+export FSWEBCAM_ARGUMENTS="-r 1280x720 -s brightness=65% -s Contrast=50% -s Gamma=100% --jpeg 100 --no-banner"
+export FSWEBCAM_IMAGE=$HOCHOB_SERVER_MEDIA_SOURCE_CAMERA_FSWEBCAM_IMAGE
+
+LOCAL_IMAGES="$1"
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+IMAGE=1
+
+if [ $# -eq 1 ]
+then
+
+    while [ $IMAGE -le "$LOCAL_IMAGES" ]
+    do
+        DATE=`date +"%Y%m%d%H%M%S"`
+        NAME=`printf %04d $IMAGE`
+        $FSWEBCAM_BINARY $FSWEBCAM_ARGUMENTS $FSWEBCAM_DIRECTORY/$NAME.jpg
+        IMAGE=$(($IMAGE+1))
+        sleep 1
+    done
+
+else
+    echo "Invalid number of arguments, see Documentation"
+    exit 1
+fi
+
+
+# End of File
