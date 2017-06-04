a455d0998e37f5ed6d607aae221b2bd9957ba2e8
Abraham Arce <xe1gyq@gmail.com>
2017-06-04 09:14:34 -0500

    Update PuertoVallarta.sh

--- Hochob/Applications/CentralMayaLowlands/PuertoVallarta.sh
+++ Hochob/Applications/CentralMayaLowlands/PuertoVallarta.sh
@@ -8,9 +8,9 @@ set -x
 
 . ~/CodeLabs/Hochob/Main.sh
 
-export CAMPECHE_PID=$$
-export CAMPECHE_PATH=$(readlink -f "$0")
-export CAMPECHE_DIRECTORY=$(dirname "$CAMPECHE_PATH")
+export PUERTOVALLARTA_PID=$$
+export PUERTOVALLARTA_PATH=$(readlink -f "$0")
+export PUERTOVALLARTA_DIRECTORY=$(dirname "$CAMPECHE_PATH")
 
 # =============================================================================
 # Functions
@@ -22,10 +22,8 @@ export CAMPECHE_DIRECTORY=$(dirname "$CAMPECHE_PATH")
 # Main
 # =============================================================================
 
-Eog.sh "$CORPORATE_IMAGE/*" &
-sleep 1
-Mpg123.sh "$APPLICATIONS_SOUND/Campeche.mp3"
+Stream2Chromecast.sh "Juum" "1.0" "$AREA_CENTRAL_MAYA_LOWLANDS/PuertoVallarta.mp3"
 
-kill -- -$(ps -o pgid=$CAMPECHE_PID | grep -o '[0-9]*')
+kill -- -$(ps -o pgid=$PUERTOVALLARTA_PID | grep -o '[0-9]*')
 
 # End of File
