9b9b65de7071eab332388b7fecb359d0162b36e9
Abraham Arce <xe1gyq@gmail.com>
2017-06-04 17:10:55 -0500

    Update BajaCaliforniaSur.sh

--- Hochob/Applications/SouthernMayaHighlands/BajaCaliforniaSur.sh
+++ Hochob/Applications/SouthernMayaHighlands/BajaCaliforniaSur.sh
@@ -23,7 +23,7 @@ export BAJACALIFORNIASUR_DIRECTORY=$(dirname "$BAJACALIFORNIASUR_PATH")
 # Main
 # =============================================================================
 
-Memories.sh "300" "on" "${BAJACALIFORNIASUR_NAME}" &
+Memories.sh "280" "on" "${BAJACALIFORNIASUR_NAME}" &
 
 Vlc.sh "off" "$AREA_SOUTERN_MAYA_HIGHLANDS/BajaCaliforniaSur01.mp4" &
 (Robot.sh lupe/inicial 1) && sleep 1.25
