d9fd1578aa53d0053e68c023d412c900427035d7
Abraham Arce <xe1gyq@gmail.com>
2017-06-03 23:14:01 -0500

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ Hochob/Applications/NorthernMayaLowlands/Campeche.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -x
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. ~/CodeLabs/Hochob/Main.sh
+
+export AGUASCALIENTES_PID=$$
+export AGUASCALIENTES_PATH=$(readlink -f "$0")
+export AGUASCALIENTES_DIRECTORY=$(dirname "$AGUASCALIENTES_PATH")
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+Eog.sh "$CORPORATE_IMAGE/*" &
+sleep 1
+Mpg123.sh "$APPLICATIONS_SOUND/Aguascalientes.mp3"
+
+kill -- -$(ps -o pgid=$AGUASCALIENTES_PID | grep -o '[0-9]*')
+
+# End of File
