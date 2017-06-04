f4e97981ddd38a3ae710daeae8e9ca7e4a4d2a37
Abraham Arce <xe1gyq@gmail.com>
2017-06-04 18:34:57 -0500

    Update MemoriesAudio.sh

--- Hochob/Services/Memories/MemoriesAudio.sh
+++ Hochob/Services/Memories/MemoriesAudio.sh
@@ -10,9 +10,7 @@ set -x
 
 export MEMORIES_PID=$$
 
-LOCAL_NUMBER="$1"
-LOCAL_VIDEO="$2"
-LOCAL_IDENTIFICATION="$(echo "${3}" | tr -d '[:space:]')"
+LOCAL_IDENTIFICATION="$(echo "${1}" | tr -d '[:space:]')"
 
 LOCAL_INSTITUTION_NAME=`Name.sh`
 LOCAL_INSTITUTION_NAME="$(echo "${LOCAL_INSTITUTION_NAME}" | tr -d '[:space:]')"
@@ -31,21 +29,11 @@ LOCAL_VISITORS_INSTITUTION_NAME=$VISITORS_SOURCE/$LOCAL_INSTITUTION_NAME/$LOCAL_
 
 mkdir -p $LOCAL_VISITORS_INSTITUTION_NAME
 
-if [ $# -eq 3 ]
+if [ $# -eq 1 ]
 then
-    rm -f $FSWEBCAM_ALLFILES
-    Fswebcam.sh $LOCAL_NUMBER
-    cp -r $FSWEBCAM_ALLFILES $LOCAL_VISITORS_INSTITUTION_NAME
 
-    LOCAL_SELECTED_FILE=`find $FSWEBCAM_DIRECTORY -type f | shuf -n 1`
-    cp $LOCAL_SELECTED_FILE $MEMORIES_CAMERA/$LOCAL_INSTITUTION_NAME$LOCAL_IDENTIFICATION.jpg
+    ffmpeg -i $FFMPEG_VIDEO -i $ESPEAK_SPEECH_MP3 -codec copy -shortest $FFMPEG_VIDEO_AUDIO
 
-    if [ "$LOCAL_VIDEO" = "on" ]; then
-        Ffmpeg.sh $FSWEBCAM_DIRECTORY
-        cp -r $FFMPEG_ALLFILES $LOCAL_VISITORS_INSTITUTION_NAME
-        cp $FFMPEG_VIDEO $MEMORIES_VIDEO/$LOCAL_INSTITUTION_NAME$LOCAL_IDENTIFICATION.mp4
-    fi
-    
 else
     echo "Invalid number of arguments, see Documentation"
     exit 1
