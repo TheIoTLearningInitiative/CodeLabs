7c3a877dea046e00bd658ee898e7884db2ceac6b
Abraham Arce <xe1gyq@gmail.com>
2017-06-04 15:38:12 -0500

    Update BajaCaliforniaSur.sh

--- Hochob/Applications/SouthernMayaHighlands/BajaCaliforniaSur.sh
+++ Hochob/Applications/SouthernMayaHighlands/BajaCaliforniaSur.sh
@@ -22,20 +22,16 @@ export BAJACALIFORNIASUR_DIRECTORY=$(dirname "$BAJACALIFORNIASUR_PATH")
 # Main
 # =============================================================================
 
-Vlc.sh "off" "$AREA_SOUTERN_MAYA_HIGHLANDS/Silencio.mp4" &
-#Vlc.sh "off" "$AREA_SOUTERN_MAYA_HIGHLANDS/BajaCaliforniaSur01.mp4" &
+Vlc.sh "off" "$AREA_SOUTERN_MAYA_HIGHLANDS/BajaCaliforniaSur01.mp4" &
 (Robot.sh lupe/inicial 1) && sleep 1.25
 
-Vlc.sh "off" "$AREA_SOUTERN_MAYA_HIGHLANDS/Parte1_v3.mp4" &
-#Vlc.sh "off" "$AREA_SOUTERN_MAYA_HIGHLANDS/BajaCaliforniaSur02.mp4" &
+Vlc.sh "off" "$AREA_SOUTERN_MAYA_HIGHLANDS/BajaCaliforniaSur02.mp4" &
 sleep 6.35 && Robot.sh lupe/emocion 1 && sleep 4
 
-Vlc.sh "off" "$AREA_SOUTERN_MAYA_HIGHLANDS/Parte2_v2.mp4" &
-#Vlc.sh "off" "$AREA_SOUTERN_MAYA_HIGHLANDS/BajaCaliforniaSur03.mp4" &
+Vlc.sh "off" "$AREA_SOUTERN_MAYA_HIGHLANDS/BajaCaliforniaSur03.mp4" &
 sleep 2.7 && Robot.sh lupe/porsupuesto 1 && sleep 2.5
 
-Vlc.sh "off" "$AREA_SOUTERN_MAYA_HIGHLANDS/Parte3_v2.mp4" &
-#Vlc.sh "off" "$AREA_SOUTERN_MAYA_HIGHLANDS/BajaCaliforniaSur04.mp4" &
+Vlc.sh "off" "$AREA_SOUTERN_MAYA_HIGHLANDS/BajaCaliforniaSur04.mp4" &
 sleep 37.5 && Robot.sh lupe/cerebro 1
 sleep 2
 Robot.sh lupe/rightup 1
@@ -47,8 +43,7 @@ sleep .25
 Robot.sh lupe/leftfold 1
 sleep 2
 
-Vlc.sh "off" "$AREA_SOUTERN_MAYA_HIGHLANDS/Parte4_v2.mp4" &
-#Vlc.sh "off" "$AREA_SOUTERN_MAYA_HIGHLANDS/BajaCaliforniaSur05.mp4" &
+Vlc.sh "off" "$AREA_SOUTERN_MAYA_HIGHLANDS/BajaCaliforniaSur05.mp4" &
 sleep 25.5 && Robot.sh lupe/inteledison 1
 sleep 3
 Robot.sh lupe/rightdown 1
@@ -60,8 +55,7 @@ sleep .25
 Robot.sh lupe/leftunfold 1
 sleep 2
 
-Vlc.sh "off" "$AREA_SOUTERN_MAYA_HIGHLANDS/Parte5_v2.mp4" &
-#Vlc.sh "off" "$AREA_SOUTERN_MAYA_HIGHLANDS/BajaCaliforniaSur06.mp4" &
+Vlc.sh "off" "$AREA_SOUTERN_MAYA_HIGHLANDS/BajaCaliforniaSur06.mp4" &
 sleep 75
 
 kill -- -$(ps -o pgid=$BAJACALIFORNIASUR_PID | grep -o '[0-9]*')
