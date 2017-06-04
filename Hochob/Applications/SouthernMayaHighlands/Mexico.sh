5672b45b07645ed7c64add0f235a2edd1e4fb962
Abraham Arce <xe1gyq@gmail.com>
2017-06-04 12:17:01 -0500

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ Hochob/Applications/SouthernMayaHighlands/Mexico.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+set -x
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. ~/CodeLabs/Hochob/Main.sh
+
+export MICHOACAN_PID=$$
+export MICHOACAN_PATH=$(readlink -f "$0")
+export MICHOACAN_DIRECTORY=$(dirname "$MICHOACAN_PATH")
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+Robot.sh lupe/headright 1
+sleep .6
+Robot.sh lupe/headleft 1
+sleep .6
+
+Fswebcam.sh 5
+cp $FSWEBCAM_DIRECTORY/* $MEMORIES_CAMERA
+
+kill -- -$(ps -o pgid=$MICHOACAN_PID | grep -o '[0-9]*')
+
+# End of File
