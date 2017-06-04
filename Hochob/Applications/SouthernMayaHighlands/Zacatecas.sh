e88f517716d26198fdfcf8d1f925034606fa22a5
Abraham Arce <xe1gyq@gmail.com>
2017-06-04 18:25:43 -0500

    Update Zacatecas.sh

--- Hochob/Applications/SouthernMayaHighlands/Zacatecas.sh
+++ Hochob/Applications/SouthernMayaHighlands/Zacatecas.sh
@@ -22,15 +22,9 @@ export ZACATECAS_DIRECTORY=$(dirname "$ZACATECAS_PATH")
 # Main
 # =============================================================================
 
-rm -f $FSWEBCAM_DIRECTORY/*
-
-Espeak.sh off spanish \
-"Muchas gracias Futuros Ingenieros!.
-A nombre del Equipo de Intel, deseamos que hayan tenido una gan visita!
-Los esperamos muy pronto!"
-
-Fswebcam.sh 100
-FfmpegAudio.sh $ESPEAK_SPEECH_MP3 $FSWEBCAM_DIRECTORY
+Memories.sh "10" "on" "${BAJACALIFORNIASUR_NAME}" &
+Espeak.sh off spanish "Hola Mundo"
+#MemoriesAudio.sh $ESPEAK_SPEECH_MP3 $FSWEBCAM_DIRECTORY
 
 #kill -- -$(ps -o pgid=$ZACATECAS_PID | grep -o '[0-9]*')
 
