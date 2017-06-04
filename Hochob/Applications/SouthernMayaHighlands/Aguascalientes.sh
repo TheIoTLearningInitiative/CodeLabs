c93a647e2b9aa9e583bed6ce7a8b59068a584049
Abraham Arce <xe1gyq@gmail.com>
2017-06-04 14:03:00 -0500

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- Hochob/Applications/SouthernMayaHighlands/Aguascalientes.sh
+++ Hochob/Applications/SouthernMayaHighlands/Aguascalientes.sh
@@ -24,9 +24,9 @@ export AGUASCALIENTES_DIRECTORY=$(dirname "$AGUASCALIENTES_PATH")
 
 Fswebcam.sh 5
 Institution=`Name.sh`
-Memories.sh "$Institution" "Estacion Aguascalientes"
+Memories.sh "camera" "${Institution}" "Estacion Aguascalientes"
 Eog.sh "$CORPORATE_IMAGE/*" &
-sleep 5
+sleep 1
 Mpg123.sh "$AREA_SOUTERN_MAYA_HIGHLANDS/Aguascalientes.mp3"
 
 kill -- -$(ps -o pgid=$AGUASCALIENTES_PID | grep -o '[0-9]*')
