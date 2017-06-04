93c124749f9d99627dbe447346730287ca5878bf
Abraham Arce <xe1gyq@gmail.com>
2017-06-04 08:52:00 -0500

    Update BajaCaliforniaSur.sh

--- Hochob/Applications/SouthernMayaHighlands/BajaCaliforniaSur.sh
+++ Hochob/Applications/SouthernMayaHighlands/BajaCaliforniaSur.sh
@@ -22,19 +22,19 @@ export BAJACALIFORNIASUR_DIRECTORY=$(dirname "$BAJACALIFORNIASUR_PATH")
 # Main
 # =============================================================================
 
-Vlc.sh "off" "$APPLICATIONS_VIDEO/Silencio.mp4" &
+Vlc.sh "off" "$SOUTERN_MAYA_HIGHLANDS/Silencio.mp4" &
 #Vlc.sh "off" "$APPLICATIONS_VIDEO/BajaCaliforniaSur01.mp4" &
 (Robot.sh lupe/inicial 1) && sleep 1.25
 
-Vlc.sh "off" "$APPLICATIONS_VIDEO/Parte1_v3.mp4" &
+Vlc.sh "off" "$SOUTERN_MAYA_HIGHLANDS/Parte1_v3.mp4" &
 #Vlc.sh "off" "$APPLICATIONS_VIDEO/BajaCaliforniaSur02.mp4" &
 sleep 6.35 && Robot.sh lupe/emocion 1 && sleep 4
 
-Vlc.sh "off" "$APPLICATIONS_VIDEO/Parte2_v2.mp4" &
+Vlc.sh "off" "$SOUTERN_MAYA_HIGHLANDS/Parte2_v2.mp4" &
 #Vlc.sh "off" "$APPLICATIONS_VIDEO/BajaCaliforniaSur03.mp4" &
 sleep 2.7 && Robot.sh lupe/porsupuesto 1 && sleep 2.5
 
-Vlc.sh "off" "$APPLICATIONS_VIDEO/Parte3_v2.mp4" &
+Vlc.sh "off" "$SOUTERN_MAYA_HIGHLANDS/Parte3_v2.mp4" &
 #Vlc.sh "off" "$APPLICATIONS_VIDEO/BajaCaliforniaSur04.mp4" &
 sleep 37.5 && Robot.sh lupe/cerebro 1
 sleep 2
@@ -47,7 +47,7 @@ sleep .25
 Robot.sh lupe/leftfold 1
 sleep 2
 
-Vlc.sh "off" "$APPLICATIONS_VIDEO/Parte4_v2.mp4" &
+Vlc.sh "off" "$SOUTERN_MAYA_HIGHLANDS/Parte4_v2.mp4" &
 #Vlc.sh "off" "$APPLICATIONS_VIDEO/BajaCaliforniaSur05.mp4" &
 sleep 25.5 && Robot.sh lupe/inteledison 1
 sleep 3
@@ -60,7 +60,7 @@ sleep .25
 Robot.sh lupe/leftunfold 1
 sleep 2
 
-Vlc.sh "off" "$APPLICATIONS_VIDEO/Parte5_v2.mp4" &
+Vlc.sh "off" "$SOUTERN_MAYA_HIGHLANDS/Parte5_v2.mp4" &
 #Vlc.sh "off" "$APPLICATIONS_VIDEO/BajaCaliforniaSur06.mp4" &
 sleep 75
 
