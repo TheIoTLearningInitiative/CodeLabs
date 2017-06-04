9969f852abdb3c1c3004fe83f48eac03cb9d3d05
Abraham Arce <xe1gyq@gmail.com>
2017-06-03 23:58:25 -0500

    Update BajaCaliforniaSur.sh

--- Hochob/Applications/NorthernMayaLowlands/BajaCaliforniaSur.sh
+++ Hochob/Applications/NorthernMayaLowlands/BajaCaliforniaSur.sh
@@ -23,16 +23,19 @@ export BAJACALIFORNIASUR_DIRECTORY=$(dirname "$BAJACALIFORNIASUR_PATH")
 # =============================================================================
 
 Vlc.sh "off" "$APPLICATIONS_VIDEO/Silencio.mp4" &
+#Vlc.sh "off" "$APPLICATIONS_VIDEO/BajaCaliforniaSur01.mp4" &
 (Robot.sh lupe/inicial 1) && sleep 1.25
 
 Vlc.sh "off" "$APPLICATIONS_VIDEO/Parte1_v3.mp4" &
+#Vlc.sh "off" "$APPLICATIONS_VIDEO/BajaCaliforniaSur02.mp4" &
 sleep 6.35 && Robot.sh lupe/emocion 1 && sleep 4
 
 Vlc.sh "off" "$APPLICATIONS_VIDEO/Parte2_v2.mp4" &
+#Vlc.sh "off" "$APPLICATIONS_VIDEO/BajaCaliforniaSur03.mp4" &
 sleep 2.7 && Robot.sh lupe/porsupuesto 1 && sleep 2.5
 
 Vlc.sh "off" "$APPLICATIONS_VIDEO/Parte3_v2.mp4" &
-
+#Vlc.sh "off" "$APPLICATIONS_VIDEO/BajaCaliforniaSur04.mp4" &
 sleep 37.5 && Robot.sh lupe/cerebro 1
 sleep 2
 Robot.sh lupe/rightup 1
@@ -45,6 +48,7 @@ Robot.sh lupe/leftfold 1
 sleep 2
 
 Vlc.sh "off" "$APPLICATIONS_VIDEO/Parte4_v2.mp4" &
+#Vlc.sh "off" "$APPLICATIONS_VIDEO/BajaCaliforniaSur05.mp4" &
 sleep 25.5 && Robot.sh lupe/inteledison 1
 sleep 3
 Robot.sh lupe/rightdown 1
@@ -57,6 +61,7 @@ Robot.sh lupe/leftunfold 1
 sleep 2
 
 Vlc.sh "off" "$APPLICATIONS_VIDEO/Parte5_v2.mp4" &
+#Vlc.sh "off" "$APPLICATIONS_VIDEO/BajaCaliforniaSur06.mp4" &
 sleep 75
 
 kill -- -$(ps -o pgid=$BAJACALIFORNIASUR_PID | grep -o '[0-9]*')
