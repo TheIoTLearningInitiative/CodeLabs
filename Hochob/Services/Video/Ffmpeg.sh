bdb855901a41a20ca90b3f36ae82258f930b738f
Abraham Arce <xe1gyq@gmail.com>
2017-06-04 11:24:21 -0500

    Update Ffmpeg.sh

--- Hochob/Services/Video/Ffmpeg.sh
+++ Hochob/Services/Video/Ffmpeg.sh
@@ -6,13 +6,11 @@ set -x
 # Variables
 # =============================================================================
 
-export VLC_PID=$$
-export VLC_BINARY=cvlc
-export VLC_ARGUMENTS="--no-video-title-show --fullscreen"
-export VLC_LOOP="--loop"
+export FFMPEG_PID=$$
+export FFMPEG_BINARY=ffmpeg
+export FFMPEG_ARGUMENTS="-y -framerate 10 -vf fps=10 -pix_fmt yuv420p"
 
-LOCAL_LOOP="$1"
-LOCAL_FILE="$2"
+LOCAL_DIRECTORY="$1"
 
 # =============================================================================
 # Functions
@@ -26,12 +24,7 @@ LOCAL_FILE="$2"
 
 if [ $# -eq 2 ]
 then
-    killall -9 $VLC_BINARY
-    if [ "$LOCAL_LOOP" = "on" ]; then
-        $VLC_BINARY $VLC_ARGUMENTS $VLC_LOOP $LOCAL_FILE &
-    elif [ "$LOCAL_LOOP" = "off" ]; then
-        $VLC_BINARY $VLC_ARGUMENTS $LOCAL_FILE &
-    fi
+    $FFMPEG_BINARY -i $LOCAL_DIRECTORY/%04d.jpg $FFMPEG_ARGUMENTS $FFMPEG_VIDEO
 else
     echo "Invalid number of arguments, see Documentation"
     exit 1
