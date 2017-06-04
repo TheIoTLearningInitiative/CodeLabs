61e637f01cc825e86f47187a460e0214de204d8b
Abraham Arce <xe1gyq@gmail.com>
2017-06-03 22:40:49 -0500

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ Hochob/Services/Communications/MqttClient/Mosquitto.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -x
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. ~/CodeLabs/Hochob/Main.sh
+
+export MOSQUITTO_PID=$$
+export FSWEBCAM_BINARY=fswebcam
+export FSWEBCAM_ARGUMENTS="-r 1280x720 -s brightness=65% -s Contrast=50% -s Gamma=100% --jpeg 100 --no-banner"
+export FSWEBCAM_IMAGE=$HOCHOB_SERVER_MEDIA_SOURCE_CAMERA_FSWEBCAM_IMAGE
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+$FSWEBCAM_BINARY $FSWEBCAM_ARGUMENTS $FSWEBCAM_IMAGE
+
+# End of File
