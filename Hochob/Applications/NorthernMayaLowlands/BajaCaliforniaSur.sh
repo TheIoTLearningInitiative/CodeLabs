e72c92bfe147ec4f606ee9eab1892b585e3882b4
Abraham Arce <xe1gyq@gmail.com>
2017-06-03 23:47:42 -0500

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ Hochob/Applications/NorthernMayaLowlands/BajaCaliforniaSur.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+set -x
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. ~/CodeLabs/Hochob/Main.sh
+
+export BAJACALIFORNIANORTE_PID=$$
+export BAJACALIFORNIANORTE_PATH=$(readlink -f "$0")
+export BAJACALIFORNIANORTE_DIRECTORY=$(dirname "$BAJACALIFORNIANORTE_PATH")
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+Eog.sh "$APPLICATIONS_IMAGE/*" &
+sleep 5
+Mpg123.sh "$APPLICATIONS_SOUND/BajaCaliforniaNorte.mp3"
+Robot.sh lupe/headright 1
+sleep .6
+Robot.sh lupe/headleft 1
+sleep .6
+Robot.sh lupe/headright 1
+sleep .6
+Robot.sh lupe/headleft 1
+sleep .6
+Robot.sh lupe/headright 1
+sleep .6
+Robot.sh lupe/resetall 1
+sleep 10
+
+kill -- -$(ps -o pgid=$BAJACALIFORNIANORTE_PID | grep -o '[0-9]*')
+
+# End of File
