b6f73d04fd8eb5a9f1d9eacffafc7907d2d9a8db
Abraham Arce <xe1gyq@gmail.com>
2017-06-04 14:55:29 -0500

    Update Memories.sh

--- Hochob/Services/Memories/Memories.sh
+++ Hochob/Services/Memories/Memories.sh
@@ -10,14 +10,13 @@ set -x
 
 export MEMORIES_PID=$$
 
-LOCAL_TYPE="$1"
-LOCAL_NUMBER="$2"
-LOCAL_IDENTIFICATION="$(echo "${3}" | tr -d '[:space:]')"
+LOCAL_NUMBER="$1"
+LOCAL_IDENTIFICATION="$(echo "${2}" | tr -d '[:space:]')"
 
 LOCAL_INSTITUTION_NAME=`Name.sh`
 LOCAL_INSTITUTION_NAME="$(echo "${LOCAL_INSTITUTION_NAME}" | tr -d '[:space:]')"
 
-LOCAL_PATH=$LOCAL_INSTITUTION_NAME/$LOCAL_IDENTIFICATION
+LOCAL_MEMORY_PATH=$MEMORIES_CAMERA/$LOCAL_INSTITUTION_NAME/$LOCAL_IDENTIFICATION
 
 # =============================================================================
 # Functions
@@ -29,27 +28,11 @@ LOCAL_PATH=$LOCAL_INSTITUTION_NAME/$LOCAL_IDENTIFICATION
 # Main
 # =============================================================================
 
-if [ $# -eq 3 ]
+if [ $# -eq 2 ]
 then
-    if [ "$LOCAL_TYPE" = "camera" ]; then
-        Fswebcam.sh $LOCAL_NUMBER &
-        DIRECTORY=$MEMORIES_CAMERA/$LOCAL_PATH
-        mkdir -p $DIRECTORY
-        cp -r $FSWEBCAM_ALLFILES $DIRECTORY
-    elif [ "$LOCAL_TYPE" = "drawing" ]; then
-        DIRECTORY=$MEMORIES_DRAWING/$LOCAL_PATH
-        mkdir -p $DIRECTORY
-        cp -r $DRAWING_ALLFILES $DIRECTORY
-    elif [ "$LOCAL_TYPE" = "robot" ]; then
-        DIRECTORY=$MEMORIES_ROBOT/$LOCAL_PATH
-        mkdir -p $DIRECTORY
-        cp -r $ESPEAK_ALLFILES $DIRECTORY
-    elif [ "$LOCAL_TYPE" = "video" ]; then
-        DIRECTORY=$MEMORIES_VIDEO/$LOCAL_PATH
-        mkdir -p $DIRECTORY
-        cp -r $FFMPEG_ALLFILES $DIRECTORY
-    fi
-
+    mkdir -p $LOCAL_MEMORY_PATH
+    Fswebcam.sh $LOCAL_NUMBER
+    cp -r $FSWEBCAM_ALLFILES $LOCAL_MEMORY_PATH
 else
     echo "Invalid number of arguments, see Documentation"
     exit 1
