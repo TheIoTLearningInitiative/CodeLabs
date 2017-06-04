b4e124c17dfac8385e9a4171ef7992e2ab625e61
Abraham Arce <xe1gyq@gmail.com>
2017-06-03 23:17:02 -0500

    Update Campeche.sh

--- Hochob/Applications/NorthernMayaLowlands/Campeche.sh
+++ Hochob/Applications/NorthernMayaLowlands/Campeche.sh
@@ -8,9 +8,9 @@ set -x
 
 . ~/CodeLabs/Hochob/Main.sh
 
-export AGUASCALIENTES_PID=$$
-export AGUASCALIENTES_PATH=$(readlink -f "$0")
-export AGUASCALIENTES_DIRECTORY=$(dirname "$AGUASCALIENTES_PATH")
+export CAMPECHE_PID=$$
+export CAMPECHE_PATH=$(readlink -f "$0")
+export CAMPECHE_DIRECTORY=$(dirname "$AGUASCALIENTES_PATH")
 
 # =============================================================================
 # Functions
@@ -26,6 +26,6 @@ Eog.sh "$CORPORATE_IMAGE/*" &
 sleep 1
 Mpg123.sh "$APPLICATIONS_SOUND/Aguascalientes.mp3"
 
-kill -- -$(ps -o pgid=$AGUASCALIENTES_PID | grep -o '[0-9]*')
+kill -- -$(ps -o pgid=$CAMPECHE_PID | grep -o '[0-9]*')
 
 # End of File
