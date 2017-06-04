bc355b07d126b87aafe3d0de62e614d6ea3b43aa
Abraham Arce <xe1gyq@gmail.com>
2017-06-04 14:49:33 -0500

    Update and rename Usb.sh to GiftUsb.sh

--- Hochob/Services/Visitors/GiftUsb.sh
+++ Hochob/Services/Visitors/GiftUsb.sh
@@ -8,12 +8,10 @@ set -x
 
 . ~/CodeLabs/Hochob/Main.sh
 
-export USB_PID=$$
+export GIFTUSB_PID=$$
 
 LOCAL_INSTITUTION_NAME=`Name.sh`
 LOCAL_INSTITUTION_NAME="$(echo "${LOCAL_INSTITUTION_NAME}" | tr -d '[:space:]')"
-
-LOCAL_PATH=$LOCAL_INSTITUTION_NAME/$LOCAL_IDENTIFICATION
 
 # =============================================================================
 # Functions
@@ -25,20 +23,12 @@ LOCAL_PATH=$LOCAL_INSTITUTION_NAME/$LOCAL_IDENTIFICATION
 # Main
 # =============================================================================
 
-DIRECTORY=$VISITORS/$MEMORIES_CAMERA/$LOCAL_PATH
+DIRECTORY=$VISITORS/$LOCAL_INSTITUTION_NAME
 mkdir -p $DIRECTORY
+
 cp -r $FSWEBCAM_ALLFILES $DIRECTORY
-
-#DIRECTORY=$MEMORIES_DRAWING/$LOCAL_PATH
-#mkdir -p $DIRECTORY
 #cp -r $DRAWING_ALLFILES $DIRECTORY
-
-#DIRECTORY=$MEMORIES_ROBOT/$LOCAL_PATH
-#mkdir -p $DIRECTORY
 #cp -r $ESPEAK_ALLFILES $DIRECTORY
-
-#DIRECTORY=$MEMORIES_VIDEO/$LOCAL_PATH
-#mkdir -p $DIRECTORY
 #cp -r $FFMPEG_ALLFILES $DIRECTORY
 
 # End of File
