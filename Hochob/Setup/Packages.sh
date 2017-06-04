556b0c352efc0be32f8d10cd3dc5f6a12cd3e1ad
Abraham Arce <xe1gyq@gmail.com>
2017-06-03 22:18:18 -0500

    Update Packages.sh

--- Hochob/Setup/Packages.sh
+++ Hochob/Setup/Packages.sh
@@ -33,6 +33,9 @@ sudo apt install eog
 # Hochob Server Services Motion
 # None
 
+# Hochob Server Services Robot
+# None
+
 # Hochob Server Services Sound Speech Synthetizer
 sudo apt install espeak tee
 
