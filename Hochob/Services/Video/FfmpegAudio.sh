a594051674f9b95be3f3a844fbc6f212077f9d7d
Abraham Arce <xe1gyq@gmail.com>
2017-06-04 11:46:15 -0500

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ Hochob/Services/Video/FfmpegAudio.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+set -x
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. ~/CodeLabs/Hochob/Main.sh
+
+export FFMPEG_PID=$$
+export FFMPEG_BINARY=ffmpeg
+export FFMPEG_ARGUMENTS="-y -s 1920x1080 -framerate 10 -vf fps=10 -pix_fmt yuv420p"
+
+LOCAL_DIRECTORY="$1"
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+if [ $# -eq 1 ]
+then
+    $FFMPEG_BINARY -i "$LOCAL_DIRECTORY/"%04d.jpg $FFMPEG_ARGUMENTS $FFMPEG_VIDEO
+else
+    echo "Invalid number of arguments, see Documentation"
+    exit 1
+fi
+
+# End of File
