eb5324359bdfff94e8d23bebf618b40ae5a8afb2
Abraham Arce <xe1gyq@gmail.com>
2017-06-04 10:43:49 -0500

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- Hochob/Services/Camera/Fswebcam.sh
+++ Hochob/Services/Camera/Fswebcam.sh
@@ -13,6 +13,8 @@ export FSWEBCAM_BINARY=fswebcam
 export FSWEBCAM_ARGUMENTS="-r 1280x720 -s brightness=65% -s Contrast=50% -s Gamma=100% --jpeg 100 --no-banner"
 export FSWEBCAM_IMAGE=$HOCHOB_SERVER_MEDIA_SOURCE_CAMERA_FSWEBCAM_IMAGE
 
+LOCAL_IMAGES="$1"
+
 # =============================================================================
 # Functions
 # =============================================================================
@@ -23,6 +25,22 @@ export FSWEBCAM_IMAGE=$HOCHOB_SERVER_MEDIA_SOURCE_CAMERA_FSWEBCAM_IMAGE
 # Main
 # =============================================================================
 
-$FSWEBCAM_BINARY $FSWEBCAM_ARGUMENTS $FSWEBCAM_IMAGE
+IMAGE=1
+
+if [ $# -eq 1 ]
+then
+
+    while [ $IMAGE -le "$LOCAL_IMAGES" ]
+    do
+        DATE=`date +"%Y%m%d%H%M%S"`
+        IMAGE=$(($IMAGE+1))
+        $FSWEBCAM_BINARY $FSWEBCAM_ARGUMENTS $FSWEBCAM_DIRECTORY/$DATE.jpg
+    done
+
+else
+    echo "Invalid number of arguments, see Documentation"
+    exit 1
+fi
+
 
 # End of File
