07a7521cf601e26ecce22779318e12b0bc4fee8d
Abraham Arce <xe1gyq@gmail.com>
2017-06-04 19:18:11 -0500

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ Hochob/Applications/NorthernMayaLowlands/Mapimi.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -x
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. ~/CodeLabs/Hochob/Main.sh
+
+export ZACATECAS_PID=$$
+export ZACATECAS_NAME="Zacatecas Application"
+export ZACATECAS_PATH=$(readlink -f "$0")
+export ZACATECAS_DIRECTORY=$(dirname "$ZACATECAS_PATH")
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+Memories.sh "20" "on" "${ZACATECAS_NAME}"
+Espeak.sh off spanish "Nel Pastel"
+MemoriesAudio.sh "${ZACATECAS_NAME}"
+
+#kill -- -$(ps -o pgid=$ZACATECAS_PID | grep -o '[0-9]*')
+
+# End of File
