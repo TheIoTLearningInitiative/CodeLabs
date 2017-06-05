061bedf604174e61a4365131e2719d87751aeea0
Abraham Arce <xe1gyq@gmail.com>
2017-06-04 22:51:22 -0500

    Update Huatulco.sh

--- Hochob/Applications/CentralMayaLowlands/Huatulco.sh
+++ Hochob/Applications/CentralMayaLowlands/Huatulco.sh
@@ -22,7 +22,12 @@ export HUATULCO_DIRECTORY=$(dirname "$HUATULCO_PATH")
 # Main
 # =============================================================================
 
-Stream2Chromecast.sh "Triki" "1.0" "$AREA_CENTRAL_MAYA_LOWLANDS/Huatulco.mp3"
+LOCAL_LANGUAGE=`Language.sh`
+if [ "$LOCAL_LANGUAGE" = "english" ]; then
+    Stream2Chromecast.sh "Triki" "1.0" "$AREA_CENTRAL_MAYA_LOWLANDS/HuatulcoEnglish.mp3"
+elif [ "$LOCAL_LANGUAGE" = "spanish" ]; then
+    Stream2Chromecast.sh "Triki" "1.0" "$AREA_CENTRAL_MAYA_LOWLANDS/Huatulco.mp3"
+fi
 
 kill -- -$(ps -o pgid=$HUATULCO_PID | grep -o '[0-9]*')
 
