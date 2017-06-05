211eb41f60223d3ad2d875b8a69769b49bd632c2
Abraham Arce <xe1gyq@gmail.com>
2017-06-04 23:02:02 -0500

    Update Veracruz.sh

--- Hochob/Applications/CentralMayaLowlands/Veracruz.sh
+++ Hochob/Applications/CentralMayaLowlands/Veracruz.sh
@@ -22,10 +22,18 @@ export VERACRUZ_DIRECTORY=$(dirname "$VERACRUZ_PATH")
 # Main
 # =============================================================================
 
-Espeak.sh off spanish \
-"Futuros Ingenieros! Bienvenidos a esta primer sala, le llamamos: El Inicio. \
-Aqui recordaras a los Mayas, la historia de Intel y de este Centro de Disenio. \
-Que disfrutes tu recorrido!"
+LOCAL_LANGUAGE=`Language.sh`
+if [ "$LOCAL_LANGUAGE" = "english" ]; then
+    Espeak.sh off spanish \
+    "Future Engineers! Welcome to this first hall! We called it: The Beginning. \
+    Here we will remember the Mayans, the history of Intel y this Design Center. \
+    Enjoy your tour!"
+else
+    Espeak.sh off spanish \
+    "Futuros Ingenieros! Bienvenidos a esta primer sala, le llamamos: El Inicio. \
+    Aqui recordaras a los Mayas, la historia de Intel y de este Centro de Disenio. \
+    Que disfrutes tu recorrido!"
+fi
 
 Stream2Chromecast.sh "Juum" "1.0" "$ESPEAK_SPEECH_MP3"
 
