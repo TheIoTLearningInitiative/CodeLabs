35ccbf8ea18e08b91553d22e6a17359deca27e50
Abraham Arce <xe1gyq@gmail.com>
2017-06-04 22:58:22 -0500

    Update PuertoVallarta.sh

--- Hochob/Applications/CentralMayaLowlands/PuertoVallarta.sh
+++ Hochob/Applications/CentralMayaLowlands/PuertoVallarta.sh
@@ -25,7 +25,7 @@ export PUERTOVALLARTA_DIRECTORY=$(dirname "$CAMPECHE_PATH")
 LOCAL_LANGUAGE=`Language.sh`
 if [ "$LOCAL_LANGUAGE" = "english" ]; then
     Stream2Chromecast.sh "Juum" "1.0" "$AREA_CENTRAL_MAYA_LOWLANDS/PuertoVallartaEnglish.mp3"
-else:
+else
     Stream2Chromecast.sh "Juum" "1.0" "$AREA_CENTRAL_MAYA_LOWLANDS/PuertoVallarta.mp3"
 fi
 
