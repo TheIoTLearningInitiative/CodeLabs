3480da61096eed37738c00a2fb78ad8c2d92b5de
Abraham Arce <xe1gyq@gmail.com>
2017-06-04 19:03:09 -0500

    Update Veracruz.sh

--- Hochob/Applications/CentralMayaLowlands/Veracruz.sh
+++ Hochob/Applications/CentralMayaLowlands/Veracruz.sh
@@ -22,7 +22,11 @@ export VERACRUZ_DIRECTORY=$(dirname "$VERACRUZ_PATH")
 # Main
 # =============================================================================
 
-Espeak.sh off spanish "1 2 3 4 5 6 7 8 9"
+Espeak.sh off spanish \
+"Futuros Ingenieros! Bienvenidos a esta primer sala, le llamamos:" \
+"El Inicio"
+"Aqui recordaras a los Mayas, la historia de Intel e Intel Centro de Disenio en Guadalajara"
+
 Stream2Chromecast.sh "Juum" "1.0" "$ESPEAK_SPEECH_MP3"
 
 kill -- -$(ps -o pgid=$VERACRUZ_PID | grep -o '[0-9]*')
