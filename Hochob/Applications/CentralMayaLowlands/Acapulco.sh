2cba69079740adb3ff648dbf48d274153c65e64b
Abraham Arce <xe1gyq@gmail.com>
2017-06-04 22:59:01 -0500

    Update Acapulco.sh

--- Hochob/Applications/CentralMayaLowlands/Acapulco.sh
+++ Hochob/Applications/CentralMayaLowlands/Acapulco.sh
@@ -24,11 +24,9 @@ export ACAPULCO_DIRECTORY=$(dirname "$ACAPULCO_PATH")
 
 LOCAL_LANGUAGE=`Language.sh`
 if [ "$LOCAL_LANGUAGE" = "english" ]; then
-    #Stream2Chromecast.sh "Triki" "1.0" "$AREA_CENTRAL_MAYA_LOWLANDS/AcapulcoEnglish.mp3"
-    echo "english"
+    Stream2Chromecast.sh "Triki" "1.0" "$AREA_CENTRAL_MAYA_LOWLANDS/AcapulcoEnglish.mp3"
 else
-    echo "spanish"
-    #Stream2Chromecast.sh "Triki" "1.0" "$AREA_CENTRAL_MAYA_LOWLANDS/Acapulco.mp3"
+    Stream2Chromecast.sh "Triki" "1.0" "$AREA_CENTRAL_MAYA_LOWLANDS/Acapulco.mp3"
 fi
 
 kill -- -$(ps -o pgid=$ACAPULCO_PID | grep -o '[0-9]*')
