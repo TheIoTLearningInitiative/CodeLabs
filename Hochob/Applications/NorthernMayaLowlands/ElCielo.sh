86bf14ba5a5f662f1ad36ab6fea4d08ab3c23683
Abraham Arce <xe1gyq@gmail.com>
2017-06-07 16:37:59 -0500

    Update ElCielo.sh

--- Hochob/Applications/NorthernMayaLowlands/ElCielo.sh
+++ Hochob/Applications/NorthernMayaLowlands/ElCielo.sh
@@ -24,20 +24,6 @@ export ELCIELO_DIRECTORY=$(dirname "$ELCIELO_PATH")
 
 Stream.sh "Eek'" "1.0" "$AREA_NORTHERN_MAYA_LOWLANDS/ElCielo.mp3"
 
-Eog.sh $HOCHOB_SERVER_MEDIA_SOURCE_CAMERA_FSWEBCAM &
-
-LOCAL_LANGUAGE=`Language.sh`
-if [ "$LOCAL_LANGUAGE" = "english" ]; then
-    Espeak.sh off english \
-    "To be written"
-else
-    Espeak.sh off spanish \
-    "Me da mucho gusto conocerlos y que hayan venido a Intel. \
-    Regresen pronto!"
-fi
-
-Stream.sh "Juum" "1.0" "$ESPEAK_SPEECH_MP3"
-
-#kill -- -$(ps -o pgid=$ELCIELO_PID | grep -o '[0-9]*')
+kill -- -$(ps -o pgid=$ELCIELO_PID | grep -o '[0-9]*')
 
 # End of File
