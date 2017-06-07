acec90f13df77f7f30ef19c1548a56eeed153e91
Abraham Arce <xe1gyq@gmail.com>
2017-06-07 15:31:32 -0500

    Update Cancun.sh

--- Hochob/Applications/CentralMayaLowlands/Cancun.sh
+++ Hochob/Applications/CentralMayaLowlands/Cancun.sh
@@ -24,9 +24,9 @@ export CANCUN_DIRECTORY=$(dirname "$CANCUN_PATH")
 
 LOCAL_LANGUAGE=`Language.sh`
 if [ "$LOCAL_LANGUAGE" = "english" ]; then
-    Stream2Chromecast.sh "Triki" "1.0" "$AREA_CENTRAL_MAYA_LOWLANDS/CancunEnglish.mp3"
+    Stream.sh "Triki" "1.0" "$AREA_CENTRAL_MAYA_LOWLANDS/CancunEnglish.mp3"
 else
-    Stream2Chromecast.sh "Triki" "1.0" "$AREA_CENTRAL_MAYA_LOWLANDS/Cancun.mp3"
+    Stream.sh "Triki" "1.0" "$AREA_CENTRAL_MAYA_LOWLANDS/Cancun.mp3"
 fi
 
 kill -- -$(ps -o pgid=$CANCUN_PID | grep -o '[0-9]*')
