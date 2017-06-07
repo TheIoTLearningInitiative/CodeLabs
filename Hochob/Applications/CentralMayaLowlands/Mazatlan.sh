ce95a6202355c9b6dce3e6d265136d2d3296155a
Abraham Arce <xe1gyq@gmail.com>
2017-06-07 15:34:24 -0500

    Update Mazatlan.sh

--- Hochob/Applications/CentralMayaLowlands/Mazatlan.sh
+++ Hochob/Applications/CentralMayaLowlands/Mazatlan.sh
@@ -24,9 +24,9 @@ export MAZATLAN_DIRECTORY=$(dirname "$CAMPECHE_PATH")
 
 LOCAL_LANGUAGE=`Language.sh`
 if [ "$LOCAL_LANGUAGE" = "english" ]; then
-    Stream2Chromecast.sh "Juum" "1.0" "$AREA_CENTRAL_MAYA_LOWLANDS/MazatlanEnglish.mp3"
+    Stream.sh "Juum" "1.0" "$AREA_CENTRAL_MAYA_LOWLANDS/MazatlanEnglish.mp3"
 else
-    Stream2Chromecast.sh "Juum" "1.0" "$AREA_CENTRAL_MAYA_LOWLANDS/Mazatlan.mp3"
+    Stream.sh "Juum" "1.0" "$AREA_CENTRAL_MAYA_LOWLANDS/Mazatlan.mp3"
 fi
 
 kill -- -$(ps -o pgid=$MAZATLAN_PID | grep -o '[0-9]*')
