ae193a113a0876e5dafd7cebbb2a2a8b5edf143d
Abraham Arce <xe1gyq@gmail.com>
2017-06-07 16:35:57 -0500

    Update LaMichilia.sh

--- Hochob/Applications/NorthernMayaLowlands/LaMichilia.sh
+++ Hochob/Applications/NorthernMayaLowlands/LaMichilia.sh
@@ -22,7 +22,20 @@ export LAMICHILIA_DIRECTORY=$(dirname "$LAMICHILIA_PATH")
 # Main
 # =============================================================================
 
-Stream.sh "Eek'" "1.0" "$AREA_NORTHERN_MAYA_LOWLANDS/LaMichilia.mp3"
+#Stream.sh "Eek'" "1.0" "$AREA_NORTHERN_MAYA_LOWLANDS/LaMichilia.mp3"
+
+LOCAL_LANGUAGE=`Language.sh`
+if [ "$LOCAL_LANGUAGE" = "english" ]; then
+    Espeak.sh off english \
+    "To be written"
+else
+    Espeak.sh off spanish \
+    "Intel no solo se queda en la tierra, es tal el alcance del Microprocesador que puede \
+     estar en el universo, por ejemplo, ha trabajado con la NASA para crear cohetes y vehiculos \
+     no tripulados. Mira el siguiente video."
+fi
+
+Stream.sh "Eek'" "1.0" "$ESPEAK_SPEECH_MP3"
 
 kill -- -$(ps -o pgid=$LAMICHILIA_PID | grep -o '[0-9]*')
 
