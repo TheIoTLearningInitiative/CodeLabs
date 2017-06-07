c5aacc56ed352c2b9883307bc3c87a929fbafcad
Abraham Arce <xe1gyq@gmail.com>
2017-06-07 15:14:34 -0500

    Update PocChuc.sh

--- Hochob/Applications/RobotMayaLands/PocChuc.sh
+++ Hochob/Applications/RobotMayaLands/PocChuc.sh
@@ -8,9 +8,9 @@ set -x
 
 . ~/CodeLabs/Hochob/Main.sh
 
-export VERACRUZ_PID=$$
-export VERACRUZ_PATH=$(readlink -f "$0")
-export VERACRUZ_DIRECTORY=$(dirname "$VERACRUZ_PATH")
+export POCCHUC_PID=$$
+export POCCHUC_PATH=$(readlink -f "$0")
+export POCCHUC_DIRECTORY=$(dirname "$POCCHUC_PATH")
 
 # =============================================================================
 # Functions
@@ -26,17 +26,17 @@ LOCAL_LANGUAGE=`Language.sh`
 if [ "$LOCAL_LANGUAGE" = "english" ]; then
     Espeak.sh off english \
     "Future Engineers! Welcome to this first hall! We called it: The Beginning. \
-    Here we will remember the Mayans, the history of Intel y this Design Center. \
+    Here we will remember the Mayans, the history of Intel y this Intel Guadalajara Design Center. \
     Enjoy your tour!"
 else
     Espeak.sh off spanish \
     "Futuros Ingenieros! Bienvenidos a esta primer sala, le llamamos: El Inicio. \
-    Aqui recordaras a los Mayas, la historia de Intel y de este Centro de Disenio. \
-    Que disfrutes tu recorrido!"
+    Aqui recordaras a los Mayas, la historia de Intel y de este Centro de Disenio de Intel en Guadalajara. \
+    Que disfruten tu recorrido!"
 fi
 
 Stream2Chromecast.sh "Juum" "1.0" "$ESPEAK_SPEECH_MP3"
 
-kill -- -$(ps -o pgid=$VERACRUZ_PID | grep -o '[0-9]*')
+kill -- -$(ps -o pgid=$PCOCHUC_PID | grep -o '[0-9]*')
 
 # End of File
