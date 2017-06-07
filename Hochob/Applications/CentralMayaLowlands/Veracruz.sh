4fca057db5149e70dac4d860e50c3b45c40bd1c7
Abraham Arce <xe1gyq@gmail.com>
2017-06-07 15:15:00 -0500

    Update Veracruz.sh

--- Hochob/Applications/CentralMayaLowlands/Veracruz.sh
+++ Hochob/Applications/CentralMayaLowlands/Veracruz.sh
@@ -26,13 +26,13 @@ LOCAL_LANGUAGE=`Language.sh`
 if [ "$LOCAL_LANGUAGE" = "english" ]; then
     Espeak.sh off english \
     "Future Engineers! Welcome to this first hall! We called it: The Beginning. \
-    Here we will remember the Mayans, the history of Intel y this Design Center. \
+    Here we will remember the Mayans, the history of Intel y this Intel Guadalajara Design Center. \
     Enjoy your tour!"
 else
     Espeak.sh off spanish \
     "Futuros Ingenieros! Bienvenidos a esta primer sala, le llamamos: El Inicio. \
-    Aqui recordaras a los Mayas, la historia de Intel y de este Centro de Disenio. \
-    Que disfrutes tu recorrido!"
+    Aqui recordaras a los Mayas, la historia de Intel y de este Centro de Disenio de Intel en Guadalajara. \
+    Que disfruten tu recorrido!"
 fi
 
 Stream2Chromecast.sh "Juum" "1.0" "$ESPEAK_SPEECH_MP3"
