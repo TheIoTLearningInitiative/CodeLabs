968405b59267808e10a920fbe2f8a02e751bac5f
Abraham Arce <xe1gyq@gmail.com>
2017-06-07 16:39:50 -0500

    Update Mapimi.sh

--- Hochob/Applications/NorthernMayaLowlands/Mapimi.sh
+++ Hochob/Applications/NorthernMayaLowlands/Mapimi.sh
@@ -23,11 +23,25 @@ export MAPIMI_DIRECTORY=$(dirname "$ZACATECAS_PATH")
 # Main
 # =============================================================================
 
-Memories.sh "40" "on" "${MAPIMI_NAME}"
-Espeak.sh off spanish "Nel Pastel Simona La Mona"
-MemoriesAudio.sh "${MAPIMI_NAME}"
+#Memories.sh "40" "on" "${MAPIMI_NAME}"
+#Espeak.sh off spanish "Nel Pastel Simona La Mona"
+#MemoriesAudio.sh "${MAPIMI_NAME}"
 
-Stream.sh "Eek'" "1.0" "$FFMPEG_VIDEO_AUDIO"
+#Stream.sh "Eek'" "1.0" "$FFMPEG_VIDEO_AUDIO"
+
+Eog.sh $HOCHOB_SERVER_MEDIA_SOURCE_CAMERA_FSWEBCAM &
+
+LOCAL_LANGUAGE=`Language.sh`
+if [ "$LOCAL_LANGUAGE" = "english" ]; then
+    Espeak.sh off english \
+    "To be written"
+else
+    Espeak.sh off spanish \
+    "Me da mucho gusto conocerlos y que hayan venido a Intel. \
+    Regresen pronto!"
+fi
+
+Stream.sh "Eek'" "1.0" "$ESPEAK_SPEECH_MP3"
 
 kill -- -$(ps -o pgid=$MAPIMI_PID | grep -o '[0-9]*')
 
