0a9be6ae51cbcd897accb03a8b32e2c6f4c79a50
Abraham Arce <xe1gyq@gmail.com>
2017-06-07 16:24:53 -0500

    Update ElCielo.sh

--- Hochob/Applications/NorthernMayaLowlands/ElCielo.sh
+++ Hochob/Applications/NorthernMayaLowlands/ElCielo.sh
@@ -22,7 +22,19 @@ export ELCIELO_DIRECTORY=$(dirname "$ELCIELO_PATH")
 # Main
 # =============================================================================
 
-Stream.sh "Eek'" "1.0" "$AREA_NORTHERN_MAYA_LOWLANDS/ElCielo.mp3"
+#Stream.sh "Eek'" "1.0" "$AREA_NORTHERN_MAYA_LOWLANDS/ElCielo.mp3"
+
+LOCAL_LANGUAGE=`Language.sh`
+if [ "$LOCAL_LANGUAGE" = "english" ]; then
+    Espeak.sh off english \
+    "To be written"
+else
+    Espeak.sh off spanish \
+    "Me da mucho gusto conocerlos y que hayan venido a Intel. \
+    Regresen pronto!"
+fi
+
+Stream.sh "Juum" "1.0" "$ESPEAK_SPEECH_MP3"
 
 kill -- -$(ps -o pgid=$ELCIELO_PID | grep -o '[0-9]*')
 
