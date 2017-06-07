a6ddd9c1b5011a2c52ef31ad5521f66939b83370
Abraham Arce <xe1gyq@gmail.com>
2017-06-07 16:03:43 -0500

    Update Mazatlan.sh

--- Hochob/Applications/CentralMayaLowlands/Mazatlan.sh
+++ Hochob/Applications/CentralMayaLowlands/Mazatlan.sh
@@ -23,6 +23,20 @@ export MAZATLAN_DIRECTORY=$(dirname "$CAMPECHE_PATH")
 # =============================================================================
 
 LOCAL_LANGUAGE=`Language.sh`
+
+if [ "$LOCAL_LANGUAGE" = "english" ]; then
+    Espeak.sh off english \
+    "Future Engineers. Welcome to this first hall! We called it: The Beginning. \
+    Here we will remember the Mayans, the history of Intel y this Intel Guadalajara Design Center. \
+    Enjoy your tour."
+else
+    Espeak.sh off spanish \
+    "Futuros Ingenieros. Ahora que saben mas acerca de Intel podemos pasar a la siguiente seccion \
+    en la que encontraremos informacion acerca del Microprocesador, nuestras Fabricas y nuestros Laboratorios. Acompanienme."
+fi
+
+Stream.sh "Juum" "1.0" "$ESPEAK_SPEECH_MP3"
+
 if [ "$LOCAL_LANGUAGE" = "english" ]; then
     Stream.sh "Juum" "1.0" "$AREA_CENTRAL_MAYA_LOWLANDS/MazatlanEnglish.mp3"
 else
