42dfcc562dc72e2b9c8747e34c6aaa6ee659327a
Abraham Arce <xe1gyq@gmail.com>
2017-06-07 15:54:50 -0500

    Update Acapulco.sh

--- Hochob/Applications/CentralMayaLowlands/Acapulco.sh
+++ Hochob/Applications/CentralMayaLowlands/Acapulco.sh
@@ -22,12 +22,41 @@ export ACAPULCO_DIRECTORY=$(dirname "$ACAPULCO_PATH")
 # Main
 # =============================================================================
 
+$Eog.sh $VISITORSCENTER_IMAGE/RP/RPP-May052017-4536.jpg
+
+LOCAL_LANGUAGE=`Language.sh`
+if [ "$LOCAL_LANGUAGE" = "english" ]; then
+    Espeak.sh off english \
+    "Future Engineers. Welcome to this first hall! We called it: The Beginning. \
+    Here we will remember the Mayans, the history of Intel y this Intel Guadalajara Design Center. \
+    Enjoy your tour."
+else
+    Espeak.sh off spanish \
+    "Futuros Ingenieros. Bienvenidos a esta primer sala, le llamamos: El Inicio. \
+    Aqui recordaras a los Mayas, la historia de Intel y de este Centro de Disenio de Intel en Guadalajara. \
+    Que disfruten tu recorrido."
+fi
+
+Stream.sh "Juum" "1.0" "$ESPEAK_SPEECH_MP3"
+
 LOCAL_LANGUAGE=`Language.sh`
 if [ "$LOCAL_LANGUAGE" = "english" ]; then
     Stream.sh "Triki" "1.0" "$AREA_CENTRAL_MAYA_LOWLANDS/AcapulcoEnglish.mp3"
 else
     Stream.sh "Triki" "1.0" "$AREA_CENTRAL_MAYA_LOWLANDS/Acapulco.mp3"
 fi
+
+if [ "$LOCAL_LANGUAGE" = "english" ]; then
+    Espeak.sh off english \
+    "Future Engineers. Welcome to this first hall! We called it: The Beginning. \
+    Here we will remember the Mayans, the history of Intel y this Intel Guadalajara Design Center. \
+    Enjoy your tour."
+else
+    Espeak.sh off spanish \
+    "Futuros Ingenieros. "
+fi
+
+wait
 
 kill -- -$(ps -o pgid=$ACAPULCO_PID | grep -o '[0-9]*')
 
