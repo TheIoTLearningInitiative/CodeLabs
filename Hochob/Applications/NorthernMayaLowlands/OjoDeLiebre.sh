3c2c683b9b7c0c9fadfab5a79776482acc053fc4
Abraham Arce Moreno <xe1gyq@gmail.com>
2017-06-09 00:16:12 -0500

    Update
    Signed-off-by: Abraham Arce Moreno <xe1gyq@gmail.com>

--- Hochob/Applications/NorthernMayaLowlands/OjoDeLiebre.sh
+++ Hochob/Applications/NorthernMayaLowlands/OjoDeLiebre.sh
@@ -25,7 +25,7 @@ export OJODELIEBRE_DIRECTORY=$(dirname "$OJODELIEBRE_PATH")
 Espeak.sh off spanish \
     "Futuros Ingenieros. Ya vimos la importancia de nuestras Fabricas y Laboratorios. Ahora entraremos a una seccion \
     que nos permite jugar y conocer de cerca la tecnología. Siganme."
-    
+
 Stream.sh "Eek'" "1.0" "$ESPEAK_SPEECH_MP3"
 
 Stream.sh "Eek'" "1.0" "$AREA_NORTHERN_MAYA_LOWLANDS/OjoDeLiebre.mp3"
