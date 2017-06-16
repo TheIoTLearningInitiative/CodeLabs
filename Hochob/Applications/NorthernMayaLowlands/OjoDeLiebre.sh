f6ba48dd8dd887b028b812de07f3b9be0f7247be
Abraham Arce <xe1gyq@gmail.com>
2017-06-16 14:34:05 -0500

    Update OjoDeLiebre.sh

--- Hochob/Applications/NorthernMayaLowlands/OjoDeLiebre.sh
+++ Hochob/Applications/NorthernMayaLowlands/OjoDeLiebre.sh
@@ -22,12 +22,6 @@ export OJODELIEBRE_DIRECTORY=$(dirname "$OJODELIEBRE_PATH")
 # Main
 # =============================================================================
 
-Espeak.sh off spanish \
-    "Futuros Ingenieros. Ya vimos la importancia de nuestras Fabricas y Laboratorios. Ahora entraremos a una seccion \
-    que nos permite jugar y conocer de cerca la tecnología. Siganme."
-
-Stream.sh "Eek'" "1.0" "$ESPEAK_SPEECH_MP3"
-
 Stream.sh "Eek'" "1.0" "$AREA_NORTHERN_MAYA_LOWLANDS/OjoDeLiebre.mp3"
 
 kill -- -$(ps -o pgid=$OJODELIEBRE_PID | grep -o '[0-9]*')
