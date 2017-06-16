86466fd2c69da8e23ba1d2666bbe7a3313da4b55
Abraham Arce <xe1gyq@gmail.com>
2017-06-16 14:34:10 -0500

    Update MontesAzules.sh

--- Hochob/Applications/NorthernMayaLowlands/MontesAzules.sh
+++ Hochob/Applications/NorthernMayaLowlands/MontesAzules.sh
@@ -22,6 +22,12 @@ export MONTESAZULES_DIRECTORY=$(dirname "$MONTESAZULES_PATH")
 # Main
 # =============================================================================
 
+Espeak.sh off spanish \
+    "Futuros Ingenieros. Ya vimos la importancia de nuestras Fabricas y Laboratorios. Ahora entraremos a una seccion \
+    que nos permite jugar y conocer de cerca la tecnología. Siganme."
+
+Stream.sh "Eek'" "1.0" "$ESPEAK_SPEECH_MP3"
+
 Stream.sh "Eek'" "1.0" "$AREA_NORTHERN_MAYA_LOWLANDS/MontesAzules.mp3"
 
 kill -- -$(ps -o pgid=$MONTESAZULES_PID | grep -o '[0-9]*')
