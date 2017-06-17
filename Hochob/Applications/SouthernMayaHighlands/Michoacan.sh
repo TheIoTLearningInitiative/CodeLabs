450d41db44c02ec022f7db71477e7af79243c30a
Abraham Arce <xe1gyq@gmail.com>
2017-06-17 12:27:43 -0500

    Update Michoacan.sh

--- Hochob/Applications/SouthernMayaHighlands/Michoacan.sh
+++ Hochob/Applications/SouthernMayaHighlands/Michoacan.sh
@@ -27,22 +27,29 @@ LogPid.sh $MICHOACAN_PID $MICHOACAN_NAME
 
 LOCAL_INSTITUTION=`Name.sh`
 
-#Robot.sh lupe/message "Bienvenidos ${LOCAL_INSTITUTION} a mi casa, su casa. \
-#                        Es un gusto conocerlos. Hace rato estaba un poco chiviado \
-#                        pero ya estoy listo para mostrarles lo interesante que es el mundo \
-#                        de las Ciencias, la Tecnología, las Ingenierías y las Matemáticas."
+#Robot.sh lupe/message \
+#   "Bienvenidos ${LOCAL_INSTITUTION} a mi casa, su casa. \
+#    Es un gusto conocerlos. Hace rato estaba un poco chiviado \
+#    pero ya estoy listo para mostrarles lo interesante que es el mundo \
+#    de las Ciencias, la Tecnología, las Ingenierías y las Matemáticas. \
+#    A nombre de cada uno de los Ingenieros que aquí trabajamos les deseamos \
+#    las mejor de las experiencias. Muchas gracias por haber venido."
 
 Espeak.sh on spanish \
     "Bienvenidos ${LOCAL_INSTITUTION} a mi casa, su casa. \
     Es un gusto conocerlos. Hace rato estaba un poco chiviado \
     pero ya estoy listo para mostrarles lo interesante que es el mundo \
-    de las Ciencias, la Tecnología, las Ingenierías y las Matemáticas."
+    de las Ciencias, la Tecnología, las Ingenierías y las Matemáticas. \
+    A nombre de cada uno de los Ingenieros que aquí trabajamos les deseamos \
+    las mejor de las experiencias. Muchas gracias por haber venido."
 
 exit 0
 
-#Robot.sh lupe/message "Tomaremos algunas fotos. Acomodense mientras yo me voy a mi lugar preferido"
+#Robot.sh lupe/message \
+#    "Ahora tomaremos una foto para el recuerdo. Acomodense mientras yo me voy a mi lugar preferido."
+    
 Espeak.sh on spanish \
-    "Tomaremos algunas fotos. Acomódense mientras yo me voy a mi lugar preferido"
+    "Ahora tomaremos una foto para el recuerdo. Acomodense mientras yo me voy a mi lugar preferido."
 
 Robot.sh lupe/headright 1
 sleep .6
@@ -62,26 +69,26 @@ sleep 3.6
 Robot.sh lupe/movestop 1
 sleep .6
 
+#Robot.sh lupe/message \
+#    "Estan listos?"
 Espeak.sh on spanish \
     "Estan listos?"
 
 sleep 3
 
+#Robot.sh lupe/message \
+#    "3, 2, 1, Sonrian!"
 Espeak.sh on spanish \
     "3, 2, 1, Sonrian!"
 
-#Robot.sh lupe/message "Estan listos?"
-#sleep 3
-#Robot.sh lupe/message "3, 2, 1, Sonrian!"
-
 Memories.sh "5" "on" "${MICHOACAN_NAME}" &
-
 wait
-
 Eog.sh "$HOCHOB_SERVER_MEDIA_SOURCE_CAMERA_FSWEBCAM_IMAGE" &
-#Robot.sh lupe/message "Listo! Tenemos nuestra foto del recuerdo. A iniciar con el recorrido futuros ingenieros."
+
+#Robot.sh lupe/message \
+#    "Listo! Tenemos nuestra foto del recuerdo. A iniciar con el recorrido futuros ingenieros."
 Espeak.sh on spanish \
-    "Listo! Tenemos nuestra foto del recuerdo. A iniciar con el recorrido ${LOCAL_INSTITUTION}"
+    "Listo! Tenemos nuestra foto del recuerdo. A iniciar con el recorrido futuros ingenieros."
 
 sleep 5
 
