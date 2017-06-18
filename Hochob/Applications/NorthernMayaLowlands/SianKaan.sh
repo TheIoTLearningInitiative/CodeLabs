3eb9c0eb8f668a5b6bad6ade7f258871cc245674
Abraham Arce <xe1gyq@gmail.com>
2017-06-18 13:26:38 -0500

    Update SianKaan.sh

--- Hochob/Applications/NorthernMayaLowlands/SianKaan.sh
+++ Hochob/Applications/NorthernMayaLowlands/SianKaan.sh
@@ -34,7 +34,7 @@ else
     "Por escribirse, ${LOCAL_INSTITUTION}"
 fi
 
-Stream.sh "Eek'" "1.0" "$ESPEAK_SPEECH_MP3"
+Stream.sh "Eek'" "1.0" "$AREA_NORTHERN_MAYA_LOWLANDS/SianKaan.mp3"
 
 kill -- -$(ps -o pgid=$SIANKAAN_PID | grep -o '[0-9]*')
 
