315570c7cb87abb118c2d9e2eafcbb0891158e52
Abraham Arce <xe1gyq@gmail.com>
2017-06-23 13:43:32 -0500

    Update

--- Hochob/Services/Communications/MqttClient/Mosquitto.sh
+++ Hochob/Services/Communications/MqttClient/Mosquitto.sh
@@ -36,13 +36,13 @@ do
     i=`expr $i + 1`
 done
 
-$MOSQUITTO_PUB_BINARY -h $MQTT_SERVER_ADDRESS -p $MQTT_SERVER_PORT -q 1 -t $LOCAL_TOPIC -m "\"${LOCAL_MESSAGE}\""
+$MOSQUITTO_PUB_BINARY -h $MQTT_SERVER_ADDRESS -p $MQTT_SERVER_PORT -q 2 -t $LOCAL_TOPIC -m "\"${LOCAL_MESSAGE}\""
 
 exit 0
 
 if [ $# -eq 2 ]
 then
-    $MOSQUITTO_PUB_BINARY -h $MQTT_SERVER_ADDRESS -p $MQTT_SERVER_PORT -q 1 -t $LOCAL_TOPIC -m $LOCAL_MESSAGE
+    $MOSQUITTO_PUB_BINARY -h $MQTT_SERVER_ADDRESS -p $MQTT_SERVER_PORT -q 2 -t $LOCAL_TOPIC -m $LOCAL_MESSAGE
 else
     echo "Invalid number of arguments, see Documentation"
     exit 1
