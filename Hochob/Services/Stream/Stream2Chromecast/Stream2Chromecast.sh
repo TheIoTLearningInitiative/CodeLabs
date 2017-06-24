8d0b55bc002afbb25c628309de37db7f98ec47e4
Abraham Arce <xe1gyq@gmail.com>
2017-06-24 13:07:30 -0500

    Update Stream2Chromecast.sh

--- Hochob/Services/Stream/Stream2Chromecast/Stream2Chromecast.sh
+++ Hochob/Services/Stream/Stream2Chromecast/Stream2Chromecast.sh
@@ -28,7 +28,7 @@ if [ $# -eq 3 ]
 then
     killall -9 $STREAM2CHROMECAST_BINARY
     $STREAM2CHROMECAST_BINARY -devicename $LOCAL_DEVICE -setvol $LOCAL_VOLUME
-    $STREAM2CHROMECAST_BINARY -devicename $LOCAL_DEVICE $STREAM2CHROMECAST_ARGUMENTS $LOCAL_FILE
+    $STREAM2CHROMECAST_BINARY -devicename $LOCAL_DEVICE $STREAM2CHROMECAST_ARGUMENTS -transcoder avconv -transcode $LOCAL_FILE
     $STREAM2CHROMECAST_BINARY -devicename $LOCAL_DEVICE -stop
 else
     echo "Invalid number of arguments, see Documentation"
