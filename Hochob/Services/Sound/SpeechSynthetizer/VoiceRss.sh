c2ceb27246f1cd4db0637344b3d1d961ade39d0f
Abraham Arce <xe1gyq@gmail.com>
2017-06-24 11:22:47 -0500

    Update VoiceRss.sh

--- Hochob/Services/Sound/SpeechSynthetizer/VoiceRss.sh
+++ Hochob/Services/Sound/SpeechSynthetizer/VoiceRss.sh
@@ -8,14 +8,14 @@ set -x
 
 #. ~/CodeLabs/Hochob/Main.sh
 
-export ESPEAK_PID=$$
-export ESPEAK_BINARY=espeak
-export ESPEAK_ARGUMENTS="--stdout"
-export ESPEAK_PLAY="-w"
-export ESPEAK_LANGUAGE="-ven+m5"
-export ESPEAK_INPUT="No input has been provided"
-export ESPEAK_DIRECTORY=$HOCHOB_SERVER_MEDIA_SOURCE_SOUND_SPEECHSYNTHETIZER_ESPEAK
-export ESPEAK_SPEECH=$HOCHOB_SERVER_MEDIA_SOURCE_SOUND_SPEECHSYNTHETIZER_ESPEAK_SPEECH
+export VOICERSS_PID=$$
+export VOICERSS_BINARY=VoiceRss.py
+export VOICERSS_ARGUMENTS="--stdout"
+export VOICERSS_PLAY="-w"
+export VOICERSS_LANGUAGE="-ven+m5"
+export VOICERSS_INPUT="No input has been provided"
+export VOICERSS_DIRECTORY=$HOCHOB_SERVER_MEDIA_SOURCE_SOUND_SPEECHSYNTHETIZER_VOICERSS
+export VOICERSS_SPEECH=$HOCHOB_SERVER_MEDIA_SOURCE_SOUND_SPEECHSYNTHETIZER_VOICERSS_SPEECH
 
 LOCAL_PLAY="$1"
 LOCAL_LANGUAGE="$2"
@@ -34,18 +34,18 @@ LOCAL_TEXT="$3"
 if [ $# -eq 3 ]
 then
     if [ "$LOCAL_LANGUAGE" = "english" ]; then
-        export ESPEAK_LANGUAGE="-ven+m5"
+        export VOICERSS_LANGUAGE="-ven+m5"
     elif [ "$LOCAL_LANGUAGE" = "spanish" ]; then
-        export ESPEAK_LANGUAGE="-v es-la -a 200"
+        export VOICERSS_LANGUAGE="-v es-la -a 200"
     fi
 
-    export ESPEAK_INPUT="$LOCAL_TEXT"
+    export VOICERSS_INPUT="$LOCAL_TEXT"
 
     if [ "$LOCAL_PLAY" = "on" ]; then
-       $ESPEAK_BINARY $ESPEAK_LANGUAGE $ESPEAK_ARGUMENTS "$ESPEAK_INPUT" | tee $ESPEAK_SPEECH | aplay -
+       $VOICERSS_BINARY $VOICERSS_LANGUAGE $VOICERSS_ARGUMENTS "$VOICERSS_INPUT" | tee $VOICERSS_SPEECH | aplay -
     elif [ "$LOCAL_PLAY" = "off" ]; then
-       $ESPEAK_BINARY $ESPEAK_PLAY $ESPEAK_SPEECH $ESPEAK_LANGUAGE "$ESPEAK_INPUT"
-       ffmpeg -y -i $ESPEAK_SPEECH $ESPEAK_SPEECH_MP3
+       $VOICERSS_BINARY $VOICERSS_PLAY $VOICERSS_SPEECH $VOICERSS_LANGUAGE "$VOICERSS_INPUT"
+       ffmpeg -y -i $VOICERSS_SPEECH $VOICERSS_SPEECH_MP3
     fi
 else
     echo "Invalid number of arguments, see Documentation"
