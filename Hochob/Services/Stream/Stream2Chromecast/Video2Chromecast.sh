c29874b83a3ea783050861b96e674e223486a234
Abraham Arce <xe1gyq@gmail.com>
2017-06-24 13:33:36 -0500

    Update Video2Chromecast.sh

--- Hochob/Services/Stream/Stream2Chromecast/Video2Chromecast.sh
+++ Hochob/Services/Stream/Stream2Chromecast/Video2Chromecast.sh
@@ -6,10 +6,10 @@ set -x
 # Variables
 # =============================================================================
 
-export CAST_PID=$$
-export STREAM2CHROMECAST_BINARY=stream2chromecast.py
-export STREAM2CHROMECAST_ARGUMENTS="-transcodebufsize 5242880"
-export STREAM2CHROMECAST_TRANSCODER="-transcoder avconv -transcode"
+export VIDEOCHROMECAST_PID=$$
+export VIDEO2CHROMECAST_BINARY=stream2chromecast.py
+export VIDEO2CHROMECAST_ARGUMENTS="-transcodebufsize 5242880"
+export VIDEO2CHROMECAST_TRANSCODER="-transcoder avconv -transcode"
 
 LOCAL_DEVICE="$1"
 LOCAL_VOLUME=0.75
@@ -28,11 +28,11 @@ LOCAL_FILE="$3"
 if [ $# -eq 3 ] 
 then
     killall -9 $STREAM2CHROMECAST_BINARY
-    $STREAM2CHROMECAST_BINARY -devicename $LOCAL_DEVICE -setvol $LOCAL_VOLUME
-    $STREAM2CHROMECAST_BINARY -devicename $LOCAL_DEVICE $STREAM2CHROMECAST_ARGUMENTS \
+    $VIDEO2CHROMECAST_BINARY -devicename $LOCAL_DEVICE -setvol $LOCAL_VOLUME
+    $VIDEO2CHROMECAST_BINARY -devicename $LOCAL_DEVICE $VIDEO2CHROMECAST_ARGUMENTS \
                               -transcodeopts '-b:v 1000k -b:a 24k' \
-                              $STREAM2CHROMECAST_TRANSCODER $LOCAL_FILE
-    $STREAM2CHROMECAST_BINARY -devicename $LOCAL_DEVICE -stop
+                              $VIDEO2CHROMECAST_TRANSCODER $LOCAL_FILE
+    $VIDEO2CHROMECAST_BINARY -devicename $LOCAL_DEVICE -stop
 else
     echo "Invalid number of arguments, see Documentation"
     exit 1
