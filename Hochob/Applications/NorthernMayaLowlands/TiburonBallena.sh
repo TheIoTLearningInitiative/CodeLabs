8c6487f07cdc2c61714b57badb41775595db62f6
Abraham Arce <xe1gyq@gmail.com>
2017-06-25 02:12:02 -0500

    Update TiburonBallena.sh

--- Hochob/Applications/NorthernMayaLowlands/TiburonBallena.sh
+++ Hochob/Applications/NorthernMayaLowlands/TiburonBallena.sh
@@ -32,11 +32,11 @@ Survey.sh spanish robot 1
 exit 0
 
 if [ "$LOCAL_LANGUAGE" = "english" ]; then
-    Northern.sh english robot "Hi Lupe, good morning"
-    Northern.sh english tt "Hi Lupe, good morning"
+    Eek.sh english robot "Hi Lupe, good morning"
+    Eek.sh english tt "Hi Lupe, good morning"
 else
-    Northern.sh spanish robot "Hola Lupe, muy buenos dias"
-    Northern.sh spanish tt "Hola Lupe, muy buenos dias"
+    Eek.sh spanish robot "Hola Lupe, muy buenos dias"
+    Eek.sh spanish tt "Hola Lupe, muy buenos dias"
     Stream.sh "Eek'" "1.0" "$AREA_NORTHERN_MAYA_LOWLANDS/TiburonBallena/LiftoffSpaceXFalcon9CRS10Mission.mp4"
 fi
 
