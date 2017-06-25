873e27d933ade8c30541c8025cae2009c5f60c48
Abraham Arce <xe1gyq@gmail.com>
2017-06-25 14:00:42 -0500

    Update BajaCaliforniaSurLocal.sh

--- Hochob/Applications/SouthernMayaHighlands/BajaCaliforniaSurLocal.sh
+++ Hochob/Applications/SouthernMayaHighlands/BajaCaliforniaSurLocal.sh
@@ -27,22 +27,22 @@ LogPid.sh $BAJACALIFORNIASUR_PID $BAJACALIFORNIASUR_NAME
 
 Memories.sh "280" "on" "${BAJACALIFORNIASUR_NAME}" &
 
-Vlc.sh "off" "$AREA_SOUTERN_MAYA_HIGHLANDS/BajaCaliforniaSur01.mp4" &
+Vlc.sh "$AREA_SOUTERN_MAYA_HIGHLANDS/BajaCaliforniaSur01.mp4" &
 Robot.sh lupe/say "Hola a todos"
 
-Vlc.sh "off" "$AREA_SOUTERN_MAYA_HIGHLANDS/BajaCaliforniaSur02.mp4" &
+Vlc.sh "$AREA_SOUTERN_MAYA_HIGHLANDS/BajaCaliforniaSur02.mp4" &
 sleep 6.35 && Robot.sh lupe/say "Perdon! Lo se!, Es que me emociono!"
 
-Vlc.sh "off" "$AREA_SOUTERN_MAYA_HIGHLANDS/BajaCaliforniaSur03.mp4" &
+Vlc.sh "$AREA_SOUTERN_MAYA_HIGHLANDS/BajaCaliforniaSur03.mp4" &
 sleep 2.7 && Robot.sh lupe/say "Por supuesto"
 
-Vlc.sh "off" "$AREA_SOUTERN_MAYA_HIGHLANDS/BajaCaliforniaSur04.mp4" &
+Vlc.sh "$AREA_SOUTERN_MAYA_HIGHLANDS/BajaCaliforniaSur04.mp4" &
 sleep 37.5 && Robot.sh lupe/say "En mi caso, mi cerebro esta aqui, en la caja azul!" 
 
-Vlc.sh "off" "$AREA_SOUTERN_MAYA_HIGHLANDS/BajaCaliforniaSur05.mp4" &
+Vlc.sh "$AREA_SOUTERN_MAYA_HIGHLANDS/BajaCaliforniaSur05.mp4" &
 sleep 25.5 && Robot.sh lupe/say "Yo funciono con la plataforma Intel Edison"
 
-Vlc.sh "off" "$AREA_SOUTERN_MAYA_HIGHLANDS/BajaCaliforniaSur06.mp4" &
+Vlc.sh "$AREA_SOUTERN_MAYA_HIGHLANDS/BajaCaliforniaSur06.mp4" &
 
 LOCAL_INSTITUTION=`Name.sh`
 SpeechSynthetizer.sh off espeak spanish \
