0a71c35578971ad6d5a6ea9555a87e8750eadd98
Abraham Arce <xe1gyq@gmail.com>
2017-06-25 02:13:36 -0500

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ Hochob/Services/Stream/StreamAudio.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+set -x
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. ~/CodeLabs/Hochob/Main.sh
+
+export EEK_PID=$$
+
+LOCAL_LANGUAGE="$1"
+LOCAL_CHARACTER="$2"
+LOCAL_TEXT="$3"
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+if [ "$LOCAL_LANGUAGE" = "english" ] && [ "$LOCAL_CHARACTER" = "robot" ]; then
+    Espeak.sh off english "${LOCAL_TEXT}"
+elif [ "$LOCAL_LANGUAGE" = "english" ] && [ "$LOCAL_CHARACTER" = "tt" ]; then
+    VoiceRss.sh off english "${LOCAL_TEXT}"
+elif [ "$LOCAL_LANGUAGE" = "spanish" ] && [ "$LOCAL_CHARACTER" = "robot" ]; then
+    Espeak.sh off spanish "${LOCAL_TEXT}"
+elif [ "$LOCAL_LANGUAGE" = "spanish" ] && [ "$LOCAL_CHARACTER" = "tt" ]; then
+    VoiceRss.sh off spanish "${LOCAL_TEXT}"
+fi
+
+Stream.sh "Eek'" "1.0" "$SPEECH_MP3"
+
+# End of File
