45d8f1e863d16ccb07142afe5609c2631784619c
Abraham Arce <xe1gyq@gmail.com>
2017-06-25 12:53:02 -0500

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ Hochob/Services/Video/FfmpegVideoAudio.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -x
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+#. ~/CodeLabs/Hochob/Main.sh
+
+export FFMPEG_PID=$$
+export FFMPEG_BINARY=ffmpeg
+export FFMPEG_ARGUMENTS="-codec copy -shortest"
+
+LOCAL_AUDIO=$1
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+if [ $# -eq 1 ]
+then
+    $FFMPEG_BINARY -y -i $FFMPEG_IMAGE -i $ESPEAK_SPEECH_MP3 -codec copy -shortest $FFMPEG_VIDEO_AUDIO
+    cp $FFMPEG_VIDEO_AUDIO $FFMPEG_VIDEO
+else
+    echo "Invalid number of arguments, see Documentation"
+    exit 1
+fi
+
+# End of File
