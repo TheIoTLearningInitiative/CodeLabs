ae00dd83832e74eb528f87c140a24f48544163d7
Abraham Arce <xe1gyq@gmail.com>
2017-06-25 00:13:15 -0500

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ Hochob/Services/Keypad/Keypad.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -x
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. ~/CodeLabs/Hochob/Main.sh
+
+export LANGUAGE_PID=$$
+
+LOCAL_COMMAND="$1"
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+if [ -z "$1" ]
+then
+    cat $HOCHOB_SERVER_CONFIGURATION_LANGUAGE
+else
+
+    test -d $HOCHOB_SERVER_MEDIA_SOURCE || mkdir $HOCHOB_SERVER_MEDIA_SOURCE
+    echo $LOCAL_COMMAND > $HOCHOB_SERVER_CONFIGURATION_LANGUAGE
+fi
+
+# End of File
