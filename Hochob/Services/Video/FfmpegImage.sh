86ca142d3b55c2f5e4f2840f2872afbc7a41d810
Abraham Arce <xe1gyq@gmail.com>
2017-06-25 13:46:00 -0500

    Update FfmpegImage.sh

--- Hochob/Services/Video/FfmpegImage.sh
+++ Hochob/Services/Video/FfmpegImage.sh
@@ -10,7 +10,8 @@ set -x
 
 export FFMPEG_PID=$$
 export FFMPEG_BINARY=ffmpeg
-export FFMPEG_ARGUMENTS="-y -s 4096x2304 -framerate 10 -vf fps=10 -pix_fmt yuv420p"
+#export FFMPEG_ARGUMENTS="-y -s 4096x2304 -framerate 10 -vf fps=10 -pix_fmt yuv420p"
+export FFMPEG_ARGUMENTS="-c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest"
 
 LOCAL_DIRECTORY="$1"
 
