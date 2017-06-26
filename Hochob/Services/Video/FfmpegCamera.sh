74f135cc417e1cb686d08420b278d857851e515e
Abraham Arce <xe1gyq@gmail.com>
2017-06-25 22:54:34 -0500

    Update FfmpegCamera.sh

--- Hochob/Services/Video/FfmpegCamera.sh
+++ Hochob/Services/Video/FfmpegCamera.sh
@@ -14,8 +14,8 @@ export FFMPEG_INPUT_DEVICE_CAMERA=/dev/video0
 export FFMPEG_INPUT_CAMERA="-i "$FFMPEG_INPUT_DEVICE_CAMERA
 export FFMPEG_INPUT_DEVICE_AUDIO="plughw:CARD=C525,DEV=0"
 export FFMPEG_INPUT_AUDIO="-f alsa -i "$FFMPEG_INPUT_DEVICE_AUDIO
-export FFMPEG_ARGUMENTS="-y -codec:v libx264 -qp 0"
-export FFMPEG_ARGUMENTS="-y"
+export FFMPEG_CAMERA_ARGUMENTS="-y -codec:v libx264 -qp 0"
+export FFMPEG_ARGUMENTS="-y -acodec libmp3lame -ab 96k"
 export FFMPEG_TIME="-t 00:00:"
 
 export LOCAL_SECONDS=$1
