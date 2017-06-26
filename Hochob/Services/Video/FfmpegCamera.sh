7adb7fd094500af1ddabca76b172b0496059a561
Abraham Arce <xe1gyq@gmail.com>
2017-06-25 22:51:52 -0500

    Update FfmpegCamera.sh

--- Hochob/Services/Video/FfmpegCamera.sh
+++ Hochob/Services/Video/FfmpegCamera.sh
@@ -10,7 +10,10 @@ set -x
 
 export FFMPEG_PID=$$
 export FFMPEG_BINARY=ffmpeg
-export FFMPEG_INPUT=/dev/video0
+export FFMPEG_INPUT_DEVICE_CAMERA=/dev/video0
+export FFMPEG_INPUT_CAMERA="-i "$FFMPEG_INPUT_DEVICE_CAMERA
+export FFMPEG_INPUT_DEVICE_AUDIO="plughw:CARD=C525,DEV=0"
+export FFMPEG_INPUT_AUDIO="-f alsa -i "$FFMPEG_INPUT_DEVICE_AUDIO
 export FFMPEG_ARGUMENTS="-y -codec:v libx264 -qp 0"
 export FFMPEG_ARGUMENTS="-y"
 export FFMPEG_TIME="-t 00:00:"
@@ -30,7 +33,7 @@ export FFMPEG_TIME="${FFMPEG_TIME}${LOCAL_SECONDS}"
 
 if [ $# -eq 1 ]
 then
-    ffmpeg -f alsa -i hw:2,0 -i $FFMPEG_INPUT $FFMPEG_TIME $FFMPEG_ARGUMENTS $FFMPEG_CAMERA
+    ffmpeg $FFMPEG_INPUT_AUDIO $FFMPEG_INPUT_CAMERA $FFMPEG_TIME $FFMPEG_ARGUMENTS $FFMPEG_CAMERA
 else
     echo "Invalid number of arguments, see Documentation"
     exit 1
