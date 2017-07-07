7228c330f66616fe80dd6e7f2a8b8a166d5904c4
Abraham Arce <xe1gyq@gmail.com>
2017-07-07 06:09:17 -0500

    Update Toyota.sh

--- Hochob/Applications/TechieMayaLands/Toyota.sh
+++ Hochob/Applications/TechieMayaLands/Toyota.sh
@@ -26,7 +26,7 @@ export TOYOTA_DIRECTORY=$(dirname "$TOYOTA_PATH")
 LogPid.sh $TOYOTA_PID $TOYOTA_NAME
 
 SpeechSynthetizer.sh on espeak spanish \
-    "Hola Mundo Uno Dos Tres Probando"
+    "Hola Mundo! Uno, Dos, Tres, Probando!"
 
 Stream.sh "Triki" "1.0" "$SPEECH_MP3"
 Stream.sh "Tran" "1.0" "$SPEECH_MP3"
