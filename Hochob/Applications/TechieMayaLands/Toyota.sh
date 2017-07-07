3b4eaca387aeb7a45f611c2a8a4889f0b980543d
Abraham Arce <xe1gyq@gmail.com>
2017-07-07 05:50:55 -0500

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ Hochob/Applications/TechieMayaLands/Toyota.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+set -x
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. ~/CodeLabs/Hochob/Main.sh
+
+export ZACATECAS_PID=$$
+export ZACATECAS_NAME="Zacatecas"
+export ZACATECAS_PATH=$(readlink -f "$0")
+export ZACATECAS_DIRECTORY=$(dirname "$ZACATECAS_PATH")
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+LogPid.sh $ZACATECAS_PID $ZACATECAS_NAME
+
+Memories.sh "5" "on" "${ZACATECAS_NAME}" &
+sleep 2
+ConvertText.sh "$FSWEBCAM_IMAGE" "Bienvenidos"
+wait
+Eog.sh "$FSWEBCAM_IMAGE" &
+
+exit 0
+
+Memories.sh "20" "on" "${ZACATECAS_NAME}"
+SpeechSynthetizer.sh off espeak spanish \
+    "Me da mucho gusto conocerlos y que hayan venido a Intel. \
+    Regresen pronto"
+MemoriesAudio.sh "${ZACATECAS_NAME}"
+
+sync
+
+# End of File
