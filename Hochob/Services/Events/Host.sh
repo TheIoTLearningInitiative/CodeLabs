e4f4cd9b5c060d0a2619274128fb6cd5d7fb001c
Abraham Arce <xe1gyq@gmail.com>
2017-07-08 11:40:18 -0500

    Update Host.sh

--- Hochob/Services/Events/Host.sh
+++ Hochob/Services/Events/Host.sh
@@ -8,16 +8,9 @@ set -x
 
 . ~/CodeLabs/Hochob/Main.sh
 
-export MEMORIES_PID=$$
-
-LOCAL_NUMBER="$1"
-LOCAL_VIDEO="$2"
-LOCAL_IDENTIFICATION="$(echo "${3}" | tr -d '[:space:]')"
+export HOST_PID=$$
 
 LOCAL_INSTITUTION_NAME=`Name.sh`
-LOCAL_INSTITUTION_NAME="$(echo "${LOCAL_INSTITUTION_NAME}" | tr -d '[:space:]')"
-
-LOCAL_VISITORS_INSTITUTION_NAME=$VISITORS_SOURCE/$LOCAL_INSTITUTION_NAME/$LOCAL_IDENTIFICATION
 
 # =============================================================================
 # Functions
@@ -29,25 +22,9 @@ LOCAL_VISITORS_INSTITUTION_NAME=$VISITORS_SOURCE/$LOCAL_INSTITUTION_NAME/$LOCAL_
 # Main
 # =============================================================================
 
-mkdir -p $LOCAL_VISITORS_INSTITUTION_NAME
-
-if [ $# -eq 3 ]
+if [ $# -eq 0 ]
 then
-    rm -f $FSWEBCAM_ALLFILES
-    Fswebcam.sh $LOCAL_NUMBER
-    cp -r $FSWEBCAM_DIRECTORY $LOCAL_VISITORS_INSTITUTION_NAME
-
-    LOCAL_SELECTED_FILE=`find $FSWEBCAM_DIRECTORY -type f | shuf -n 1`
-    cp $LOCAL_SELECTED_FILE $MEMORIES_CAMERA/$LOCAL_INSTITUTION_NAME$LOCAL_IDENTIFICATION.jpg
-    cp $LOCAL_SELECTED_FILE $FSWEBCAM_IMAGE
-    
-    if [ "$LOCAL_VIDEO" = "on" ]; then
-        FfmpegImage.sh $FSWEBCAM_DIRECTORY
-        cp -r $FFMPEG_ALLFILES $LOCAL_VISITORS_INSTITUTION_NAME
-        cp $FFMPEG_IMAGE $LOCAL_VISITORS_INSTITUTION_NAME
-        cp $FFMPEG_IMAGE $MEMORIES_VIDEO/$LOCAL_INSTITUTION_NAME$LOCAL_IDENTIFICATION.mp4
-    fi
-
+    echo 
 else
     echo "Invalid number of arguments, see Documentation"
     exit 1
