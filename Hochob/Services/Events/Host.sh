54bf65388fe9f5ab41e0edff4771edd58bc3f398
Abraham Arce <xe1gyq@gmail.com>
2017-07-08 16:10:22 -0500

    Update Host.sh

--- Hochob/Services/Events/Host.sh
+++ Hochob/Services/Events/Host.sh
@@ -10,23 +10,52 @@ set -x
 
 export HOST_PID=$$
 
+LOCAL_LANGUAGE="$1"
+
 LOCAL_INSTITUTION_NAME=`Name.sh`
+
+LOCAL_GREETING_ENGLISH[0]="Hi There."
+LOCAL_GREETING_ENGLISH[1]="Good morning."
+LOCAL_GREETING_ENGLISH[2]="Hi Friend."
+LOCAL_GREETING_ENGLISH[3]="Hey."
+LOCAL_GREETING_ENGLISH[4]="Hello There."
+
+LOCAL_GREETING_SPANISH[0]="Hola."
+LOCAL_GREETING_SPANISH[1]="Buenos dias."
+LOCAL_GREETING_SPANISH[2]="Hola Cacerola."
+LOCAL_GREETING_SPANISH[3]="Que onda."
+LOCAL_GREETING_SPANISH[4]="Espera."
 
 # =============================================================================
 # Functions
 # =============================================================================
 
-# None
+chooseGreeting()
+{
+    LANGUAGE=$1
+    rand=$[ $RANDOM % 5 ]
+    if [ "$LANGUAGE" = "english" ]; then
+        LOCAL_GREETING=${LOCAL_GREETING_ENGLISH[$rand]}
+    else
+        LOCAL_GREETING=${LOCAL_GREETING_SPANISH[$rand]}
+    fi
+}
 
 # =============================================================================
 # Main
 # =============================================================================
 
-if [ $# -eq 0 ]
-then
-    echo 
-else
-    echo "Invalid number of arguments, see Documentation"
-    exit 1
-fi
+chooseGreeting $LOCAL_LANGUAGE
+
+LOCAL_MESSAGE=${LOCAL_GREETING}
+
+Robot.sh lupe/decir \
+    ${LOCAL_MESSAGE}
+Robot.sh lupe/decir \
+    "Hola Mundo! Uno, Dos, Tres!"
+
+Robot.sh lupe/armfold 1
+sleep 2
+Robot.sh lupe/armunfold 1
+
 # End of File
