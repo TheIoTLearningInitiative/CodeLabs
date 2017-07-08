9c1482920ace6be1b74be0ffdbafeb15c30aa3de
Abraham Arce <xe1gyq@gmail.com>
2017-07-08 15:56:49 -0500

    Update WindRiver.sh

--- Hochob/Applications/TechieMayaLands/WindRiver.sh
+++ Hochob/Applications/TechieMayaLands/WindRiver.sh
@@ -13,11 +13,32 @@ export WINDRIVER_NAME="WindRiver"
 export WINDRIVER_PATH=$(readlink -f "$0")
 export WINDRIVER_DIRECTORY=$(dirname "$WINDRIVER_PATH")
 
+LOCAL_GREETING_ENGLISH[0]="Hi There."
+LOCAL_GREETING_ENGLISH[1]="Good morning."
+LOCAL_GREETING_ENGLISH[2]="Hi Friend."
+LOCAL_GREETING_ENGLISH[3]="Hey."
+LOCAL_GREETING_ENGLISH[4]="Hello There."
+
+LOCAL_GREETING_SPANISH[0]="Hola."
+LOCAL_GREETING_SPANISH[0]="Buenos dias."
+LOCAL_GREETING_SPANISH[0]="Hola Cacerola."
+LOCAL_GREETING_SPANISH[0]="Que onda."
+LOCAL_GREETING_SPANISH[0]="Espera."
+
 # =============================================================================
 # Functions
 # =============================================================================
 
-# None
+chooseGreeting()
+{
+    LANGUAGE=$1
+    rand=$[ $RANDOM % 5 ]
+    if [ "$LANGUAGE" = "english" ]; then
+        LOCAL_GREETING=${LOCAL_GREETING_ENGLISH[$rand]}
+    else
+        LOCAL_GREETING=${LOCAL_GREETING_SPANISH[$rand]}
+    fi
+}
 
 # =============================================================================
 # Main
@@ -25,7 +46,9 @@ export WINDRIVER_DIRECTORY=$(dirname "$WINDRIVER_PATH")
 
 LogPid.sh $WINDRIVER_PID $WINDRIVER_NAME
 
+LOCAL_MESSAGE=$LOCAL_GREETING
 
+Robot.sh lupe/message $LOCAL_MESSAGE
 
 Robot.sh lupe/armfold 1
 sleep 2
