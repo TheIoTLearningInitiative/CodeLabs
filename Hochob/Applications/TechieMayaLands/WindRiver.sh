291c25f1c3de96eb65d0aef7e219d05ba7477a67
Abraham Arce <xe1gyq@gmail.com>
2017-07-08 15:30:33 -0500

    Update WindRiver.sh

--- Hochob/Applications/TechieMayaLands/WindRiver.sh
+++ Hochob/Applications/TechieMayaLands/WindRiver.sh
@@ -8,10 +8,10 @@ set -x
 
 . ~/CodeLabs/Hochob/Main.sh
 
-export TOYOTA_PID=$$
-export TOYOTA_NAME="Zacatecas"
-export TOYOTA_PATH=$(readlink -f "$0")
-export TOYOTA_DIRECTORY=$(dirname "$TOYOTA_PATH")
+export WINDRIVER_PID=$$
+export WINDRIVER_NAME="WindRiver"
+export WINDRIVER_PATH=$(readlink -f "$0")
+export WINDRIVER_DIRECTORY=$(dirname "$WINDRIVER_PATH")
 
 # =============================================================================
 # Functions
@@ -23,34 +23,8 @@ export TOYOTA_DIRECTORY=$(dirname "$TOYOTA_PATH")
 # Main
 # =============================================================================
 
-LogPid.sh $TOYOTA_PID $TOYOTA_NAME
-
-SpeechSynthetizer.sh on espeak spanish \
-    "Hola Mundo! Uno, Dos, Tres, Probando!"
-
-Stream.sh "Triki" "1.0" "$SPEECH_MP3"
-echo $?
+LogPid.sh $WINDRIVER_PID $WINDRIVER_NAME
 
 Robot.sh lupe/inicial 1
-Robot.sh lupe/emocion 1
-Robot.sh lupe/porsupuesto 1
-Robot.sh lupe/cerebro 1
-Robot.sh lupe/inteledison 1
-
-Stream.sh "Tran" "1.0" "$SPEECH_MP3"
-echo $?
-Stream.sh "Juum" "1.0" "$SPEECH_MP3"
-echo $?
-Stream.sh "Eek'" "1.0" "$SPEECH_MP3"
-echo $?
-
-Memories.sh "5" "on" "${ZACATECAS_NAME}" &
-sleep 2
-ConvertText.sh "$FSWEBCAM_IMAGE" "System Health"
-wait
-Eog.sh "$FSWEBCAM_IMAGE" &
-sleep 2
-
-KillApp.sh "eog"
 
 # End of File
