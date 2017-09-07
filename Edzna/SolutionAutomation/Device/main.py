af4e027ba0b351495f71327b5acaa825fc0d4cd1
Abraham Arce <xe1gyq@gmail.com>
2017-09-06 22:50:09 -0500

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ Edzna/SolutionAutomation/Device/main.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python
+
+import paho.mqtt.client as paho
+import signal
+import sys
+import time
+
+from threading import Thread
+from upm import pyupm_grove as grove
+from upm import pyupm_biss0001 as grovemotion
+
+light = grove.GroveLight(0)
+relay = grove.GroveRelay(2)
+motion = grovemotion.BISS0001(6)
+button = grove.GroveButton(4)
+
+mqttserver = "iot.eclipse.org"
+mqttport = 1883
+
+def functionSubscribeActuatorData(mosq, obj, msg):
+    print "Subscribe Actuator Data: We received %s!" % msg.payload
+    if msg.payload == "1":
+        relay.on()
+    elif msg.payload == "0":
+        relay.off()
+
+def functionSubscribeActuator():
+    mqttclient = paho.Client()
+    mqttclient.on_message = functionSubscribeActuatorData
+    mqttclient.connect(mqttserver, mqttport, 60)
+    mqttclient.subscribe("edzna/one/actuator/switch", 0)
+    while mqttclient.loop() == 0:
+        pass
+
+def functionPublishSensorLuxesData():
+    luxes = light.value()
+    print "Publish Sensor Luxes Data: We got %s!" % luxes
+    return luxes
+
+def functionPublishSensorLuxes():
+    mqttclient = paho.Client()
+    mqttclient.connect(mqttserver, mqttport, 60)
+    while True:
+        data = functionPublishSensorLuxesData()
+        topic = "edzna/one/luxes"
+        mqttclient.publish(topic, data)
+        time.sleep(1)
+
+def functionPublishSensorBinaryMotionData():
+    value = motion.value()
+    if value:
+        value = 1
+    else:
+        value = 0
+    print "Publish Sensor Binary Motion Data: We got %s!" % value
+    return value
+
+def functionPublishSensorBinaryMotion():
+    mqttclient = paho.Client()
+    mqttclient.connect(mqttserver, mqttport, 60)
+    while True:
+        data = functionPublishSensorBinaryMotionData()
+        topic = "edzna/one/motion"
+        mqttclient.publish(topic, data)
+        time.sleep(1)
+
+def functionPublishSensorBinaryOpeningData():
+    value = button.value()
+    print "Publish Sensor Binary Opening Data: We got %s!" % value
+    return value
+
+def functionPublishSensorBinaryOpening():
+    mqttclient = paho.Client()
+    mqttclient.connect(mqttserver, mqttport, 60)
+    while True:
+        data = functionPublishSensorBinaryOpeningData()
+        topic = "edzna/one/opening"
+        mqttclient.publish(topic, data)
+        time.sleep(1)
+
+def functionSignalHandler(signal, frame):
+    sys.exit(0)
+
+if __name__ == '__main__':
+
+    signal.signal(signal.SIGINT, functionSignalHandler)
+
+    threadmqttsubscribeactuator = Thread(target=functionSubscribeActuator)
+    threadmqttsubscribeactuator.start()
+
+    threadmqttpublishsensorluxes = Thread(target=functionPublishSensorLuxes)
+    threadmqttpublishsensorluxes.start()
+
+    threadmqttpublishsensorbinarymotion = Thread(target=functionPublishSensorBinaryMotion)
+    threadmqttpublishsensorbinarymotion.start()
+
+    threadmqttpublishsensorbinaryopening = Thread(target=functionPublishSensorBinaryOpening)
+    threadmqttpublishsensorbinaryopening.start()
+
+    print "IoT Solution Architect: From The Device To A Cloud Platform"
+
+    while True:
+        time.sleep(5)
+
+# End of File
---
+++ Edzna/SolutionAutomation/Device/setup/setup.sh
@@ -0,0 +1,19 @@
+echo
+echo "Welcome to Edzna Playground"
+echo
+
+sleep 4
+
+#git clone https://github.com/TheIoTLearningInitiative/CodeLabs.git
+cd CodeLabs/Edzna/device
+
+pip install pip --upgrade
+sh requirements.opkg
+pip install -r requirements.pip
+# sh requirements.manual
+
+echo
+echo "Now go to CodeLabs/Edzna directory to get started!"
+echo
+echo "Happy Edzna'ing!"
+echo
