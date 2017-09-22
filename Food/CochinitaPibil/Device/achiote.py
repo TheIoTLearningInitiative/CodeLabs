288dc79885457935547a65b58ea58c7d5803909f
Abraham Arce <xe1gyq@gmail.com>
2017-09-22 10:23:47 -0700

    Update achiote.py

--- Food/CochinitaPibil/Device/achiote.py
+++ Food/CochinitaPibil/Device/achiote.py
@@ -12,7 +12,7 @@ from upm import pyupm_biss0001 as grovemotion
 luxes = grove.GroveLight(0)
 motion = grovemotion.BISS0001(6)
 opening = grove.GroveButton(4)
-switch = grove.GroveRelay(2)
+dswitch = grove.GroveRelay(2)
 
 mqttserver = "iot.eclipse.org"
 mqttport = 1883
@@ -38,9 +38,9 @@ def functionSensorLuxes():
 def functionBinarySensorMotionData():
     value = motion.value()
     if value:
-        value = 1
+        value = "ON"
     else:
-        value = 0
+        value = "OFF"
     print "Publish Binary Sensor Motion Data: We got %s!" % value
     return value
 
@@ -57,6 +57,10 @@ def functionBinarySensorMotion():
 
 def functionBinarySensorOpeningData():
     value = opening.value()
+    if value:
+        value = "ON"
+    else:
+        value = "OFF"
     print "Publish Binary Sensor Opening Data: We got %s!" % value
     return value
 
@@ -73,10 +77,10 @@ def functionBinarySensorOpening():
 
 def functionSwitchData(mosq, obj, msg):
     print "Subscribe Actuator Data: We received %s!" % msg.payload
-    if msg.payload == "1":
-        switch.on()
-    elif msg.payload == "0":
-        switch.off()
+    if msg.payload == "ON":
+        dswitch.on()
+    elif msg.payload == "OFF":
+        dswitch.off()
 
 def functionSwitch():
     mqttclient = paho.Client()
