7145e1e63fcb923baea76cc320c9f330b8453a35
Abraham Arce <xe1gyq@gmail.com>
2017-10-14 09:30:36 -0500

    Update goto.py

--- Gods/Bitol/goto.py
+++ Gods/Bitol/goto.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python
+#
+!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 """
@@ -19,7 +20,7 @@ from dronekit import connect, VehicleMode, LocationGlobalRelative
 import argparse
 parser = argparse.ArgumentParser(description='Commands vehicle using vehicle.simple_goto.')
 parser.add_argument('--connect',
-                    help="Vehicle connection target string. If not specified, SITL automatically started and used.")
+help="Vehicle connection target string. If not specified, SITL automatically started and used.")
 args = parser.parse_args()
 
 connection_string = args.connect
@@ -28,9 +29,9 @@ sitl = None
 
 # Start SITL if no connection string specified
 if not connection_string:
-    import dronekit_sitl
-    sitl = dronekit_sitl.start_default()
-    connection_string = sitl.connection_string()
+import dronekit_sitl
+sitl = dronekit_sitl.start_default()
+connection_string = sitl.connection_string()
 
 
 # Connect to the Vehicle
@@ -39,39 +40,39 @@ vehicle = connect(connection_string, wait_ready=True)
 
 
 def arm_and_takeoff(aTargetAltitude):
-    """
-    Arms vehicle and fly to aTargetAltitude.
-    """
+"""
+Arms vehicle and fly to aTargetAltitude.
+"""
 
-    print("Basic pre-arm checks")
-    # Don't try to arm until autopilot is ready
-    while not vehicle.is_armable:
-        print(" Waiting for vehicle to initialise...")
-        time.sleep(1)
+print("Basic pre-arm checks")
+# Don't try to arm until autopilot is ready
+while not vehicle.is_armable:
+print(" Waiting for vehicle to initialise...")
+time.sleep(1)
 
-    print("Arming motors")
-    # Copter should arm in GUIDED mode
-    vehicle.mode = VehicleMode("GUIDED")
-    vehicle.armed = True
+print("Arming motors")
+# Copter should arm in GUIDED mode
+vehicle.mode = VehicleMode("GUIDED")
+vehicle.armed = True
 
-    # Confirm vehicle armed before attempting to take off
-    while not vehicle.armed:
-        print(" Waiting for arming...")
-        time.sleep(1)
+# Confirm vehicle armed before attempting to take off
+while not vehicle.armed:
+print(" Waiting for arming...")
+time.sleep(1)
 
-    print("Taking off!")
-    vehicle.simple_takeoff(aTargetAltitude)  # Take off to target altitude
+print("Taking off!")
+vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
 
-    # Wait until the vehicle reaches a safe height before processing the goto
-    #  (otherwise the command after Vehicle.simple_takeoff will execute
-    #   immediately).
-    while True:
-        print(" Altitude: ", vehicle.location.global_relative_frame.alt)
-        # Break and return from function just below target altitude.
-        if vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:
-            print("Reached target altitude")
-            break
-        time.sleep(1)
+# Wait until the vehicle reaches a safe height before processing the goto
+# (otherwise the command after Vehicle.simple_takeoff will execute
+# immediately).
+while True:
+print(" Altitude: ", vehicle.location.global_relative_frame.alt)
+# Break and return from function just below target altitude.
+if vehicle.location.global_relative_frame.alt >= aTargetAltitude * 0.95:
+print("Reached target altitude")
+break
+time.sleep(1)
 
 
 arm_and_takeoff(10)
@@ -80,14 +81,14 @@ print("Set default/target airspeed to 3")
 vehicle.airspeed = 3
 
 print("Going towards first point for 30 seconds ...")
-point1 = LocationGlobalRelative(19.4031814, -99.1848997, 20)
+point1 = LocationGlobalRelative(19.3942835,-99.1763303, 20)
 vehicle.simple_goto(point1)
 
 # sleep so we can see the change in map
 time.sleep(30)
 
 print("Going towards second point for 30 seconds (groundspeed set to 10 m/s) ...")
-point2 = LocationGlobalRelative(19.4931814, -99.2748997, 20)
+point2 = LocationGlobalRelative(19.3950225, -99.1758533, 20)
 vehicle.simple_goto(point2, groundspeed=10)
 
 # sleep so we can see the change in map
@@ -102,4 +103,4 @@ vehicle.close()
 
 # Shut down simulator if it was started.
 if sitl:
-    sitl.stop()
+sitl.stop()
