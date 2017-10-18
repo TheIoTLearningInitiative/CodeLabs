72ff1e53f3869c1fa1ba1a9dc0c8eeb255168952
Abraham Arce <xe1gyq@gmail.com>
2017-10-17 19:52:54 -0500

    Create Yucatan.sh

---
+++ Hochob/Applications/SouthernMayaHighlands/Yucatan.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Implementation under HomeAssistant default configuration file: configuration.yaml
+
+# command_on: "cvlc --no-video-title-show --fullscreen v4l2:///dev/video0 &"
+# command_off: "pkill -9 vlc"
+
+# End of File
