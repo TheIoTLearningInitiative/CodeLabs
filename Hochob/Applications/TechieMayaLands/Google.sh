74580d02477d3cbd2a984d14a2a5019494206815
Abraham Arce <xe1gyq@gmail.com>
2017-10-18 16:57:49 -0500

    Update

--- Hochob/Applications/TechieMayaLands/Google.sh
+++ Hochob/Applications/TechieMayaLands/Google.sh
@@ -14,21 +14,13 @@ export GOOGLE_PATH=$(readlink -f "$0")
 export GOOGLE_DIRECTORY=$(dirname "$GOOGLE_PATH")
 
 LOCAL_SEASON="Halloween"
-
-LOCAL_DEVICE[0]="Tran"
-LOCAL_DEVICE[1]="Juum"
-LOCAL_DEVICE[2]="Eek'"
-#LOCAL_DEVICE[3]="Triki"
+LOCAL_DEVICE="Triki"
 
 # =============================================================================
 # Functions
 # =============================================================================
 
-chooseDevice()
-{
-    rand=$[ $RANDOM % 4 ]
-    LOCAL_DEVICE=${LOCAL_DEVICE[$rand]}
-}
+# None
 
 # =============================================================================
 # Main
@@ -37,18 +29,14 @@ chooseDevice()
 LogPid.sh $GOOGLE_PID $GOOGLE_NAME
 
 if [ "$LANGUAGE" = "english" ]; then
-    SpeechSynthetizer.sh off espeak english \
-        "Happy Halloween"
+    Robot.sh lupe/say "Happy Halloween"
 else
-    SpeechSynthetizer.sh off espeak spanish \
-        "Feliz dia de muertos"
+    Robot.sh lupe/decir "Feliz dia de muertos"
 fi
 
 LOCAL_SOUND=`find $SEASON/$LOCAL_SEASON/ -type f | shuf -n 1`
 Stream.sh "$LOCAL_DEVICE" "1.0" "$LOCAL_SOUND"
-#Stream.sh "$LOCAL_DEVICE" "1.0" "$SPEECH_MP3"
 
 # See implement WindRiver.sh logic, maybe Season service?
-# Choose Random Audio from Directory
 
 # End of File
