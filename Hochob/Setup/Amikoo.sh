f82e7b3ac2064f638bc032d588add825fda41ac2
Abraham Arce <xe1gyq@gmail.com>
2017-10-26 15:58:03 -0500

    Create Amikoo.sh

---
+++ Hochob/Setup/Amikoo.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo apt-get install espeak
+sudo apt-get install nodejs-legacy
+sudo apt-get install npm
+sudo npm install mqtt --save
+sudo npm install espeak --save
+sudo npm install sleep --save
+
+# End of File
