7a367577a656bd6d3b172788d502a208e76c5ec0
´Abraham <xe1gyq@gmail.com>
2017-10-27 17:06:42 -0500

    Update
    Signed-off-by: ´Abraham <xe1gyq@gmail.com>

---
+++ Hochob/Applications/TraditionMayaLands/Akbal.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -x
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. ~/CodeLabs/Hochob/Main.sh
+
+export IMIX_PID=$$
+export IMIX_NAME="Imix"
+export IMIX_PATH=$(readlink -f "$0")
+export IMIX_DIRECTORY=$(dirname "$IMIX_PATH")
+
+LOCAL_SEASON="DiaDeMuertos"
+LOCAL_DEVICE="Triki"
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+LogPid.sh $IMIX_PID $IMIX_NAME
+
+LOCAL_SOUND=`find $TRADITION_DIADEMUERTOS/Sound/ -type f | shuf -n 1`
+Stream.sh "$LOCAL_DEVICE" "1.0" "$LOCAL_SOUND"
+
+exit 0
+
+if [ "$LANGUAGE" = "english" ]; then
+    Amikoo.sh $AMIKOO_SAY "Happy Halloween"
+else
+    Amikoo.sh $AMIKOO_DECIR "Feliz dia de muertos"
+fi
+
+# See implement WindRiver.sh logic, maybe Season service?
+
+# End of File
---
+++ Hochob/Applications/TraditionMayaLands/Kan.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -x
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. ~/CodeLabs/Hochob/Main.sh
+
+export IK_PID=$$
+export IK_NAME="Ik"
+export IK_PATH=$(readlink -f "$0")
+export IK_DIRECTORY=$(dirname "$IK_PATH")
+
+LOCAL_TRADITION="DiaDeMuertos"
+LOCAL_DEVICE="Triki"
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+LogPid.sh $IK_PID $IK_NAME
+
+AmikooTradition.sh "$LOCAL_TRADITION" "Audio" "Random"
+sleep 5
+AmikooTradition.sh "$LOCAL_TRADITION" "Audio" "Stop"
+
+# End of File
