49bb4f9c09c7df11d82448522871dee2328ef063
Abraham Arce <xe1gyq@gmail.com>
2017-10-27 15:02:51 -0500

    Update Ik.sh

--- Hochob/Applications/TraditionMayaLands/Ik.sh
+++ Hochob/Applications/TraditionMayaLands/Ik.sh
@@ -13,7 +13,7 @@ export IK_NAME="Ik"
 export IK_PATH=$(readlink -f "$0")
 export IK_DIRECTORY=$(dirname "$IK_PATH")
 
-LOCAL_SEASON="DiaDeMuertos"
+LOCAL_TRADITION="DiaDeMuertos"
 LOCAL_DEVICE="Triki"
 
 # =============================================================================
@@ -28,6 +28,6 @@ LOCAL_DEVICE="Triki"
 
 LogPid.sh $IK_PID $IK_NAME
 
-AmikooTradition.sh "$LOCAL_SEASON" "Audio" "Stop"
+AmikooTradition.sh "$LOCAL_TRADITION" "Audio" "Stop"
 
 # End of File
