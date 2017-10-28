51f6f304126375dcb76d0976f7658780b620390f
Abraham Arce <xe1gyq@gmail.com>
2017-10-28 08:19:36 -0500

    Create Calaveritas.sh

---
+++ Hochob/Applications/TraditionMayaLands/Calaveritas.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -x
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. ~/CodeLabs/Hochob/Main.sh
+
+export CALAVERITAS_PID=$$
+export CALAVERITAS_NAME="Calaveritas"
+export CALAVERITAS_PATH=$(readlink -f "$0")
+export CALAVERITAS_DIRECTORY=$(dirname "$PCALAVERITAS_PATH")
+
+LOCAL_TRADITION="DiaDeMuertos"
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+LogPid.sh $CALAVERITAS_PID $CALAVERITAS_NAME
+
+AmikooTradition.sh "$LOCAL_TRADITION" "Audio" "ghostlymoan.mp3"
+Amikoo.sh amikoo/headright 1 && sleep .6
+Amikoo.sh amikoo/headleft 1 && sleep .6
+AmikooTradition.sh "$LOCAL_TRADITION" "Audio" "haunting.mp3"
+Amikoo.sh amikoo/moveforward 1 && sleep .1
+Amikoo.sh amikoo/movestop 1 && sleep .6
+Amikoo.sh amikoo/movebackward 1 && sleep .1
+Amikoo.sh amikoo/movestop 1 && sleep .6
+
+AmikooTradition.sh "$LOCAL_TRADITION" "Audio" "Stop"
+
+# End of File
