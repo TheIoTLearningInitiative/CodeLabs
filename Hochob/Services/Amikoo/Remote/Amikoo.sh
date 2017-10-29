174bbad3930fc7bff6af4487d33a3bac8ae1c4e7
Abraham Arce <xe1gyq@gmail.com>
2017-10-29 18:37:58 -0500

    Update Amikoo.sh

--- Hochob/Services/Amikoo/Remote/Amikoo.sh
+++ Hochob/Services/Amikoo/Remote/Amikoo.sh
@@ -41,11 +41,9 @@ if [ "$LOCAL_COMMAND" = "gcvisionapi" ]; then
     Mosquitto.sh $AMIKOO_GCVISIONAPI "Google Cloud Vision API"
 elif [ "$LOCAL_COMMAND" = "gctranslationapi" ]; then
     Mosquitto.sh $AMIKOO_GCTRANSLATIONAPI "Google Cloud Translation API"
-elif [ "$LOCAL_COMMAND" = "picture" ]; then
-    Mosquitto.sh $AMIKOO_PICTURE "Camera Picture"
 fi
 
-# Movement & Speak
+# Movement
 
 Mosquitto.sh $LOCAL_COMMAND $LOCAL_MESSAGE
 
