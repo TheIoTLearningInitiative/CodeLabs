0d54ed2d4b11016dd48fa7f912ce027daecd524e
Abraham Arce <xe1gyq@gmail.com>
2017-10-29 16:57:01 -0500

    Create AmikooVoice.sh

---
+++ Hochob/Services/Amikoo/Remote/AmikooVoice.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+set -x
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. ~/CodeLabs/Hochob/Main.sh
+
+export AMIKOOVOICE_PID=$$
+
+LOCAL_LANGUAGE="$1"
+LOCAL_MESSAGE=""
+LOCAL_TOPIC=""
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+i=0
+for var in "$@"
+do
+    if [ "$i" -ne 0 ]; then
+      LOCAL_MESSAGE=$LOCAL_MESSAGE"$var"
+      echo $LOCAL_MESSAGE
+    fi
+    i=`expr $i + 1`
+done
+
+if [ "$LOCAL_LANGUAGE" = "english" ]; then
+    LOCAL_TOPIC=$AMIKOO_TALK
+elif [ "$LOCAL_LANGUAGE" = "spanish" ]; then
+    LOCAL_TOPIC=$AMIKOO_HABLAR
+fi
+
+Mosquitto.sh $LOCAL_TOPIC $LOCAL_MESSAGE
