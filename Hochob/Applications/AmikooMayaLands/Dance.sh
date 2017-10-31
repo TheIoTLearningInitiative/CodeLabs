bb1ee086860390e6c85093921e29b8b5fc12e509
Abraham Arce <xe1gyq@gmail.com>
2017-10-30 23:42:08 -0500

    Create Dance.sh

---
+++ Hochob/Applications/AmikooMayaLands/Dance.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+set -x
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. ~/CodeLabs/Hochob/Main.sh
+
+export DANCE_PID=$$
+export DANCE_NAME="Dance"
+export DANCE_PATH=$(readlink -f "$0")
+export DANCE_DIRECTORY=$(dirname "$DANCE_PATH")
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+LogPid.sh $MECCANOID_PID $MECCANOID_NAME
+
+Amikoo.sh amikoo/headright && sleep .8
+Amikoo.sh amikoo/headleft && sleep .8
+Amikoo.sh amikoo/rightup && sleep .8
+Amikoo.sh amikoo/leftup && sleep .8
+Amikoo.sh amikoo/rightfold && sleep .8
+Amikoo.sh amikoo/leftfold && sleep .8
+#Amikoo.sh amikoo/moveright && sleep .8
+#Amikoo.sh amikoo/movestop && sleep .8
+#Amikoo.sh amikoo/moveleft && sleep .8
+#Amikoo.sh amikoo/movestop && sleep .8
+#Amikoo.sh amikoo/moveleft && sleep .8
+#Amikoo.sh amikoo/movestop && sleep .8
+#Amikoo.sh amikoo/moveleft && sleep .8
+#Amikoo.sh amikoo/movestop && sleep .8
+Amikoo.sh amikoo/leftfold && sleep .8
+Amikoo.sh amikoo/rightfold && sleep .8
+Amikoo.sh amikoo/leftup && sleep .8
+Amikoo.sh amikoo/rightup && sleep .8
+Amikoo.sh amikoo/headleft && sleep .8
+Amikoo.sh amikoo/headright && sleep .8
+
+# End of File
