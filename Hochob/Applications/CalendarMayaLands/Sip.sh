6b8739d86585db0071709ef95ac8ddf608fe14eb
Abraham Arce <xe1gyq@gmail.com>
2017-11-07 08:28:18 -0600

    Update Sip.sh

--- Hochob/Applications/CalendarMayaLands/Sip.sh
+++ Hochob/Applications/CalendarMayaLands/Sip.sh
@@ -13,12 +13,22 @@ export SIP_NAME="Sip"
 export SIP_PATH=$(readlink -f "$0")
 export SIP_DIRECTORY=$(dirname "$SIP_PATH")
 
-LOCAL_INSTITUTION_NAME="Intel Guadalajara"
-LOCAL_EVENT_NAME_ENGLISH="Innovation Week 2017"
-LOCAL_EVENT_NAME_SPANISH="Semana de Innovación 2017"
+#LOCAL_INSTITUTION_NAME="Intel Guadalajara"
+#LOCAL_EVENT_NAME_ENGLISH="Innovation Week 2017"
+#LOCAL_EVENT_NAME_SPANISH="Semana de Innovación 2017"
+
+# PCIE Tech Week 2017
+LOCAL_INSTITUTION_NAME=`Intel Guadalajara`
+LOCAL_EVENT_NAME_ENGLISH="PCI Express Tech Week"
+LOCAL_EVENT_NAME_SPANISH="Semana de Tecnología PCI Express"
+LOCAL_PHRASE="The PCI Center of Excellence invites you to attend \
+              the PCIE Tech Week at Intel GDC Auditorium this November 7th, 8th and 9th. \
+              Senior Principal Engineers, Principal Engineers and Technical Leaders \
+              will be available to hear your ideas. Please come and share your knowledge so \
+              we can provide customer support with excellende and efficiency."
 
 LOCAL_LANGUAGE[0]="english"
-LOCAL_LANGUAGE[1]="spanish"
+LOCAL_LANGUAGE[1]="english"
 
 # =============================================================================
 # Functions
