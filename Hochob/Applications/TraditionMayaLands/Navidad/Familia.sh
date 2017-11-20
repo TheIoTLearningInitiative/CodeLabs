9036630fbe68a00d6b8413f119d2f5c37887d9e9
Abraham Arce <xe1gyq@gmail.com>
2017-11-20 15:02:26 -0600

    Create Familia.sh

---
+++ Hochob/Applications/TraditionMayaLands/Navidad/Familia.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -x
+
+# =============================================================================
+# Variables
+# =============================================================================
+
+. ~/CodeLabs/Hochob/Main.sh
+
+export FAMILIA_PID=$$
+export FAMILIA_NAME="Familia"
+export FAMILIA_PATH=$(readlink -f "$0")
+export FAMILIA_DIRECTORY=$(dirname "$FAMILIA_PATH")
+
+LOCAL_DEVICE="Triki"
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+# None
+
+# =============================================================================
+# Main
+# =============================================================================
+
+LogPid.sh $FAMILIA_PID $FAMILIA_NAME
+
+LOCAL_SOUND=`find $TRADITION_NAVIDAD/Sound/ -type f | shuf -n 1`
+Stream.sh "$LOCAL_DEVICE" "1.0" "$LOCAL_SOUND"
