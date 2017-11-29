d2079028c48c2833b6c0d3d50c41c4fcc72b6e98
Abraham Arce <xe1gyq@gmail.com>
2017-11-29 08:56:51 -0600

    Update Loteria.py

--- Hochob/Services/Games/Loteria/Loteria.py
+++ Hochob/Services/Games/Loteria/Loteria.py
@@ -23,7 +23,7 @@ with open("LasCartasSimple.rnd", "w") as f:
         print image
         os.system('mosquitto_pub -h 10.215.56.158 -p 1883 -q 2 -t amikoo/tradition/image -m Loteria' + '/' + image)
         os.system('mosquitto_pub -h 10.215.56.158 -p 1883 -q 2 -t amikoo/sound/speechsynthetizer/decir -m "' + line + '"')
-        time.sleep(3.5)
+        time.sleep(5)
     f.close()
 
 #os.system('espeak -v es-la -a 100 -p 50 -s 170 -f LasCartasSimple.rnd -w LasCartasSimple.wav')
