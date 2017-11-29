d1b7b6954316311accfb030fc10702beb8fd8489
Abraham Arce <xe1gy@gmail.com>
2017-11-28 21:55:18 -0600

    Update
    Signed-off-by: Abraham Arce <xe1gy@gmail.com>

--- Hochob/Services/Games/Loteria/Loteria.py
+++ Hochob/Services/Games/Loteria/Loteria.py
@@ -1,7 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-def shuffle():
-    with open("teams.txt", mode="r", encoding="utf-8") as myFile:
-        lines = list(myFile)
+import os
+import random
+import time
+
+with open("LasCartasSimple.txt") as f:
+    lines = f.readlines()
     random.shuffle(lines)
-    print(lines)
+    f.close()
+with open("LasCartasSimple.rnd", "w") as f:
+    f.writelines(lines)
+    for line in lines:
+        line = line.rstrip('\r\n')
+        print line
+        os.system('mosquitto_pub -h iot.eclipse.org -p 1883 -q 2 -t amikoo/sound/speechsynthetizer/decir -m "' + line + '"')
+        time.sleep(3)
+    f.close()
+
+os.system('espeak -v es-la -a 100 -p 50 -s 170 -f LasCartasSimple.rnd -w LasCartasSimple.wav')
 
