380e6c634992a27c00a75d30ca20effaec276c4c
Abraham Arce <xe1gyq@gmail.com>
2017-12-06 22:54:00 -0600

    Update AmikooExpertise.sh

--- Hochob/Services/Amikoo/Remote/AmikooExpertise.sh
+++ Hochob/Services/Amikoo/Remote/AmikooExpertise.sh
@@ -11,7 +11,7 @@ set -x
 export AMIKOO_PID=$$
 
 LOCAL_COMMAND="$1"
-LOCAL_MESSAGE=""
+LOCAL_MESSAGE="$2"
 
 # =============================================================================
 # Functions
@@ -23,53 +23,12 @@ LOCAL_MESSAGE=""
 # Main
 # =============================================================================
 
-i=0
-for var in "$@"
-do
-    if [ "$i" -ne 0 ]; then
-      LOCAL_MESSAGE=$LOCAL_MESSAGE"$var"
-      echo $LOCAL_MESSAGE
-    fi
-    i=`expr $i + 1`
-done
-
-echo $LOCAL_MESSAGE
-
-# Skills Advanced
-
 if [ "$LOCAL_COMMAND" = "gcvisionapi" ]; then
     Mosquitto.sh $AMIKOO_GCVISIONAPI "Google Cloud Vision API"
 elif [ "$LOCAL_COMMAND" = "gctranslationapi" ]; then
     Mosquitto.sh $AMIKOO_GCTRANSLATIONAPI "Google Cloud Translation API"
-fi
-
-# Movement
-
-Mosquitto.sh $LOCAL_COMMAND $LOCAL_MESSAGE
-
-exit 0
-
-# Sandbox
-
-LOCAL_DEMO=`Demo.sh`
-if [ "$LOCAL_DEMO" -eq "1" ]; then
-    if [ "$LOCAL_MESSAGE" != "1" ]
-    then
-        Espeak.sh on spanish "$LOCAL_MESSAGE"
-    fi
-else
-    Mosquitto.sh $LOCAL_COMMAND "$LOCAL_MESSAGE"
-fi
-
-if [ "$LOCAL_COMMAND" = "lupe/message" ]; then
-    LOCAL_LANGUAGE=`Language.sh`
-    if [ "$LOCAL_LANGUAGE" = "english" ]; then
-        AmikooVoice.sh $LOCAL_LANGUAGE $LOCAL_MESSAGE
-    else
-        AmikooVoice.sh $LOCAL_LANGUAGE $LOCAL_MESSAGE
-    fi
-else
-    Mosquitto.sh $LOCAL_COMMAND $LOCAL_MESSAGE
+elif [ "$LOCAL_COMMAND" = "amazonalexa" ]; then
+    Mosquitto.sh $AMIKOO_GCTRANSLATIONAPI $LOCAL_MESSAGE
 fi
 
 # End of File
