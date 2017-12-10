a4ef13c7716727a5c17ded6e7198b07ead94feaa
Abraham Arce <xe1gyq@gmail.com>
2017-12-10 09:54:28 -0600

    Update CarasYGestos.sh

--- Hochob/Services/Games/CarasYGestos/CarasYGestos.sh
+++ Hochob/Services/Games/CarasYGestos/CarasYGestos.sh
@@ -27,6 +27,9 @@ LOCAL_GAME="CarasYGestos"
 
 LOCAL_IMAGE=`find $GAMES_CARASYGESTOS/Images/ -type f | shuf -n 1`
 LOCAL_IMAGE=`echo $LOCAL_IMAGE | rev | cut -d'/' -f-1 | rev`
+
 AmikooCommunication.sh "$LOCAL_GAME" "Image" "$LOCAL_IMAGE"
+
+AmikooCommunication.sh "$LOCAL_GAME" "Image" "Stop"
 
 # End of File
