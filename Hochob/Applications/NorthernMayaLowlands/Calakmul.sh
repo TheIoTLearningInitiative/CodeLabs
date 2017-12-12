bd4c6b82314c993d0fcb76b8095b2e8d8f580d5f
Abraham Arce <xe1gyq@gmail.com>
2017-12-12 15:49:17 -0600

    Update Calakmul.sh

--- Hochob/Applications/NorthernMayaLowlands/Calakmul.sh
+++ Hochob/Applications/NorthernMayaLowlands/Calakmul.sh
@@ -24,26 +24,25 @@ export CALAKMUL_DIRECTORY=$(dirname "$CALAKMUL_PATH")
 # =============================================================================
 
 LOCAL_INSTITUTION=`Name.sh`
+LOCAL_DEVICE="Eek'"
 LOCAL_LANGUAGE=`Language.sh`
 LOCAL_TTS_ENGINE="pico2wave"
 
 if [ "$LOCAL_LANGUAGE" = "english" ]; then
-    SpeechSynthetizer.sh off $LOCAL_TTS_ENGINE english \
-    "To be written"
+    StreamText.sh $LOCAL_DEVICE $LOCAL_LANGUAGE $LOCAL_TTS_ENGINE \
+        "To be written"
 else
-    SpeechSynthetizer.sh off $LOCAL_TTS_ENGINE spanish \
-    "La ciencia es muy divertida, es la curiosidad sobre el mundo y cómo se comporta. \
-    Es observarlo, escucharlo y registrar lo que se aprende. \
-    Te gustaría entender porqué la Tierra gira alrededor del Sol? \
-    Porqué las cosas caen hacia el suelo? \
-    Que hace a los pájaros volar? \
-    El científico es una persona que se dedica a entender el mundo y saber como se comporta. \
-    Cualquiera puede pensar como un científico, y a ti, te gustaría ser científico? \
-    "
+    StreamText.sh $LOCAL_DEVICE $LOCAL_LANGUAGE $LOCAL_TTS_ENGINE \
+        "La ciencia es muy divertida, es la curiosidad sobre el mundo y cómo se comporta. \
+         Es observarlo, escucharlo y registrar lo que se aprende. \
+         Te gustaría entender porqué la Tierra gira alrededor del Sol? \
+         Porqué las cosas caen hacia el suelo? \
+         Que hace a los pájaros volar? \
+         El científico es una persona que se dedica a entender el mundo y saber como se comporta. \
+         Cualquiera puede pensar como un científico, y a ti, te gustaría ser científico? \
+        "
 fi
 
 #Los que aplican los descubrimientos de la ciencia para hacer cosas útiles se llaman Ingenieros.
-
-Stream.sh "Eek'" "1.0" "$SPEECH_MP3"
 
 # End of File
