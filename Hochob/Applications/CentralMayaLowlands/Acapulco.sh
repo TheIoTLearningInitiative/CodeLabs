efe3069e4f383640a2c6c72b5814b88f7d83b79c
Abraham Arce <xe1gyq@gmail.com>
2017-12-12 11:55:04 -0600

    Update Acapulco.sh

--- Hochob/Applications/CentralMayaLowlands/Acapulco.sh
+++ Hochob/Applications/CentralMayaLowlands/Acapulco.sh
@@ -27,14 +27,6 @@ LOCAL_DEVICE="Eek'"
 LOCAL_LANGUAGE=`Language.sh`
 LOCAL_TTS_ENGINE="pico2wave"
 
-
-    StreamText.sh "$LOCAL_DEVICE" "$LOCAL_LANGUAGE" "$LOCAL_TTS_ENGINE" \
-    "Futuros Ingenieros de ${LOCAL_INSTITUTION}. Bienvenidos a esta primer sala, le llamamos: El Inicio. \
-    Aquí  recordaras a los Mayas, la historia de Intel y de este Centro de Diseño de Intel en Guadalajara. \
-    Que disfruten su recorrido."
-    
-exit 0
-
 if [ "$LOCAL_LANGUAGE" = "english" ]; then
     StreamText.sh "$LOCAL_DEVICE" "$LOCAL_LANGUAGE" "$LOCAL_TTS_ENGINE" \
     "Future Engineers from ${LOCAL_INSTITUTION}. Welcome to this first hall! We called it: The Beginning. \
