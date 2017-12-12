d2cafc4270224a44cb337120aa46db3af6c817b3
Abraham Arce <xe1gyq@gmail.com>
2017-12-12 11:13:14 -0600

    Update Acapulco.sh

--- Hochob/Applications/CentralMayaLowlands/Acapulco.sh
+++ Hochob/Applications/CentralMayaLowlands/Acapulco.sh
@@ -24,19 +24,22 @@ export ACAPULCO_DIRECTORY=$(dirname "$ACAPULCO_PATH")
 
 LOCAL_INSTITUTION=`Name.sh`
 LOCAL_LANGUAGE=`Language.sh`
-LOCAL_TTS_ENGINE="pico2wave"
+LOCAL_DEVICE='Triki'
+LOCAL_TTS_ENGINE='pico2wave'
 
 if [ "$LOCAL_LANGUAGE" = "english" ]; then
-    StreamText.sh $LOCAL_TTS_ENGINE $LOCAL_LANGUAGE \
+    StreamText.sh $LOCAL_DEVICE $LOCAL_TTS_ENGINE $LOCAL_LANGUAGE \
     "Future Engineers from ${LOCAL_INSTITUTION}. Welcome to this first hall! We called it: The Beginning. \
     Here we will remember the Mayans, the history of Intel y this Intel Guadalajara Design Center. \
     Enjoy your tour."
 else
-    StreamText.sh $LOCAL_TTS_ENGINE $LOCAL_LANGUAGE \
+    StreamText.sh $LOCAL_DEVICE $LOCAL_TTS_ENGINE $LOCAL_LANGUAGE \
     "Futuros Ingenieros de ${LOCAL_INSTITUTION}. Bienvenidos a esta primer sala, le llamamos: El Inicio. \
     Aquí  recordaras a los Mayas, la historia de Intel y de este Centro de Diseño de Intel en Guadalajara. \
     Que disfruten su recorrido."
 fi
+
+exit 0
 
 if [ "$LOCAL_LANGUAGE" = "english" ]; then
     StreamAudio.sh "Triki" "1.0" "$AREA_CENTRAL_MAYA_LOWLANDS/AcapulcoEnglish.mp3"
