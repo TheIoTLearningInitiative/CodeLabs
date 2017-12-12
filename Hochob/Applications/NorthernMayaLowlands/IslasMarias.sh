0980cfa152dd024616d44256346147bda5b9f0f8
Abraham Arce <xe1gyq@gmail.com>
2017-12-11 22:26:40 -0600

    Update IslasMarias.sh

--- Hochob/Applications/NorthernMayaLowlands/IslasMarias.sh
+++ Hochob/Applications/NorthernMayaLowlands/IslasMarias.sh
@@ -17,7 +17,7 @@ LOCAL_LANGUAGE="spanish"
 
 LOCAL_QUESTION_SPANISH[0]="¿Cual fue la aportación de los Mayas?"
 LOCAL_QUESTION_SPANISH[1]="¿Cual es el sistema de numeración en el que los números se representan utilizando solamente dos cifras? cero y uno"
-LOCAL_QUESTION_SPANISH[2]="¿Con cuantas personas empezó Intel en Gdl?"
+LOCAL_QUESTION_SPANISH[2]="¿Con cuantas personas empezó Intel en Guadalajara?"
 LOCAL_QUESTION_SPANISH[3]="¿Qué es lo que hace Intel?"
 LOCAL_QUESTION_SPANISH[4]="¿Qué otras cosas hace Intel?"
 LOCAL_QUESTION_SPANISH[5]="¿Elemento para la elaboración de los microprocesadores?"
