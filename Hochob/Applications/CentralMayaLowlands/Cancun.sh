9fc2cb3936f62265984540789bc538eae71195ea
Abraham Arce <xe1gyq@gmail.com>
2017-12-12 13:51:36 -0600

    Update Cancun.sh

--- Hochob/Applications/CentralMayaLowlands/Cancun.sh
+++ Hochob/Applications/CentralMayaLowlands/Cancun.sh
@@ -12,7 +12,9 @@ export CANCUN_PID=$$
 export CANCUN_PATH=$(readlink -f "$0")
 export CANCUN_DIRECTORY=$(dirname "$CANCUN_PATH")
 
+LOCAL_LANGUAGE=`Language.sh`
 LOCAL_DEVICE="Triki"
+LOCAL_VOLUME="1.0"
 
 # =============================================================================
 # Functions
@@ -24,12 +26,10 @@ LOCAL_DEVICE="Triki"
 # Main
 # =============================================================================
 
-LOCAL_LANGUAGE=`Language.sh`
-
 if [ "$LOCAL_LANGUAGE" = "english" ]; then
-    StreamAudio.sh "$LOCAL_DEVICE" "1.0" "$AREA_CENTRAL_MAYA_LOWLANDS/CancunEnglish.mp3"
+    StreamAudio.sh "$LOCAL_DEVICE" "$LOCAL_VOLUME" "$AREA_CENTRAL_MAYA_LOWLANDS/CancunEnglish.mp3"
 else
-    StreamAudio.sh "$LOCAL_DEVICE" "1.0" "$AREA_CENTRAL_MAYA_LOWLANDS/Cancun.mp3"
+    StreamAudio.sh "$LOCAL_DEVICE" "$LOCAL_VOLUME" "$AREA_CENTRAL_MAYA_LOWLANDS/Cancun.mp3"
 fi
 
 # End of File
