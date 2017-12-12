589cf747821e774cd541ecd6ce2c676c32cf5b49
Abraham Arce <xe1gyq@gmail.com>
2017-12-12 15:42:39 -0600

    Update Stream2Chromecast.sh

--- Hochob/Services/Stream/Stream2Chromecast/Stream2Chromecast.sh
+++ Hochob/Services/Stream/Stream2Chromecast/Stream2Chromecast.sh
@@ -39,9 +39,11 @@ then
     $STREAM2CHROMECAST_BINARY -devicename $LOCAL_DEVICE -stop
     killall -9 $STREAM2CHROMECAST_BINARY    
     $STREAM2CHROMECAST_BINARY -devicename $LOCAL_DEVICE -setvol $LOCAL_VOLUME
-    $STREAM2CHROMECAST_BINARY -devicename $LOCAL_DEVICE $STREAM2CHROMECAST_ARGUMENTS \
-                              -transcodeopts '-b:v 1000k -b:a  32k' \
-                              $STREAM2CHROMECAST_TRANSCODER $LOCAL_FILE
+    $STREAM2CHROMECAST_BINARY -devicename $LOCAL_DEVICE \
+                              #$STREAM2CHROMECAST_ARGUMENTS \
+                              #-transcodeopts '-b:v 1000k -b:a  32k' \
+                              $STREAM2CHROMECAST_TRANSCODER \
+                              $LOCAL_FILE
     $STREAM2CHROMECAST_BINARY -devicename $LOCAL_DEVICE -stop
 else
     echo "Invalid number of arguments, see Documentation"
