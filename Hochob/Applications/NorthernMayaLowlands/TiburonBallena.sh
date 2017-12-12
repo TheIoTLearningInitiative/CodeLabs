1fec7c8f5fcf7bb7d3b042d2e8a884ab7c671605
Abraham Arce <xe1gyq@gmail.com>
2017-12-12 15:26:16 -0600

    Update TiburonBallena.sh

--- Hochob/Applications/NorthernMayaLowlands/TiburonBallena.sh
+++ Hochob/Applications/NorthernMayaLowlands/TiburonBallena.sh
@@ -13,6 +13,11 @@ export TIBURONBALLENA_NAME="Mapimi Application"
 export TIBURONBALLENA_PATH=$(readlink -f "$0")
 export TIBURONBALLENA_DIRECTORY=$(dirname "$TIBURONBALLENA_PATH")
 
+LOCAL_DEVICE="Eek'"
+LOCAL_INSTITUTION=`Name.sh`
+LOCAL_LANGUAGE=`Language.sh`
+LOCAL_TTS_ENGINE="pico2wave"
+
 # =============================================================================
 # Functions
 # =============================================================================
@@ -22,11 +27,6 @@ export TIBURONBALLENA_DIRECTORY=$(dirname "$TIBURONBALLENA_PATH")
 # =============================================================================
 # Main
 # =============================================================================
-
-LOCAL_DEVICE="Eek'"
-LOCAL_INSTITUTION=`Name.sh`
-LOCAL_LANGUAGE=`Language.sh`
-LOCAL_TTS_ENGINE="pico2wave"
 
 StreamText.sh $LOCAL_DEVICE spanish $LOCAL_TTS_ENGINE \
       "
@@ -49,8 +49,6 @@ StreamText.sh $LOCAL_DEVICE spanish $LOCAL_TTS_ENGINE \
       Cuatro, Mariner 4.
       "
 SurveyMultipleChoice.sh $LOCAL_DEVICE spanish $LOCAL_TTS_ENGINE 4
-
-exit 0
 
 StreamText.sh $LOCAL_DEVICE english tt \
       "
