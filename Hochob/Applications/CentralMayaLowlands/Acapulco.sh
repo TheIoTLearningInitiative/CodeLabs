cfe0ebabab751fdd0ff984f464abfde6606bf335
Abraham Arce <xe1gyq@gmail.com>
2017-12-12 11:09:26 -0600

    Update Acapulco.sh

--- Hochob/Applications/CentralMayaLowlands/Acapulco.sh
+++ Hochob/Applications/CentralMayaLowlands/Acapulco.sh
@@ -24,27 +24,24 @@ export ACAPULCO_DIRECTORY=$(dirname "$ACAPULCO_PATH")
 
 LOCAL_INSTITUTION=`Name.sh`
 LOCAL_LANGUAGE=`Language.sh`
+LOCAL_TTS_ENGINE="pico2wave"
 
 if [ "$LOCAL_LANGUAGE" = "english" ]; then
-    SpeechSynthetizer.sh off espeak english \
+    StreamText.sh $LOCAL_TTS_ENGINE $LOCAL_LANGUAGE \
     "Future Engineers from ${LOCAL_INSTITUTION}. Welcome to this first hall! We called it: The Beginning. \
     Here we will remember the Mayans, the history of Intel y this Intel Guadalajara Design Center. \
     Enjoy your tour."
 else
-    SpeechSynthetizer.sh off espeak spanish \
+    StreamText.sh $LOCAL_TTS_ENGINE $LOCAL_LANGUAGE \
     "Futuros Ingenieros de ${LOCAL_INSTITUTION}. Bienvenidos a esta primer sala, le llamamos: El Inicio. \
     Aquí  recordaras a los Mayas, la historia de Intel y de este Centro de Diseño de Intel en Guadalajara. \
     Que disfruten su recorrido."
 fi
 
-Stream.sh "Triki" "1.0" "$SPEECH_MP3"
-
-LOCAL_LANGUAGE=`Language.sh`
-
 if [ "$LOCAL_LANGUAGE" = "english" ]; then
-    Stream.sh "Triki" "1.0" "$AREA_CENTRAL_MAYA_LOWLANDS/AcapulcoEnglish.mp3"
+    StreamAudio.sh "Triki" "1.0" "$AREA_CENTRAL_MAYA_LOWLANDS/AcapulcoEnglish.mp3"
 else
-    Stream.sh "Triki" "1.0" "$AREA_CENTRAL_MAYA_LOWLANDS/Acapulco.mp3"
+    StreamAudio.sh "Triki" "1.0" "$AREA_CENTRAL_MAYA_LOWLANDS/Acapulco.mp3"
 fi
 
 # End of File
