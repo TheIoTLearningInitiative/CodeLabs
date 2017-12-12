3dfa0ecab280f886fc2db0671a93b3bd36007f95
Abraham Arce <xe1gyq@gmail.com>
2017-12-12 16:07:42 -0600

    Update Ixtapa.sh

--- Hochob/Applications/CentralMayaLowlands/Ixtapa.sh
+++ Hochob/Applications/CentralMayaLowlands/Ixtapa.sh
@@ -30,72 +30,71 @@ LOCAL_TTS_ENGINE="pico2wave"
 # =============================================================================
 
 StreamText.sh $LOCAL_DEVICE $LOCAL_LANGUAGE $LOCAL_TTS_ENGINE \
-      "
-      Puedes decirme que aportaron los Mayas a la tecnología? \
-      Uno, Las pirámides. \
-      Dos, El cero. \
-      Tres, El calendario. \
-      Cuatro, El chocolate. \
+      " \
+       Puedes decirme que aportaron los Mayas a la tecnología? \
+       Uno, Las pirámides. \
+       Dos, El cero. \
+       Tres, El calendario. \
+       Cuatro, El chocolate. \
       "
 SurveyMultipleChoice.sh $LOCAL_DEVICE $LOCAL_LANGUAGE $LOCAL_TTS_ENGINE 2
 
 StreamText.sh $LOCAL_DEVICE $LOCAL_LANGUAGE $LOCAL_TTS_ENGINE \
-      "
-      Quiénes son los fundadores de Intel? \
+      " \
+       Quiénes son los fundadores de Intel? \
       "
 StreamText.sh $LOCAL_DEVICE $LOCAL_LANGUAGE $LOCAL_TTS_ENGINE \
-      "
-      Uno, Robert Noyce, Gordon Moore. \
-      Dos, Steve Allen. \
-      Tres, Elon Musk. \
-      Cuatro, Larry Page and Sergey Brin. \
+      " \
+       Uno, Robert Noyce, Gordon Moore. \
+       Dos, Steve Allen. \
+       Tres, Elon Musk. \
+       Cuatro, Larry Page and Sergey Brin. \
       "      
 SurveyMultipleChoice.sh $LOCAL_DEVICE $LOCAL_LANGUAGE $LOCAL_TTS_ENGINE 1
 
 StreamText.sh $LOCAL_DEVICE $LOCAL_LANGUAGE $LOCAL_TTS_ENGINE \
-      "
-      Cuántos ingenieros erán cuando empezó Intel Guadalajara?
-      Uno, 60. \
-      Dos, 33. \
-      Tres, 1200. \
-      Cuatro, 15.
+      " \
+       Cuántos ingenieros erán cuando empezó Intel Guadalajara? \
+       Uno, 60. \
+       Dos, 33. \
+       Tres, 1200. \
+       Cuatro, 15.
       " 
 SurveyMultipleChoice.sh $LOCAL_DEVICE $LOCAL_LANGUAGE $LOCAL_TTS_ENGINE 2
 
 StreamText.sh $LOCAL_DEVICE $LOCAL_LANGUAGE $LOCAL_TTS_ENGINE \
-      "
-      El microprocesador es el cerebro de la computadora? Sí o no?
+      " \
+       El microprocesador es el cerebro de la computadora? Sí o no?
       "
 SurveyYesNo.sh $LOCAL_DEVICE $LOCAL_LANGUAGE $LOCAL_TTS_ENGINE yes
 
 StreamText.sh $LOCAL_DEVICE $LOCAL_LANGUAGE $LOCAL_TTS_ENGINE \
-      "
-      Las fábricas de Intel son 200 veces mas limpias que un quirófano? Sí o no?
+      " \
+       Las fábricas de Intel son 200 veces mas limpias que un quirófano? Sí o no?
       "
 SurveyYesNo.sh $LOCAL_DEVICE $LOCAL_LANGUAGE $LOCAL_TTS_ENGINE no
 
 StreamText.sh $LOCAL_DEVICE $LOCAL_LANGUAGE $LOCAL_TTS_ENGINE \
-      "
-      Podemos encontrar el microprocesador en?
-      Uno, Accesorios para las personas. \
-      Dos, En la tierra. \
-      Tres, En el espacio. \
-      Cuatro, En todos los anteriores.
+      " \
+       Podemos encontrar el microprocesador en? \
+       Uno, Accesorios para las personas. \
+       Dos, En la tierra. \
+       Tres, En el espacio. \
+       Cuatro, En todos los anteriores.
       "
 SurveyMultipleChoice.sh $LOCAL_DEVICE $LOCAL_LANGUAGE $LOCAL_TTS_ENGINE 4
 
 StreamText.sh $LOCAL_DEVICE $LOCAL_LANGUAGE $LOCAL_TTS_ENGINE \
-      "
-      En el sistema binario, el cero significa?
-      Uno, Encendido. \
-      Dos, Apagado. \
-      Tres, Nada. \
-      Cuatro, Todo.
+      " \
+       En el sistema binario, el cero significa? \
+       Uno, Encendido. \
+       Dos, Apagado. \
+       Tres, Nada. \
+       Cuatro, Todo.
       "
 SurveyMultipleChoice.sh $LOCAL_DEVICE $LOCAL_LANGUAGE $LOCAL_TTS_ENGINE 2
 
 StreamText.sh $LOCAL_DEVICE $LOCAL_LANGUAGE $LOCAL_TTS_ENGINE \
-      "
-      Eso es todo. Muchas gracias por tu participacion.
-      "
+      "Eso es todo. Muchas gracias por tu participacion."
+
 # End of File
