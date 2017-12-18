4bd9dd3f1526b1b5358e5bc5e85aca30ff04eb1a
Abraham Arce <xe1gyq@gmail.com>
2017-12-18 10:00:24 -0600

    Update ElCielo.sh

--- Hochob/Applications/NorthernMayaLowlands/ElCielo.sh
+++ Hochob/Applications/NorthernMayaLowlands/ElCielo.sh
@@ -12,6 +12,9 @@ export ELCIELO_PID=$$
 export ELCIELO_PATH=$(readlink -f "$0")
 export ELCIELO_DIRECTORY=$(dirname "$ELCIELO_PATH")
 
+LOCAL_DEVICE=$HOCHOB_SERVER_APPLICATIONS_NORTHERN_MAYA_LOWLANDS_DEVICE
+LOCAL_VOLUME=$HOCHOB_SERVER_APPLICATIONS_NORTHERN_MAYA_LOWLANDS_DEVICE_VOLUME
+
 # =============================================================================
 # Functions
 # =============================================================================
@@ -22,7 +25,7 @@ export ELCIELO_DIRECTORY=$(dirname "$ELCIELO_PATH")
 # Main
 # =============================================================================
 
-Stream.sh "Eek'" "1.0" "$AREA_NORTHERN_MAYA_LOWLANDS/ElCielo.mp3"
+Stream.sh $LOCAL_DEVICE $LOCAL_VOLUME "$AREA_NORTHERN_MAYA_LOWLANDS/ElCielo.mp3"
 
 #kill -- -$(ps -o pgid=$ELCIELO_PID | grep -o '[0-9]*')
 
