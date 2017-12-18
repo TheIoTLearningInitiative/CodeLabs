8bc96c1f31edc58e8e6f10f652634e3491f3831c
Abraham Arce <xe1gyq@gmail.com>
2017-12-17 18:46:42 -0600

    Update LaMichilia.sh

--- Hochob/Applications/NorthernMayaLowlands/LaMichilia.sh
+++ Hochob/Applications/NorthernMayaLowlands/LaMichilia.sh
@@ -12,7 +12,11 @@ export LAMICHILIA_PID=$$
 export LAMICHILIA_PATH=$(readlink -f "$0")
 export LAMICHILIA_DIRECTORY=$(dirname "$LAMICHILIA_PATH")
 
-LOCAL_TTS_ENGINE="pico2wave"
+LOCAL_INSTITUTION=`Name.sh`
+LOCAL_DEVICE="Eek'"
+LOCAL_VOLUME="1.0"
+LOCAL_LANGUAGE=`Language.sh`
+LOCAL_TTS_ENGINE="festival"
 
 # =============================================================================
 # Functions
@@ -28,16 +32,17 @@ LOCAL_TTS_ENGINE="pico2wave"
 
 LOCAL_LANGUAGE=`Language.sh`
 if [ "$LOCAL_LANGUAGE" = "english" ]; then
-    SpeechSynthetizer.sh off $LOCAL_TTS_ENGINE english \
-    "To be written"
+    StreamText.sh $LOCAL_DEVICE $LOCAL_LANGUAGE $LOCAL_TTS_ENGINE \
+    " \
+    "
 else
-    SpeechSynthetizer.sh off $LOCAL_TTS_ENGINE spanish \
-    "Intel no solo se queda en la tierra, es tal el alcance del Microprocesador que puede \
+    StreamText.sh $LOCAL_DEVICE $LOCAL_LANGUAGE $LOCAL_TTS_ENGINE \
+    " \
+     Intel no solo se queda en la tierra, es tal el alcance del Microprocesador que puede \
      estar en el universo, por ejemplo, ha trabajado con la NASA para crear cohetes y vehiculos \
-     no tripulados. Veamos el siguiente video."
+     no tripulados. Veamos el siguiente video. \
+    "
 fi
-
-Stream.sh "Eek'" "1.0" "$SPEECH_MP3"
 
 #kill -- -$(ps -o pgid=$LAMICHILIA_PID | grep -o '[0-9]*')
 
