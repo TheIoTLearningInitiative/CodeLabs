cba2c867b91d66a1c6739cfd3c3b199366ef6cc7
Abraham Arce <xe1gyq@gmail.com>
2017-12-17 18:54:52 -0600

    Update TiburonBallena.sh

--- Hochob/Applications/NorthernMayaLowlands/TiburonBallena.sh
+++ Hochob/Applications/NorthernMayaLowlands/TiburonBallena.sh
@@ -9,7 +9,7 @@ set -x
 . ~/CodeLabs/Hochob/Main.sh
 
 export TIBURONBALLENA_PID=$$
-export TIBURONBALLENA_NAME="Mapimi Application"
+export TIBURONBALLENA_NAME="TiburonBallena"
 export TIBURONBALLENA_PATH=$(readlink -f "$0")
 export TIBURONBALLENA_DIRECTORY=$(dirname "$TIBURONBALLENA_PATH")
 
@@ -31,35 +31,39 @@ LOCAL_TTS_ENGINE="festival"
 StreamAudio.sh "Eek'" "1.0" "$AREA_NORTHERN_MAYA_LOWLANDS/TiburonBallena/LiftoffSpaceXFalcon9CRS10Mission.mp4"
 
 StreamText.sh $LOCAL_DEVICE $LOCAL_LANGUAGE $LOCAL_TTS_ENGINE \
-      "
-      Imagina esto, cielo rosa, atardeceres naranjas, mucho desierto y acantilados. \
-      Pero donde puedes ver esto y mas? En el planeta marte! Veamos si puedes responder un par de preguntas.
+      " \
+       Imagina esto, cielo rosa, atardeceres naranjas, mucho desierto y acantilados. \
+       Pero donde puedes ver esto y mas? En el planeta marte! Veamos si puedes responder un par de preguntas.
       "
 
 StreamText.sh $LOCAL_DEVICE $LOCAL_LANGUAGE $LOCAL_TTS_ENGINE \
-      "
-      A Marte se le conoce también como el planeta rojo? que opinas, sí o no?\
+      " \
+       A Marte se le conoce también como el planeta rojo? que opinas, sí o no? \
       "
 SurveyYesNo.sh $LOCAL_DEVICE $LOCAL_LANGUAGE $LOCAL_TTS_ENGINE yes
 
 StreamText.sh $LOCAL_DEVICE $LOCAL_LANGUAGE $LOCAL_TTS_ENGINE \
+      " \
+       Cual fue la primer misión que tomó por primera vez fotos de Marte? \
       "
-      Cual fue la primer misión que tomó por primera vez fotos de Marte? \
-      Uno, Curiosity. \
-      Dos, Spirit. \
-      Tres, Sojourner. \
-      Cuatro, Mariner 4.
+
+StreamText.sh $LOCAL_DEVICE english $LOCAL_TTS_ENGINE \
+      " \
+       Uno, Curiosity. \
+       Dos, Spirit. \
+       Tres, Sojourner. \
+       Cuatro, Mariner 4.
       "
 SurveyMultipleChoice.sh $LOCAL_DEVICE $LOCAL_LANGUAGE $LOCAL_TTS_ENGINE 4
 
-StreamText.sh $LOCAL_DEVICE $LOCAL_LANGUAGE $LOCAL_TTS_ENGINE \
+StreamText.sh $LOCAL_DEVICE english $LOCAL_TTS_ENGINE \
+      " \
+       Which spacecraft took the first photos of Mars? \
+       One, Curiosity. \
+       Two, Spirit. \
+       Three, Sojourner. \
+       Four, Mariner 4.
       "
-      Which spacecraft took the first photos of Mars? \
-      One, Curiosity. \
-      Two, Spirit. \
-      Three, Sojourner. \
-      Four, Mariner 4.
-      "
-SurveyMultipleChoice.sh $LOCAL_DEVICE $LOCAL_LANGUAGE $LOCAL_TTS_ENGINE 4
+SurveyMultipleChoice.sh $LOCAL_DEVICE english $LOCAL_TTS_ENGINE 4
 
 # End of File
