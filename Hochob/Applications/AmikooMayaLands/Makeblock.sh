fe9bc26d6066544049a9421dca9cc88588e8b217
Abraham Arce <xe1gyq@gmail.com>
2017-12-17 18:40:39 -0600

    Update Makeblock.sh

--- Hochob/Applications/AmikooMayaLands/Makeblock.sh
+++ Hochob/Applications/AmikooMayaLands/Makeblock.sh
@@ -17,7 +17,7 @@ LOCAL_INSTITUTION=`Name.sh`
 LOCAL_DEVICE="Amikoo"
 LOCAL_VOLUME="1.0"
 LOCAL_LANGUAGE=`Language.sh`
-LOCAL_TTS_ENGINE="espeak"
+LOCAL_TTS_ENGINE="festival"
 
 # =============================================================================
 # Functions
