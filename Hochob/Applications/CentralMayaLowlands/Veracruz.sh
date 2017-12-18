8d8f3db4dded0467b85ffe3beaf62f034daab6ad
Abraham Arce <xe1gyq@gmail.com>
2017-12-18 12:22:15 -0600

    Update Veracruz.sh

--- Hochob/Applications/CentralMayaLowlands/Veracruz.sh
+++ Hochob/Applications/CentralMayaLowlands/Veracruz.sh
@@ -12,6 +12,12 @@ export VERACRUZ_PID=$$
 export VERACRUZ_PATH=$(readlink -f "$0")
 export VERACRUZ_DIRECTORY=$(dirname "$VERACRUZ_PATH")
 
+LOCAL_INSTITUTION=`Name.sh`
+LOCAL_DEVICE=$HOCHOB_SERVER_APPLICATIONS_CENTRAL_MAYA_HIGHLANDS_DEVICE
+LOCAL_VOLUME="1.0"
+LOCAL_LANGUAGE=`Language.sh`
+LOCAL_TTS_ENGINE=$HOCHOB_SERVER_APPLICATIONS_CENTRAL_MAYA_LOWLANDS_TTS_ENGINE
+
 # =============================================================================
 # Functions
 # =============================================================================
@@ -24,14 +30,16 @@ export VERACRUZ_DIRECTORY=$(dirname "$VERACRUZ_PATH")
 
 LOCAL_LANGUAGE=`Language.sh`
 if [ "$LOCAL_LANGUAGE" = "english" ]; then
-    SpeechSynthetizer.sh off espeak english \
-    "Hi There"
+    StreamText.sh "$LOCAL_DEVICE" "$LOCAL_LANGUAGE" "$LOCAL_TTS_ENGINE" \
+    " \
+     Hi There!. \
+    "
 else
-    SpeechSynthetizer.sh off espeak spanish \
-    "Hola a todos"
+    StreamText.sh "$LOCAL_DEVICE" "$LOCAL_LANGUAGE" "$LOCAL_TTS_ENGINE" \
+    " \
+     Hola a todos!. \
+    "
 fi
-
-Stream.sh "Juum" "1.0" "$SPEECH_MP3"
 
 # kill -- -$(ps -o pgid=$VERACRUZ_PID | grep -o '[0-9]*')
 
