d2e1ddc5bbae5d0be9e2484fafd7950936618291
Abraham Arce <xe1gyq@gmail.com>
2017-12-17 18:47:36 -0600

    Update Mapimi.sh

--- Hochob/Applications/NorthernMayaLowlands/Mapimi.sh
+++ Hochob/Applications/NorthernMayaLowlands/Mapimi.sh
@@ -17,7 +17,7 @@ LOCAL_INSTITUTION=`Name.sh`
 LOCAL_DEVICE="Eek'"
 LOCAL_VOLUME="1.0"
 LOCAL_LANGUAGE=`Language.sh`
-LOCAL_TTS_ENGINE="pico2wave"
+LOCAL_TTS_ENGINE="festival"
 
 # =============================================================================
 # Functions
@@ -31,12 +31,14 @@ LOCAL_TTS_ENGINE="pico2wave"
 
 if [ "$LOCAL_LANGUAGE" = "english" ]; then
     StreamText.sh "$LOCAL_DEVICE" "$LOCAL_LANGUAGE" "$LOCAL_TTS_ENGINE" \
-        "It was a great pleasure to meet you and have you here at Intel Guadalajara. \
+        " \
+         It was a great pleasure to meet you and have you here at Intel Guadalajara. \
          Come back soon future Engineers. \
         "
 else
     StreamText.sh "$LOCAL_DEVICE" "$LOCAL_LANGUAGE" "$LOCAL_TTS_ENGINE" \
-        "Nos dio mucho gusto conocerlas, conocerlos y que hayan venido a Intel Guadalajara. \
+        " \
+         Nos dio mucho gusto conocerlas, conocerlos y que hayan venido a Intel Guadalajara. \
          Fue un placer recibirlos en nuestras instalaciones y esperamos verlos de nuevo!. \
         "
 fi
