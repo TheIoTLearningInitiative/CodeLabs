3c4f017d311850e8c686b21c678e0d44168df4b9
Abraham Arce <xe1gyq@gmail.com>
2017-12-31 17:50:14 -0600

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- Gods/Bitol/Documentation/DroneSolution/QGroundControl.md
+++ Gods/Bitol/Documentation/DroneSolution/QGroundControl.md
@@ -23,3 +23,5 @@ user@workstation:~/Drones$ ./QGroundControl.AppImage
 ```
 
 
+
+
--- Gods/Bitol/DroneSolution/Autopilot/SoftwareInTheLoop/DroneKitSitl/HelloDroneKitSitl.py
+++ Gods/Bitol/DroneSolution/Autopilot/SoftwareInTheLoop/DroneKitSitl/HelloDroneKitSitl.py
@@ -1,7 +1,12 @@
-print "Start simulator (SITL)"
+#!/usr/bin/python
+
 import dronekit_sitl
+
+print "Start DroneKit Simulator (SITL)"
 sitl = dronekit_sitl.start_default()
 connection_string = sitl.connection_string()
 
+print "Stop DroneKit Simulator (SITL)"
 sitl.stop()
+
 print("Completed")
